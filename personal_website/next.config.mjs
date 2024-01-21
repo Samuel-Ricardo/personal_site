645ad5392de0729a15ac535fbe94ed728201e56c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-21 10:05:18 -0300

    [ :gear: ] | update: config - next > sass (style)

--- personal_website/next.config.mjs
+++ personal_website/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [`${__dirname}/styles`],
+  },
+};
 
 export default nextConfig;
