b603e17bcdce041798cc744c73161e8ae3fda334
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-21 09:53:43 -0300

    [ :gear: ] | update: config - cypress (test)

--- personal_website/cypress.config.ts
+++ personal_website/cypress.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  viewportWidth: 1280,
+  viewportHeight: 720,
+
+  video: true,
+  videoCompression: true,
+  screenshotOnRunFailure: true,
+
+  videosFolder: 'cypress/videos',
+  screenshotsFolder: 'cypress/screenshots',
+  downloadsFolder: 'cypress/downloads',
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
