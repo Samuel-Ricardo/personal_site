c9bc4cec906cbf30663de560e3f4bb97f5bfc854
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-22 14:31:48 -0300

    [ :link: ] | create: hook - theme (app)

--- personal_website/src/hooks/config/ui/themes.hook.ts
+++ personal_website/src/hooks/config/ui/themes.hook.ts
@@ -2,7 +2,7 @@ import { IAppThemeState } from '@/@types/hooks/config/ui/themes';
 import { AppTheme } from '@/@types/style/theme';
 import { create } from 'zustand';
 
-const useTheme = create<IAppThemeState>(set => ({
+export const useTheme = create<IAppThemeState>(set => ({
   theme: 'light',
   setTheme: (theme: AppTheme) => set({ theme }),
 }));
