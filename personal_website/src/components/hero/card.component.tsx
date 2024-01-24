801ad0628f7f176de13fb043bdca6c98981c7c01
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-24 16:47:38 -0300

    [ :art: ] | create: component - hero > card (app)

---
+++ personal_website/src/components/hero/card.component.tsx
@@ -0,0 +1,6 @@
+import { FrostedGlassCard } from '../card/glass.card';
+import './card.style.scss';
+
+export const HeroCard = () => {
+  return <FrostedGlassCard className="z-50">Card</FrostedGlassCard>;
+};
