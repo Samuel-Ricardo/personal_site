583d8b2b9e37e6062d7c25fdc1c4a969da654096
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-24 00:44:17 -0300

    [ :art: ] | create: component - canvas > circuit (app)

--- personal_website/src/components/canvas/circuit.component.tsx
+++ personal_website/src/components/canvas/circuit.component.tsx
@@ -16,6 +16,44 @@ export const Circuit = () => {
 
   const mouse: IPosition = { x: 0, y: 0 };
 
+  useEffect(() => {
+    if (canvasRef.current) canvasRef.current.width = window.innerWidth;
+    if (canvasRef.current) canvasRef.current.height = window.innerHeight;
+
+    const context = canvasRef.current?.getContext('2d');
+
+    const space: ISpace = {
+      width: canvasRef.current?.width || 1,
+      height: canvasRef.current?.height || 1,
+    };
+
+    const points =
+      (canvasRef.current?.width || 1) < 720
+        ? generate({ space, quantity: 20 })
+        : generate({ space });
+
+    canvasRef.current?.addEventListener('mousemove', e => {
+      mouse.x = e.clientX;
+      mouse.y = e.clientY * 2;
+    });
+
+    function animate() {
+      render({
+        context,
+        points,
+        space,
+        networkConfig: {
+          mouse,
+        },
+      });
+
+      updateFrame({ space, points });
+      window.requestAnimationFrame(animate);
+    }
+
+    if (context) animate();
+  }, [canvasRef, generate, render, updateFrame]);
+
   return (
     <canvas
       id="canvas-circuit"
