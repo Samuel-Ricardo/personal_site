6b15937747c0c14b669d22b7426d25a1e041adb2
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-24 00:06:23 -0300

    [ :link: ] | create: hook - circuit > usePoints (app::hook:canvas)

---
+++ personal_website/src/hooks/canvas/circuit/points/generator.hook.ts
@@ -0,0 +1,25 @@
+import { IPoint } from '@/@types/props/circuit/point';
+import { IPointsProps } from '@/@types/props/circuit/points';
+import { useCallback } from 'react';
+
+export const usePoints = () => {
+  const generate = useCallback(({ space, quantity = 35 }: IPointsProps) => {
+    const points: IPoint[] = [];
+    for (let i = 0; i < quantity; i++) {
+      points.push({
+        position: {
+          x: Math.random() * space.width,
+          y: Math.random() * space.height,
+        },
+        radius: Math.random() * 1 + 1,
+        velocity: {
+          x: Math.floor(Math.random() * 50) - 25,
+          y: Math.floor(Math.random() * 50) - 25,
+        },
+      });
+    }
+    return points;
+  }, []);
+
+  return { generate };
+};
