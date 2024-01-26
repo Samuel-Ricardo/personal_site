e51ca419c10dbae5210f08631a9a0956bbe8c66d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-26 14:49:56 -0300

    [ :black_nib: ] | setup: fonts (app)

---
+++ personal_website/src/app/style/fonts.ts
@@ -0,0 +1,4 @@
+import { Fraunces, Inconsolata } from 'next/font/google';
+
+export const FRAUNCES = Fraunces({ subsets: ['latin'], preload: true });
+export const INCONSOLATA = Inconsolata({ subsets: ['latin'], preload: true });
