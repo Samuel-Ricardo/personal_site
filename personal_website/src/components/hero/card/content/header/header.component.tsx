0568915cf5f438cbe827acf8ad5063d887b8c1b7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-26 17:41:56 -0300

    [ :art: ] | create: card - content > header (app::style/hero)

---
+++ personal_website/src/components/hero/card/content/header/header.component.tsx
@@ -0,0 +1,10 @@
+import './header.style.scss';
+
+export const HeroCardContentContainerHeader = () => {
+  return (
+    <div className={`header w-full`}>
+      <h1>Samuel Ricardo</h1>
+      <h2>Full Stack Developer</h2>
+    </div>
+  );
+};
