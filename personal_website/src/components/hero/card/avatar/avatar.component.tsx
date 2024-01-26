2d253e2c7cdb732da27e522d0e070f4874da13b1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-26 14:01:47 -0300

    [ :art: ] | update: hero - card > avatar (app::components)

--- personal_website/src/components/hero/card/avatar/avatar.component.tsx
+++ personal_website/src/components/hero/card/avatar/avatar.component.tsx
@@ -4,7 +4,14 @@ import './avatar.style.scss';
 export const HeroAvatar = () => {
   return (
     <div className="avatar">
-      <Image src="/hero_avatar.jpg" alt="avatar" width={1920} height={1080} />
+      <Image
+        src="/hero_avatar.jpg"
+        alt="avatar"
+        width={1920}
+        height={1080}
+        placeholder="blur"
+        blurDataURL="/hero_avatar.jpg"
+      />
     </div>
   );
 };
