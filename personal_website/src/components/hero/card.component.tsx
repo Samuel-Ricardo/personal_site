6d4a59ea2ccb6482b1a69861f920e4d83e28e3c6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-26 16:34:38 -0300

    [ :art: ] | update: hero - card (app::components)

--- personal_website/src/components/hero/card.component.tsx
+++ personal_website/src/components/hero/card.component.tsx
@@ -1,12 +1,13 @@
 import { FrostedGlassCard } from '../card/glass.card';
 import './card.style.scss';
 import { HeroAvatar } from './card/avatar/avatar.component';
+import { HeroCardContentContainer } from './card/content/container/container.component';
 
 export const HeroCard = () => {
   return (
     <FrostedGlassCard className="z-50">
       <HeroAvatar />
-      <div className="w-full bg-violet-500">Content</div>
+      <HeroCardContentContainer />
     </FrostedGlassCard>
   );
 };
