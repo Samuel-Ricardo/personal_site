21f2953de4e52aabe21b5d209fa8561af5540cfc
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-26 18:07:33 -0300

    [ :art: ] | create: card - content > about (app::components/hero)

---
+++ personal_website/src/components/hero/card/content/about/about.component.tsx
@@ -0,0 +1,10 @@
+export const HeroCardContentAbout = () => {
+  return (
+    <div className="about">
+      <p>
+        I am a Full Stack Developer with a passion for creating innovative and
+        user-friendly web applications.
+      </p>
+    </div>
+  );
+};
