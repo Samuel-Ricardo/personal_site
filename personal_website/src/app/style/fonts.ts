9b45995c9ef2490497ecb290f8f7310d05153fee
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-26 16:32:56 -0300

    [ :black_nib: ] | setup: font - NEUTON (app)

--- personal_website/src/app/style/fonts.ts
+++ personal_website/src/app/style/fonts.ts
@@ -1,4 +1,16 @@
-import { Fraunces, Inconsolata } from 'next/font/google';
+import {
+  Fraunces,
+  Inconsolata,
+  Libre_Baskerville,
+  Neuton,
+  Space_Mono,
+  Xanh_Mono,
+} from 'next/font/google';
 
 export const FRAUNCES = Fraunces({ subsets: ['latin'], preload: true });
 export const INCONSOLATA = Inconsolata({ subsets: ['latin'], preload: true });
+export const NEUTON = Neuton({
+  subsets: ['latin'],
+  preload: true,
+  weight: '400',
+});
