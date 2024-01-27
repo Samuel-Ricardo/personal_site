e620a2e7e3ae1799524edc91bbaba905ae384efe
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-26 21:57:25 -0300

    [ :art: ] | create: card - content > about (app::style/hero)

--- personal_website/src/components/hero/card/content/about/about.component.tsx
+++ personal_website/src/components/hero/card/content/about/about.component.tsx
@@ -4,9 +4,9 @@ export const HeroCardContentAbout = () => {
   return (
     <div className="about">
       <p>
-        👨‍💻| I&aposm a Programmer passionated for unlock 🔓 the barriers between
-        the real 🧠 and the imaginary 🌌, convert dreams into reality and of
-        course... <br /> Coffee ☕ into solutions 💡
+        👨‍💻| I&apos;m a Programmer passionated about unlocking 🔓 the barriers
+        between the real 🧠 and the imaginary 🌌, convert dreams into reality
+        and of course... <br /> Coffee ☕ into solutions 💡
       </p>
     </div>
   );
