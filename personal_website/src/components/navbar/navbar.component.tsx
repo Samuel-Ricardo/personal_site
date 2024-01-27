db579b75bb2b30d8b9f45f131faf21c041f0f260
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 15:49:39 -0300

    [ :art: ] | update: navbar (app::component)

--- personal_website/src/components/navbar/navbar.component.tsx
+++ personal_website/src/components/navbar/navbar.component.tsx
@@ -1,6 +1,7 @@
 import { Logo } from '../logo/logo.component';
 import { NavbarItemContainer } from './item/container/container.component';
 import { NavbarItem } from './item/item.component';
+import { Menu } from './menu/menu.component';
 
 import './navbar.style.scss';
 
@@ -13,7 +14,9 @@ export const Navbar = () => {
         <NavbarItem href="/portfolio">Portfolio</NavbarItem>
         <NavbarItem href="/articles">Articles</NavbarItem>
         <NavbarItem href="/jobs">Jobs</NavbarItem>
+        <Menu />
       </NavbarItemContainer>
+      <Menu />
     </div>
   );
 };
