57744498e80b3e72870d80fbdc2b0ba90b1ea75f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 17:38:46 -0300

    [ :syringe: ] | update: module - app > animation (app::modules)

--- personal_website/src/modules/app.module.ts
+++ personal_website/src/modules/app.module.ts
@@ -1,6 +1,11 @@
 import { Container } from 'inversify';
 import { FONTS_MODULE } from './fonts/fonts.module';
+import { ANIMATION_MODULE } from './animations/animation.module';
 
 const MODULES = new Container();
 
-export const APP_MODULE = Container.merge(MODULES, FONTS_MODULE);
+export const APP_MODULE = Container.merge(
+  MODULES,
+  FONTS_MODULE,
+  ANIMATION_MODULE,
+);
