aa8f6f79d5fc8e0ab5280cc587d6d59232b80096
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 17:39:26 -0300

    [ :syringe: ] | update: factory - app > animation (app::modules)

--- personal_website/src/modules/app.factory.ts
+++ personal_website/src/modules/app.factory.ts
@@ -1,5 +1,7 @@
+import { ANIMATION_FACTORY as ANIMATION } from './animations/animation.factory';
 import { FONTS_FACTORY as FONTS } from './fonts/fonts.factory';
 
 export const MODULES = {
   FONTS,
+  ANIMATION,
 };
