3ddf280cef4fa98537dab9d3d6b2bd011364bd5b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 12:42:14 -0300

    [ :art: ] | update: navbar - item  (app::component)

--- personal_website/src/components/navbar/item/item.component.tsx
+++ personal_website/src/components/navbar/item/item.component.tsx
@@ -1,3 +1,13 @@
-export const NavbarItem = ({ children }: React.PropsWithChildren) => {
-  return <li>{children}</li>;
+import './item.style.scss';
+
+import { INavbarItemProps } from '@/@types/props/navbar/item';
+import Link from 'next/link';
+
+export const NavbarItem = ({ children, href }: INavbarItemProps) => {
+  return (
+    <li>
+      <Link href={href}>{children}</Link>
+      <div className="line" />
+    </li>
+  );
 };
