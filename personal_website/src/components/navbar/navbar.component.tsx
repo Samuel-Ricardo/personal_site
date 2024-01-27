bfacea3319af1832c585ce6326f9b634ac32d07e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 11:37:04 -0300

    [ :art: ] | create: component - navbar (app::component)

---
+++ personal_website/src/components/navbar/navbar.component.tsx
@@ -0,0 +1,9 @@
+import { Logo } from '../logo/logo.component';
+
+export const Navbar = () => {
+  return (
+    <div id="navbar" className="fixed top-0 z-50 frosted-glass w-full px-3">
+      <Logo />
+    </div>
+  );
+};
