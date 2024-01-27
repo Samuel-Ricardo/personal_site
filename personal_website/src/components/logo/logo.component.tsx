1209f139d705a6c4c2e0a50b684fc2b2b762f3d2
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 11:36:35 -0300

    [ :art: ] | update: component - logo (app::components)

--- personal_website/src/components/logo/logo.component.tsx
+++ personal_website/src/components/logo/logo.component.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+import './logo.style.scss';
+import { INCONSOLATA } from '@/app/style/fonts';
+
 export const Logo = () => {
-  return <div id="logo">{'<Samuel/>'}</div>;
+  return (
+    <div id="logo" className={`${INCONSOLATA.className}`}>
+      <Link href="/#hero">{'<Samuel/>'}</Link>
+    </div>
+  );
 };
