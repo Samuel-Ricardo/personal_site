69733022d8eff28ad47b2a428dede618dcfc8cbd
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 15:32:29 -0300

    [ :recycle: ] | refact: rename - component

--- personal_website/src/components/navbar/menu/container/container.component.tsx
+++ personal_website/src/components/navbar/menu/container/container.component.tsx
@@ -1,7 +1,5 @@
 import { MotionDiv } from '@/components/motion/div.component';
 
-export const MenuContainerComponent = ({
-  children,
-}: React.PropsWithChildren) => {
+export const MenuContainer = ({ children }: React.PropsWithChildren) => {
   return <MotionDiv>{children}</MotionDiv>;
 };
