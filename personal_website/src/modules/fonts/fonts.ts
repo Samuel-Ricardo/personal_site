01668a0873331596a9737aba375aed7fea26e4b1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 15:22:52 -0300

    [ :black_nib: ] | setup: fonts - module (app::modules)

---
+++ personal_website/src/modules/fonts/fonts.ts
@@ -0,0 +1,31 @@
+import {
+  Fraunces,
+  Inconsolata,
+  Libre_Baskerville,
+  Neuton,
+  Space_Mono,
+  Xanh_Mono,
+} from 'next/font/google';
+
+export const FRAUNCES = Fraunces({ subsets: ['latin'], preload: true });
+export const INCONSOLATA = Inconsolata({ subsets: ['latin'], preload: true });
+export const NEUTON = Neuton({
+  subsets: ['latin'],
+  preload: true,
+  weight: '400',
+});
+export const LIBRE_BASKERVILLE = Libre_Baskerville({
+  subsets: ['latin'],
+  preload: true,
+  weight: '400',
+});
+export const SPACE_MONO = Space_Mono({
+  subsets: ['latin'],
+  preload: true,
+  weight: '400',
+});
+export const XANH_MONO = Xanh_Mono({
+  subsets: ['latin'],
+  preload: true,
+  weight: '400',
+});
