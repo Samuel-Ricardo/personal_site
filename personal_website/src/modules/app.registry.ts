f9d516b1fccd8099854ce5b06a289c58d585770b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 17:38:09 -0300

    [ :syringe: ] | update: registry - app > animation (app::modules)

--- personal_website/src/modules/app.registry.ts
+++ personal_website/src/modules/app.registry.ts
@@ -1,5 +1,7 @@
+import { ANIMATION_REGISTRY as ANIMATION } from './animations/animation.registry';
 import { FONTS_REGISTRY as FONTS } from './fonts/fonts.registry';
 
 export const MODULE = {
   FONTS,
+  ANIMATION,
 };
