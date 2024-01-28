bc2295ee4c79cb49543808f424cecb2d55d93e3c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 23:20:36 -0300

    [ :syringe: ] | update: animation - factory (app::modules:framer_motion)

--- personal_website/src/modules/animations/framer_motion/motion.factory.ts
+++ personal_website/src/modules/animations/framer_motion/motion.factory.ts
@@ -3,6 +3,12 @@ import { ANIMATION_FRAMER_MOTION_REGISTRY } from './motion.registry';
 import { Animator } from '@/@types/animations/motion/animator';
 
 export const ANIMATION_FRAMER_MOTION_FACTORY = {
+  SLIDE: {
+    IN: () =>
+      ANIMATION_FRAMER_MOTION_MODULE.get<Animator>(
+        ANIMATION_FRAMER_MOTION_REGISTRY.SLIDE.IN,
+      ),
+  },
   SVG: {
     DRAW: () =>
       ANIMATION_FRAMER_MOTION_MODULE.get<Animator>(
