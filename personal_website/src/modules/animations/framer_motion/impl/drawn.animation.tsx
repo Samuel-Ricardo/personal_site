d512fcd44b9917cdcc99fe43f673be8d1becebfa
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 23:08:12 -0300

    [ :clapper: ] | update: animation - draw (app::modules:framer_motion)

--- personal_website/src/modules/animations/framer_motion/impl/drawn.animation.tsx
+++ personal_website/src/modules/animations/framer_motion/impl/drawn.animation.tsx
@@ -3,6 +3,7 @@ import { Animator } from '@/@types/animations/motion/animator';
 export const DRAW_ANIMATION: Animator = props => ({
   initial: { pathLength: 0 },
   animate: { pathLength: 1 },
+  transition: { duration: 3 },
   variants: {
     erase: { pathLength: 0 },
     draw: { pathLength: 1 },
