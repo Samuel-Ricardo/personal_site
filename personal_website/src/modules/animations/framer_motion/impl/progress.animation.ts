8f48565812d61b42de19dc2ae934441be1d823f7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-28 17:29:33 -0300

    [ :clapper: ] | update: animation - progress (app::modules:framer_motion)

--- personal_website/src/modules/animations/framer_motion/impl/progress.animation.ts
+++ personal_website/src/modules/animations/framer_motion/impl/progress.animation.ts
@@ -3,4 +3,10 @@ import { Animator } from '@/@types/animations/motion/animator';
 export const PROGRESS_ANIMATION: Animator = props => ({
   initial: { opacity: 0, width: 0 },
   animate: { opacity: 1, width: '100%' },
+  ...props,
+  transition: {
+    duration: 2,
+    ease: 'easeInOut',
+    ...props?.transition,
+  },
 });
