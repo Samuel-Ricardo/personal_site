7f07967979637d4205afe181e4806e68f51f591b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-28 13:06:10 -0300

    [ :bookmark_tabs: ] | create: section - i work with (app)

---
+++ personal_website/src/app/sections/work_with/works.section.tsx
@@ -0,0 +1,12 @@
+import { InfinityHorizontalCarousel } from '@/components/carousel/horizontal/infity.component';
+
+import './works.style.scss';
+
+export const WorkWithSection = () => {
+  return (
+    <section id="i-work-with">
+      <h1>I Work With</h1>
+      <InfinityHorizontalCarousel />
+    </section>
+  );
+};
