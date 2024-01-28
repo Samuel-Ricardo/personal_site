20fa197ac52bff6da999dcee1cfa56ddee21f319
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-28 00:01:53 -0300

    [ :art: ] | update: navbar - items (app::component)

--- personal_website/src/components/navbar/navbar.component.tsx
+++ personal_website/src/components/navbar/navbar.component.tsx
@@ -1,3 +1,4 @@
+import { MODULES } from '@/modules/app.factory';
 import { Logo } from '../logo/logo.component';
 import { NavbarItemContainer } from './item/container/container.component';
 import { NavbarItem } from './item/item.component';
@@ -6,14 +7,32 @@ import { Menu } from './menu/menu.component';
 import './navbar.style.scss';
 
 export const Navbar = () => {
+  const animation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.IN();
   return (
     <div id="navbar">
       <Logo />
       <NavbarItemContainer>
-        <NavbarItem href="/home">Home</NavbarItem>
-        <NavbarItem href="/portfolio">Portfolio</NavbarItem>
-        <NavbarItem href="/articles">Articles</NavbarItem>
-        <NavbarItem href="/jobs">Jobs</NavbarItem>
+        <NavbarItem li={{ id: 'home', ...animation() }} href="/home">
+          Home
+        </NavbarItem>
+        <NavbarItem
+          li={{ id: 'portfolio', ...animation({ transition: { delay: 1 } }) }}
+          href="/portfolio"
+        >
+          Portfolio
+        </NavbarItem>
+        <NavbarItem
+          li={{ id: 'articles', ...animation({ transition: { delay: 2 } }) }}
+          href="/articles"
+        >
+          Articles
+        </NavbarItem>
+        <NavbarItem
+          li={{ id: 'jobs', ...animation({ transition: { delay: 3 } }) }}
+          href="/jobs"
+        >
+          Jobs
+        </NavbarItem>
         <Menu />
       </NavbarItemContainer>
       <Menu />
