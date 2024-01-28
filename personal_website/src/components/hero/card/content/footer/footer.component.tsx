3033ad7b68a05808004ed8b52ef1b3c717a52aad
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 21:40:24 -0300

    [ :art: ] | update: footer - button > download (app::component)

--- personal_website/src/components/hero/card/content/footer/footer.component.tsx
+++ personal_website/src/components/hero/card/content/footer/footer.component.tsx
@@ -2,16 +2,19 @@ import './footer.style.scss';
 
 import { Button } from '@/components/button/button.component';
 import { NavigateButton } from '@/components/button/navigate/button.component';
-import { SimpleDownloadIcon } from '@/icons/download/simple.icon';
+import { DownloadIcon } from '@/icons/download/download.icon';
+import { MODULES } from '@/modules/app.factory';
 
 export const HeroCardContentFooter = () => {
+  console.log(MODULES.ANIMATION.FRAMER_MOTION.SVG.DRAW()());
   return (
     <div className="footer">
       <NavigateButton link={{ href: './#contact-me' }}>
         Contact me
       </NavigateButton>
       <Button>
-        <SimpleDownloadIcon /> Resume
+        <DownloadIcon Paths={MODULES.ANIMATION.FRAMER_MOTION.SVG.DRAW()()} />
+        Resume
       </Button>
     </div>
   );
