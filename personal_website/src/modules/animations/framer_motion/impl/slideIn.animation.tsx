734c83fc3d6a9ad1bf76f43322e685463a88a096
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-28 00:02:17 -0300

    [ :clapper: ] | update: animation - slideIn (app::modules:framer_motion)

--- personal_website/src/modules/animations/framer_motion/impl/slideIn.animation.tsx
+++ personal_website/src/modules/animations/framer_motion/impl/slideIn.animation.tsx
@@ -1,8 +1,8 @@
 import { Animator } from '@/@types/animations/motion/animator';
 
 export const SLIDE_IN_ANIMATION: Animator = props => ({
+  ...props,
   initial: { x: -100, opacity: 0 },
   animate: { x: 0, opacity: 1 },
-  transition: { duration: 2 },
-  ...props,
+  transition: { duration: 1.5, ...props?.transition },
 });
