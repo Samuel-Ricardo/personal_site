59fbcd77ee5e37199cfe8635eb1b12aed41b5489
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-28 17:43:24 -0300

    [ :syringe: ] | update: framer_motion - factory (app::modules:animation)

--- personal_website/src/modules/animations/framer_motion/motion.factory.ts
+++ personal_website/src/modules/animations/framer_motion/motion.factory.ts
@@ -3,6 +3,12 @@ import { ANIMATION_FRAMER_MOTION_REGISTRY } from './motion.registry';
 import { Animator } from '@/@types/animations/motion/animator';
 
 export const ANIMATION_FRAMER_MOTION_FACTORY = {
+  INFINITY: {
+    SLIDE: () =>
+      ANIMATION_FRAMER_MOTION_MODULE.get<Animator>(
+        ANIMATION_FRAMER_MOTION_REGISTRY.INFINITY.SLIDE,
+      ),
+  },
   PROGRESS: () =>
     ANIMATION_FRAMER_MOTION_MODULE.get<Animator>(
       ANIMATION_FRAMER_MOTION_REGISTRY.PROGRESS,
