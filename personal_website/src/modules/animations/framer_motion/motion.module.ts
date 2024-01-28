049e130d5e2feafe78f97cca48bbf1a1974e330e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 23:16:21 -0300

    [ :recycle: ] | refact: move - draw (app::modules)

--- personal_website/src/modules/animations/framer_motion/motion.module.ts
+++ personal_website/src/modules/animations/framer_motion/motion.module.ts
@@ -1,6 +1,6 @@
 import { Container } from 'inversify';
 import { ANIMATION_FRAMER_MOTION_REGISTRY } from './motion.registry';
-import { DRAW_ANIMATION } from './impl/drawn.animation';
+import { DRAW_ANIMATION } from './impl/svg/drawn.animation';
 
 export const ANIMATION_FRAMER_MOTION_MODULE = new Container({
   defaultScope: 'Singleton',
