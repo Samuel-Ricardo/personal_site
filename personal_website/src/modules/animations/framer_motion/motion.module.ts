5a9553595f2a78f3774de0b776efb2f4c34e913c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 23:20:12 -0300

    [ :syringe: ] | update: animation - module (app::modules:framer_motion)

--- personal_website/src/modules/animations/framer_motion/motion.module.ts
+++ personal_website/src/modules/animations/framer_motion/motion.module.ts
@@ -1,6 +1,7 @@
 import { Container } from 'inversify';
 import { ANIMATION_FRAMER_MOTION_REGISTRY } from './motion.registry';
 import { DRAW_ANIMATION } from './impl/svg/drawn.animation';
+import { SLIDE_IN_ANIMATION } from './impl/slideIn.animation';
 
 export const ANIMATION_FRAMER_MOTION_MODULE = new Container({
   defaultScope: 'Singleton',
@@ -9,3 +10,7 @@ export const ANIMATION_FRAMER_MOTION_MODULE = new Container({
 ANIMATION_FRAMER_MOTION_MODULE.bind(
   ANIMATION_FRAMER_MOTION_REGISTRY.SVG.DRAW,
 ).toConstantValue(DRAW_ANIMATION);
+
+ANIMATION_FRAMER_MOTION_MODULE.bind(
+  ANIMATION_FRAMER_MOTION_REGISTRY.SLIDE.IN,
+).toConstantValue(SLIDE_IN_ANIMATION);
