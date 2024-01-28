d5b802d4629e96aa63e7a17ee9f9f426099387c0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-28 18:48:12 -0300

    [ :art: ] | Create: component - carouse > container (app::component)

--- personal_website/src/components/carousel/horizontal/container/container.component.tsx
+++ personal_website/src/components/carousel/horizontal/container/container.component.tsx
@@ -2,16 +2,23 @@ import { IInfinityHorizontalCarouselContainer } from '@/@types/props/carousel/ho
 import { MotionDiv } from '@/components/motion/div.component';
 
 import './container.style.scss';
+import { MODULES } from '@/modules/app.factory';
+import { MotionUL } from '@/components/motion/ul.component';
 
 export const InfinityHorizontalCarouselContainer = (
   props: IInfinityHorizontalCarouselContainer,
 ) => {
+  const animation = MODULES.ANIMATION.FRAMER_MOTION.INFINITY.SLIDE();
+
   return (
     <MotionDiv
       {...props}
       className={`${props.className} infinity-horizontal-carousel`}
     >
-      {props.children}
+      <div className="h-full min-w-[10%] z-20 justify-start bg-gradient-to-r from-primary to-transparent" />
+      <MotionUL {...animation()}>{props.children as any}</MotionUL>
+      <MotionUL {...animation()}>{props.children as any}</MotionUL>
+      <div className="h-full -translate-x-[119.8vw] min-w-[10%] z-20 justify-end bg-gradient-to-l from-primary to-transparent" />
     </MotionDiv>
   );
 };
