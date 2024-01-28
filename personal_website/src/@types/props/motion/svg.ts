01e1e5baf0658ece2db5fe7d0a494687c214305b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-27 21:41:59 -0300

    [ :label: ] | update: type - IMotionSVGProps (@types::props)

--- personal_website/src/@types/props/motion/svg.ts
+++ personal_website/src/@types/props/motion/svg.ts
@@ -1,6 +1,6 @@
 import { SVGMotionProps } from 'framer-motion';
 
 export interface IMotionSVGProps {
-  svg?: SVGMotionProps<SVGSVGElement>;
-  paths?: SVGMotionProps<SVGPathElement>;
+  Svg?: SVGMotionProps<SVGSVGElement>;
+  Paths?: SVGMotionProps<SVGPathElement>;
 }
