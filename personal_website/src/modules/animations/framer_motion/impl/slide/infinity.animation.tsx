4c650203c9c3ea4c886942e17bdf076d2e34e674
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-28 18:09:39 -0300

    [ :clapper: ] | create: animation - infinity > slide (app::modules:framer_motion)

--- personal_website/src/modules/animations/framer_motion/impl/slide/infinity.animation.tsx
+++ personal_website/src/modules/animations/framer_motion/impl/slide/infinity.animation.tsx
@@ -2,7 +2,12 @@ import { Animator } from '@/@types/animations/motion/animator';
 
 export const INFINITY_SLIDE_ANIMATION: Animator = props => ({
   initial: { translateX: 0 },
-  animate: { translateX: '100%' },
+  animate: { translateX: '-100%' },
   ...props,
-  transition: { duration: 5, ...props?.transition },
+  transition: {
+    duration: 15,
+    repeat: Infinity,
+    ease: 'linear',
+    ...props?.transition,
+  },
 });
