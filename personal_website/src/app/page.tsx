19d150199ea6ed12c7fdb0cd66eb3ca3c29c161d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-28 23:03:22 -0300

    [ :book: ] | update: page - home > work with (app)

--- personal_website/src/app/page.tsx
+++ personal_website/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from './sections/hero/hero.section';
+import { WorkWithSection } from './sections/work_with/works.section';
 
 export default function Home() {
   return (
     <div className="flex flex-col w-screen h-screen bg-primary">
       <Hero />
+      <WorkWithSection />
     </div>
   );
 }
