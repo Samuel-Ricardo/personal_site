61ae860811f0148a670399ee6b30529d3e7a864b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-28 23:03:59 -0300

    [ :iphone: ] | responsive: circuit (app::components)

--- personal_website/src/app/sections/hero/hero.section.tsx
+++ personal_website/src/app/sections/hero/hero.section.tsx
@@ -5,7 +5,7 @@ import './hero.style.scss';
 export const Hero = () => {
   return (
     <section id="hero">
-      <Circuit className="absolute top-0 left-0 right-0 h-[75vh] w-full bg-primary-foreground z-0" />
+      <Circuit className="absolute top-0 left-0 right-0 h-screen w-full bg-primary-foreground z-0" />
       <HeroCard />
     </section>
   );
