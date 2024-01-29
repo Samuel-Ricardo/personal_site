c6e8cf7ab08735c45a9912c391048efcbaf352ba
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-29 09:23:18 -0300

    [ :book: ] | update: page - home > about-me (app)

--- personal_website/src/app/page.tsx
+++ personal_website/src/app/page.tsx
@@ -1,11 +1,13 @@
+import { AboutMe } from './sections/about/about.section';
 import { Hero } from './sections/hero/hero.section';
 import { WorkWithSection } from './sections/work_with/works.section';
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-screen h-screen bg-primary">
+    <div id="home-page" className="flex flex-col overflow-x-hidden">
       <Hero />
       <WorkWithSection />
+      <AboutMe />
     </div>
   );
 }
