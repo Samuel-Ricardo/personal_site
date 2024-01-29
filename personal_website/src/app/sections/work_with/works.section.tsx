d1cbe9ad790cc3582afd6d7749571635daf20dc5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-28 23:04:23 -0300

    [ :art: ] | update: section - i work with > cards (app)

--- personal_website/src/app/sections/work_with/works.section.tsx
+++ personal_website/src/app/sections/work_with/works.section.tsx
@@ -20,12 +20,44 @@ export const WorkWithSection = () => {
         >
           Building, sharing and contributing to open source.
         </IWorkWithCard>
+
+        <IWorkWithCard
+          title="Freelancer"
+          image="https://th.bing.com/th/id/OIG2.S1ygsRnG1jar4RwHFbgG?pid=ImgGn"
+        >
+          The Best Solutions for your bussines
+        </IWorkWithCard>
         <IWorkWithCard
           image="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg"
           title="Frontend"
         >
           {' '}
           Building websites with the best technologies.
+        </IWorkWithCard>
+        <IWorkWithCard
+          title="Backend"
+          image="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg"
+        >
+          Scalable API&#39;s with healthy architecture.
+        </IWorkWithCard>
+        <IWorkWithCard
+          title="Devops"
+          image="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
+        >
+          Containerization and CI/CD.
+        </IWorkWithCard>
+        <IWorkWithCard
+          title="Microsservice"
+          image="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-plain.svg"
+        >
+          Building scalable microservices.
+        </IWorkWithCard>
+
+        <IWorkWithCard
+          title="Mobile"
+          image="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg"
+        >
+          Building responsive and fast mobile applications.
         </IWorkWithCard>
       </InfinityHorizontalCarousel>
     </section>
