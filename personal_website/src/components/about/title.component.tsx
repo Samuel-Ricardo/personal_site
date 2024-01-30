ac543bfadf9a355e03e00370b8921bb589afb1cf
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-29 21:27:52 -0300

    [ :art: ] | create: component - title > about (app)

---
+++ personal_website/src/components/about/title.component.tsx
@@ -0,0 +1,7 @@
+import { MODULES } from '@/modules/app.factory';
+
+export const AboutMeTitle = () => (
+  <h1 className={`${MODULES.FONTS.MONO.INCONSOLATA().className} title`}>
+    BRING THE POWER OF <span>CREATIVITY</span> TO THE WORLD
+  </h1>
+);
