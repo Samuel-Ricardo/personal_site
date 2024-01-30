fa2256c8579da6e1249eac7dd96bda1558a29f11
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-29 22:30:52 -0300

    [ :bookmark_tabs: ] | update: section - about-me (app)

--- personal_website/src/app/sections/about/about.section.tsx
+++ personal_website/src/app/sections/about/about.section.tsx
@@ -1,25 +1,12 @@
-import Image from 'next/image';
-
 import './about.style.scss';
-import { MODULES } from '@/modules/app.factory';
+import { AboutMeTitle } from '@/components/about/title/title.component';
+import { AboutMeContent } from '@/components/about/content/content.component';
 
 export const AboutMe = () => {
   return (
     <section id="about-me" className="flex flex-col w-screen h-screen ">
-      <h1 className={`${MODULES.FONTS.MONO.INCONSOLATA().className}`}>
-        BRING THE POWER OF <span>CREATIVITY</span> TO YOUR BUSINESS
-      </h1>
-      <div className="content-container">
-        <div className="image-container">
-          <Image
-            src="/image-2.png"
-            alt="about me image"
-            width={1920}
-            height={1080}
-          />
-        </div>
-        <div className="text-container"></div>
-      </div>
+      <AboutMeTitle />
+      <AboutMeContent />
     </section>
   );
 };
