1e6e6f77b08f257d8f7ba2b960111c96a6bedc59
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-02 17:46:37 -0300

    [ :art: ] | update: techs - galaxy > title (app::component)

--- personal_website/src/components/section/techs/galaxy/galaxy.component.tsx
+++ personal_website/src/components/section/techs/galaxy/galaxy.component.tsx
@@ -6,6 +6,7 @@ import { TechsGalaxyItem } from './item/item.component';
 export const TechsGalaxy = ({ items }: ITechGalaxyProps) => {
   return (
     <div id="techs-galaxy" className="frosted-glass-lg">
+      <h1 id="techs-galaxy-title">Frontend</h1>
       <ul className="items-container">
         {items?.map(item => <TechsGalaxyItem key={item?.icon} {...item} />)}
       </ul>
