1086a666aa23999767260de65bb0a0995e1bf085
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-02 15:04:44 -0300

    [ :art: ] | create: techs - galaxy > item (app::components)

---
+++ personal_website/src/components/section/techs/galaxy/item/item.component.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import { TechPreview } from './preview/preview.component';
+import { FrostedGlassCard } from '@/components/card/glass.card';
+import { ITechGalaxyItemProps } from '@/@types/props/section/techs/item';
+
+export const TechsGalaxyItems = ({ icon }: ITechGalaxyItemProps) => {
+  return (
+    <li id="tech-item">
+      <FrostedGlassCard id="tech-item-img-container">
+        <Image src={icon} alt="tech icon" width={80} height={80} />
+      </FrostedGlassCard>
+
+      <TechPreview />
+    </li>
+  );
+};
