8c7b11741fb6ea0d2e9e88b177bd04135f4d0c65
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-02 15:19:02 -0300

    [ :art: ] | update: techs - galaxy (app::component)

--- personal_website/src/components/section/techs/galaxy/galaxy.component.tsx
+++ personal_website/src/components/section/techs/galaxy/galaxy.component.tsx
@@ -1,12 +1,13 @@
 import { ITechGalaxyProps } from '@/@types/props/section/techs/galaxy';
 
 import './galaxy.style.scss';
+import { TechsGalaxyItem } from './item/item.component';
 
-export const TechsGalaxy = ({ children }: ITechGalaxyProps) => {
+export const TechsGalaxy = ({ items }: ITechGalaxyProps) => {
   return (
     <div id="techs-galaxy" className="frosted-glass-lg">
       <ul className="items-container">
-        {children?.map(c => <li key={c?.toString()}>{c}</li>)}
+        {items?.map(item => <TechsGalaxyItem key={item?.icon} {...item} />)}
       </ul>
     </div>
   );
