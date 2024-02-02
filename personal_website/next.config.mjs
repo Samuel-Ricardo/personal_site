da44fd20a6f508f3921c8ba576ea763e59a032bb
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-02 16:03:00 -0300

    [ :gear: ] | update: config - image > domains (next)

--- personal_website/next.config.mjs
+++ personal_website/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['th.bing.com'],
+    domains: ['th.bing.com', 'img.freepik.com'],
   },
   sassOptions: {
     includePaths: ['styles'],
