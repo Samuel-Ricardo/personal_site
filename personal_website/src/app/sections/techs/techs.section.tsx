2401611f4ab02491b6200246346881600df5be2d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-02 14:35:47 -0300

    [ :bookmark_tabs: ] | update: section - techs > titles (app)

--- personal_website/src/app/sections/techs/techs.section.tsx
+++ personal_website/src/app/sections/techs/techs.section.tsx
@@ -1,5 +1,10 @@
 import './techs.style.scss';
 
 export const Techs = () => {
-  return <section id="techs-section">HI</section>;
+  return (
+    <section id="techs-section">
+      <h1 id="techs-title">A UNIVERSE OF POSSIBILITIES</h1>
+      <h2 id="techs-subtitle">THE BEST SOLUTIONS FOR EACH CHALLENGE</h2>
+    </section>
+  );
 };
