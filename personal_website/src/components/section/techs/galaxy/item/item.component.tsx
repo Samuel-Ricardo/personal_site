9fc8a01d2b8f2cb3ea4b2da6c3fdb3eba943892c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-02 17:46:55 -0300

    [ :art: ] | update: techs - galaxy > item (app::component)

--- personal_website/src/components/section/techs/galaxy/item/item.component.tsx
+++ personal_website/src/components/section/techs/galaxy/item/item.component.tsx
@@ -1,16 +1,15 @@
 import Image from 'next/image';
 import { TechPreview } from './preview/preview.component';
-import { FrostedGlassCard } from '@/components/card/glass.card';
 import { ITechGalaxyItemProps } from '@/@types/props/section/techs/item';
 
 import './item.style.scss';
 
 export const TechsGalaxyItem = ({ icon }: ITechGalaxyItemProps) => {
   return (
-    <li id="tech-item">
-      <FrostedGlassCard id="tech-item-img-container">
+    <li id="tech-item" className="frosted-glass-lg">
+      <div id="tech-item-img-container">
         <Image src={icon} alt="tech icon" width={80} height={80} />
-      </FrostedGlassCard>
+      </div>
 
       <TechPreview />
     </li>
