068618cf59ec8313dfee7c8478bcb8d804f1d52e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-04 14:26:20 -0300

    [ :gear: ] | update: image - domain > miro.medium.com (next::config)

--- personal_website/next.config.mjs
+++ personal_website/next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['th.bing.com', 'img.freepik.com', 'camo.githubusercontent.com'],
+    domains: [
+      'th.bing.com',
+      'img.freepik.com',
+      'camo.githubusercontent.com',
+      'miro.medium.com',
+    ],
   },
   sassOptions: {
     includePaths: ['styles'],
