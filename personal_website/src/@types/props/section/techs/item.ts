fd9ea014ca20a17fdbb3812a761f7b0257e8c0b7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-03 23:46:13 -0300

    [ :label: ] | update: type - ITechGalaxyItemProps (@types::props)

--- personal_website/src/@types/props/section/techs/item.ts
+++ personal_website/src/@types/props/section/techs/item.ts
@@ -1,5 +1,7 @@
 import { HTMLAttributes } from 'react';
+import { IPreview } from './preview';
 
 export interface ITechGalaxyItemProps extends HTMLAttributes<HTMLLIElement> {
   icon: string;
+  preview: IPreview;
 }
