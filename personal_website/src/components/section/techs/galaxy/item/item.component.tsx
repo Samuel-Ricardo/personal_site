ba9a66af041980204293d0ae10115ac01d6d99db
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-03 23:41:32 -0300

    [ :art: ] | update: techs - galaxy > item (app::component)

--- personal_website/src/components/section/techs/galaxy/item/item.component.tsx
+++ personal_website/src/components/section/techs/galaxy/item/item.component.tsx
@@ -3,15 +3,18 @@ import { TechPreview } from './preview/preview.component';
 import { ITechGalaxyItemProps } from '@/@types/props/section/techs/item';
 
 import './item.style.scss';
+import { MotionLI } from '@/components/motion/li.component';
+import { MODULES } from '@/modules/app.factory';
 
-export const TechsGalaxyItem = ({ icon }: ITechGalaxyItemProps) => {
+export const TechsGalaxyItem = ({ icon, preview }: ITechGalaxyItemProps) => {
+  const animation = MODULES.ANIMATION.FRAMER_MOTION.HOVER.MOUNT()();
   return (
-    <li id="tech-item" className="frosted-glass-lg">
+    <MotionLI id="tech-item" className="frosted-glass-lg" {...animation.parent}>
       <div id="tech-item-img-container">
         <Image src={icon} alt="tech icon" width={80} height={80} />
       </div>
 
-      <TechPreview />
-    </li>
+      <TechPreview preview={preview} {...animation.children} />
+    </MotionLI>
   );
 };
