ba5910cda927d4c386d272f45671ed1981096e85
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-03 23:34:10 -0300

    [ :clapper: ] | update: animation - mount on hover (app::modules:framer_motion)

--- personal_website/src/components/section/techs/galaxy/item/preview/preview.component.tsx
+++ personal_website/src/components/section/techs/galaxy/item/preview/preview.component.tsx
@@ -1,3 +1,28 @@
-export const TechPreview = () => {
-  return <div id="tech-item-preview"></div>;
+'use client';
+
+import { IPreviewProps } from '@/@types/props/section/techs/preview';
+import { MotionDiv } from '@/components/motion/div.component';
+import Image from 'next/image';
+
+export const TechPreview = (props: IPreviewProps) => {
+  return (
+    <MotionDiv
+      id="tech-item-preview"
+      {...props}
+      className="absolute -top-12 left-20 z-10"
+    >
+      <h4>{props.preview.name}</h4>
+
+      <p>{props.preview.description}</p>
+
+      {/*props.preview.images.map((image, index) => */}
+
+      <Image
+        src={props.preview.images[0]}
+        alt="tech preview image"
+        width={250}
+        height={250}
+      />
+    </MotionDiv>
+  );
 };
--- personal_website/src/modules/animations/framer_motion/impl/hover/mount.animation.tsx
+++ personal_website/src/modules/animations/framer_motion/impl/hover/mount.animation.tsx
@@ -7,18 +7,28 @@ export const MOUNT_ON_HOVER: HierarchicalAnimator = props => {
       whileTap: {
         scale: 0.9,
       },
+      variants: {
+        'parent-hover': {
+          scale: 1.1,
+        },
+      },
     },
     children: {
       initial: {
         display: 'none',
         filter: 'blur(8px)',
+        opacity: 0,
+      },
+      transition: {
+        duration: 0.5,
+        type: 'tween',
+        ease: 'easeInOut',
       },
       variants: {
         'parent-hover': {
           display: 'flex',
-          width: 'fit-content',
-          height: 'fit-content',
           filter: 'blur(0px)',
+          opacity: 1,
         },
       },
     },
