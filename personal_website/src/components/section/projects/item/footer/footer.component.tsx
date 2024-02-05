cdb07bb6e77aa3d872f1e5b7668e1ee06366fb72
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-05 11:57:07 -0300

    [ :art: ] | update: project - item > footer (app::components)

--- personal_website/src/components/section/projects/item/footer/footer.component.tsx
+++ personal_website/src/components/section/projects/item/footer/footer.component.tsx
@@ -2,9 +2,9 @@ import { IProjectItemFooterProps } from '@/@types/props/section/project/item';
 
 export const ProjectItemFooter = ({ repo, demo }: IProjectItemFooterProps) => {
   return (
-    <div>
-      <button>{repo}</button>
-      <button>{demo}</button>
+    <div className="footer">
+      <button className="btn-repo">{repo}</button>
+      <button className="btn-demo">{demo}</button>
     </div>
   );
 };
