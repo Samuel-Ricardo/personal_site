1cd0ee213d8667c12cb8587bc353ef5bb756c6e5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-05 18:24:39 -0300

    [ :art: ] | update: project - item > footer (app::component)

--- personal_website/src/components/section/projects/item/footer/footer.component.tsx
+++ personal_website/src/components/section/projects/item/footer/footer.component.tsx
@@ -3,16 +3,25 @@ import { NavigateButton } from '@/components/button/navigate/button.component';
 
 import './footer.style.scss';
 
-export const ProjectItemFooter = ({ repo, demo }: IProjectItemFooterProps) => {
+export const ProjectItemFooter = ({
+  repo,
+  demo,
+  about,
+}: IProjectItemFooterProps) => {
   return (
     <div className="footer">
-      <NavigateButton link={{ href: repo, target: '_blank' }}>
-        Code
-      </NavigateButton>
+      {repo && (
+        <NavigateButton link={{ href: repo, target: '_blank' }}>
+          Code
+        </NavigateButton>
+      )}
       {demo && (
         <NavigateButton link={{ href: demo, target: '_blank' }}>
           Demo
         </NavigateButton>
+      )}
+      {about && (
+        <NavigateButton link={{ href: 'linkedin' }}>About</NavigateButton>
       )}
     </div>
   );
