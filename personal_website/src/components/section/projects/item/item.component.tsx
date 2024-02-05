599857d8dfbc41e937c78eab544f860a3d3f721e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-05 10:19:02 -0300

    [ :art: ] | update: project - item (app::components)

--- personal_website/src/components/section/projects/item/item.component.tsx
+++ personal_website/src/components/section/projects/item/item.component.tsx
@@ -1,18 +1,20 @@
 import { IProjectItemProps } from '@/@types/props/section/project/item';
 import { ProjectItemPreview } from './preview/preview.component';
 import { ProjectItemContent } from './content/content.component';
-import { ProjectItemTechs } from './techs/techs.component';
-import { ProjectItemFooter } from './footer/footer.component';
 
 import './item.style.scss';
 
 export const ProjectItem = (props: IProjectItemProps) => {
   return (
-    <div {...props} className="project-item">
+    <li {...props} className="project-item">
       <ProjectItemPreview image={props.image} />
-      <ProjectItemContent title={props.title} description={props.description} />
-      <ProjectItemTechs main_techs={props.main_techs} />
-      <ProjectItemFooter repo={props.repo} demo={props.demo} />
-    </div>
+      <ProjectItemContent
+        title={props.title}
+        description={props.description}
+        main_techs={props.main_techs}
+        repo={props.repo}
+        demo={props.demo}
+      />
+    </li>
   );
 };
