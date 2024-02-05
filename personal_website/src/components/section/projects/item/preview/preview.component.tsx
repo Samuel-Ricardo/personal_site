180ac84950b9c7b6acd166c9f1e17e610ea0dff5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-05 10:39:16 -0300

    [ :art: ] | update: project - item > preview (app::component)

--- personal_website/src/components/section/projects/item/preview/preview.component.tsx
+++ personal_website/src/components/section/projects/item/preview/preview.component.tsx
@@ -1,8 +1,16 @@
 import { IProjectItemPreviewProps } from '@/@types/props/section/project/item';
 import Image from 'next/image';
 
+import './preview.style.scss';
+
 export const ProjectItemPreview = ({ image }: IProjectItemPreviewProps) => (
-  <div className="frosted-glass">
-    <Image src={image} alt="project item preview" width={500} height={500} />
+  <div className="frosted-glass image-container">
+    <Image
+      src={image}
+      alt="project item preview"
+      width={700}
+      height={700}
+      quality={100}
+    />
   </div>
 );
