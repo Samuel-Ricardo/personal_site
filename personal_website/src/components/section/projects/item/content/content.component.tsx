963a46a2900a22ebd7d0e6a6cd28a6731ae87050
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-05 10:39:33 -0300

    [ :art: ] | update: project - item > content (app::component)

--- personal_website/src/components/section/projects/item/content/content.component.tsx
+++ personal_website/src/components/section/projects/item/content/content.component.tsx
@@ -1,11 +1,19 @@
 import { IProjectItemContentProps } from '@/@types/props/section/project/item';
+import { ProjectItemTechs } from '../techs/techs.component';
+import { ProjectItemFooter } from '../footer/footer.component';
 
 export const ProjectItemContent = ({
   title,
   description,
+  demo,
+  repo,
+  main_techs,
 }: IProjectItemContentProps) => (
   <div>
     <h2>{title}</h2>
     <p>{description}</p>
+
+    <ProjectItemTechs main_techs={main_techs} />
+    <ProjectItemFooter repo={repo} demo={demo} />
   </div>
 );
