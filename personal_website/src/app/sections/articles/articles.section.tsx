db4a3f6c31e34f718b5f1c4a8a6281713ef17581
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-09 18:19:10 -0300

    [ :art: ] | create: section - articles (app::component)

---
+++ personal_website/src/app/sections/articles/articles.section.tsx
@@ -0,0 +1,11 @@
+import { ArticlesBackground } from '@/images/background/articles/articles.background';
+import './articles.style.scss';
+
+export const Articles = () => {
+  return (
+    <section id="articles-section">
+      <ArticlesBackground />
+      <div className="z-0"> HI :D </div>
+    </section>
+  );
+};
