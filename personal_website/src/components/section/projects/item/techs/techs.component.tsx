5d9b74c6281ad37846ce8795d71975debadd79e7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-09 17:13:46 -0300

    [ :art: ] | create: project - item > techs (app::component)

--- personal_website/src/components/section/projects/item/techs/techs.component.tsx
+++ personal_website/src/components/section/projects/item/techs/techs.component.tsx
@@ -1,5 +1,14 @@
 import { IProjectItemTechsProps } from '@/@types/props/section/project/item';
+import { TechsGalaxy } from '@/components/section/techs/galaxy/galaxy.component';
+
+import './techs.style.scss';
 
 export const ProjectItemTechs = ({ main_techs }: IProjectItemTechsProps) => {
-  return <div>{main_techs + ''}</div>;
+  return (
+    <TechsGalaxy
+      items={main_techs}
+      title=""
+      className="flex flex-wrap w-full h-fit"
+    />
+  );
 };
