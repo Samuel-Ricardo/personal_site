8cf521c09570073b4ad45918855977114b74d017
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-10 12:54:10 -0300

    [ :label: ] | create: type - articles > item (@types::props)

--- personal_website/src/@types/props/section/articles/item.ts
+++ personal_website/src/@types/props/section/articles/item.ts
@@ -1,8 +1,16 @@
 import { IPlatform } from '@/@types/platform';
+import { HTMLAttributes } from 'react';
 
 export interface IArticle {
   title: string;
+  scientific?: boolean;
   description: string;
   platforms: IPlatform[];
   image?: string;
+}
+
+export interface IArticleItemProps {
+  html?: HTMLAttributes<HTMLLIElement>;
+  key?: string;
+  article: IArticle;
 }
