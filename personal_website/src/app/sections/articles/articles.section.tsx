ca80c624d69741af0d3f4c8359b2716280fe47d9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-10 10:05:37 -0300

    [ :art: ] | create: section - articles > title (app::component)

--- personal_website/src/app/sections/articles/articles.section.tsx
+++ personal_website/src/app/sections/articles/articles.section.tsx
@@ -1,11 +1,18 @@
 import { ArticlesBackground } from '@/images/background/articles/articles.background';
 import './articles.style.scss';
+import { MODULES } from '@/modules/app.factory';
 
 export const Articles = () => {
   return (
     <section id="articles-section">
       <ArticlesBackground />
-      <div className="z-0"> HI :D </div>
+      <div className="z-0 flex flex-1">
+        <h1
+          className={`${MODULES.FONTS.CLASSIC.LIBRE_BASKERVILLE().className} section-title`}
+        >
+          Articles that i Wrotte
+        </h1>
+      </div>
     </section>
   );
 };
