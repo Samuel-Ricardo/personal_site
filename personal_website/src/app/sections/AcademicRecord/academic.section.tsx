ea1d3224ac9045396f50b3c59dbae1836bff6d45
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-11 16:13:58 -0300

    [ :art: ] | create: section - academic redord (app::style)

---
+++ personal_website/src/app/sections/AcademicRecord/academic.section.tsx
@@ -0,0 +1,34 @@
+import { ExperienceContainer } from '@/components/section/experience/container/container.component';
+import './experience.style.scss';
+
+export const AcademicRecord = () => {
+  return (
+    <section id="academic-section">
+      <h1>Experience</h1>
+      <ExperienceContainer
+        itens={[
+          {
+            title: 'Open Source Dev',
+            company: {
+              name: 'Github',
+              icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
+            },
+            description: '123456789123456789123456789123456789123456789',
+            start: new Date(),
+            left: true,
+          },
+          {
+            title: 'Open Source Dev',
+            company: {
+              name: 'Github',
+              icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
+            },
+            description: '123456789123456789123456789123456789123456789',
+            start: new Date(),
+            finish: new Date(),
+          },
+        ]}
+      />
+    </section>
+  );
+};
