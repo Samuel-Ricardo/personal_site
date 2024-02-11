ce0a77440b8246f8346c1e95477bf0578eae263c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-11 17:05:56 -0300

    [ :art: ] | create: section - testimonials (app::component)

---
+++ personal_website/src/app/sections/testimonials/testimonials.section.tsx
@@ -0,0 +1,9 @@
+import './testimonials.style.scss';
+
+export const Testimonials = () => {
+  return (
+    <section id="testimonials-section">
+      <h1>Testimonials</h1>
+    </section>
+  );
+};
