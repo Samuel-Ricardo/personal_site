d1b164679c66986f9279445820d1135bd5a7fa3f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-10 23:19:14 -0300

    [ :label: ] | create: type - platform (@types::props)

--- personal_website/src/@types/platform.ts
+++ personal_website/src/@types/platform.ts
@@ -1,5 +1,12 @@
+import { HTMLAttributes } from 'react';
+
 export interface IPlatform {
   icon?: string;
   name: string;
   url: string;
+}
+
+export interface IPlatformProps {
+  platform: IPlatform;
+  html?: HTMLAttributes<HTMLDivElement>;
 }
