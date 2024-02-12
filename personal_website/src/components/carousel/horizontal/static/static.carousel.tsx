f806c043ef116c919b8c7ac5244f869578c171f9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-12 16:29:14 -0300

    [ :art: ] | create: carousel - static > arrows (app::component)

--- personal_website/src/components/carousel/horizontal/static/static.carousel.tsx
+++ personal_website/src/components/carousel/horizontal/static/static.carousel.tsx
@@ -1,15 +1,27 @@
 import { IStaticCarouselProps } from '@/@types/props/carousel/horizontal/static/carousel';
 
 import './static.style.scss';
+import { RightArrow } from '@/icons/arrow/right/arrow.icon';
+import { LeftArrow } from '@/icons/arrow/left/arrow.icon';
 
 export const StaticCarousel = ({ children }: IStaticCarouselProps) => {
   return (
-    <ul className="static-carousel">
-      {children.map(child => (
-        <li key={child?.toString()} className="h-fit w-fit">
-          {child}
-        </li>
-      ))}
-    </ul>
+    <div className="static-carousel">
+      <button className="left-button">
+        <LeftArrow />
+      </button>
+
+      <ul className="static-carousel-container">
+        {children.map(child => (
+          <li key={child?.toString()} className="h-fit w-fit">
+            {child}
+          </li>
+        ))}
+      </ul>
+
+      <button className="right-button">
+        <RightArrow />
+      </button>
+    </div>
   );
 };
