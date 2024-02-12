ea55c01dcf51fa928b034100f65b0c841edfd291
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-12 13:49:57 -0300

    [ :art: ] | update: section - testimonials (app::style)

--- personal_website/src/app/sections/testimonials/testimonials.section.tsx
+++ personal_website/src/app/sections/testimonials/testimonials.section.tsx
@@ -1,9 +1,21 @@
+import { TestimonialsBackground } from '@/images/background/testimonials/testimonials.background';
 import './testimonials.style.scss';
+import { StaticCarousel } from '@/components/carousel/horizontal/static/static.carousel';
+import { SocialCard } from '@/components/card/social/social.card';
 
 export const Testimonials = () => {
   return (
     <section id="testimonials-section">
-      <h1>Testimonials</h1>
+      <TestimonialsBackground />
+      <div className="content">
+        <h1>Testimonials</h1>
+
+        <StaticCarousel>
+          <SocialCard />
+          <SocialCard />
+          <SocialCard />
+        </StaticCarousel>
+      </div>
     </section>
   );
 };
