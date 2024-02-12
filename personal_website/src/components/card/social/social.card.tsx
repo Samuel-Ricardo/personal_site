9f10def057baa41d0f98adc398f799ae65a30456
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-12 13:53:46 -0300

    [ :art: ] | update: card - social (app::component)

--- personal_website/src/components/card/social/social.card.tsx
+++ personal_website/src/components/card/social/social.card.tsx
@@ -1,18 +1,20 @@
 import Image from 'next/image';
 import './social.style.scss';
 import { SocialCardCover } from './cover/cover.card';
+import { SocialCardFooter } from './footer/footer.component';
 
 export const SocialCard = () => {
   return (
-    <div className="social-card">
+    <div className="social-card selected">
       <Image
         src="/hero_avatar.jpg"
         alt="Github"
-        width={30}
-        height={30}
+        width={800}
+        height={800}
         className="avatar"
       />
       <SocialCardCover />
+      <SocialCardFooter />
     </div>
   );
 };
