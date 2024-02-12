8942abebe39d1c09d0c555bf17f0655c3cd130d7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-12 14:30:59 -0300

    [ :art: ] | update: card - social > footer (app::component)

--- personal_website/src/components/card/social/footer/footer.component.tsx
+++ personal_website/src/components/card/social/footer/footer.component.tsx
@@ -2,7 +2,7 @@ import './footer.style.scss';
 
 export const SocialCardFooter = () => {
   return (
-    <footer>
+    <footer className="card-footer">
       <p className="name"> SAMUEL RICARDO</p>
       <p className="role">| Full Stack • Github</p>
     </footer>
