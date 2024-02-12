82d6b67ece472e03f6863c9520290f04f08f1aaf
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-12 17:04:15 -0300

    [ :art: ] | update: card - testimony > header (app::component)

---
+++ personal_website/src/components/card/testimony/header/header.component.tsx
@@ -0,0 +1,9 @@
+export const TestimonyHeader = () => {
+  return (
+    <header className="testimony-header">
+      <h1>Samuel Ricard</h1>
+      <div className="line" />
+      <p>Full Stack • Github</p>
+    </header>
+  );
+};
