f21a74da3a0662ace4066dbfea5bf0da4c10e679
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-13 10:46:36 -0300

    [ :art: ] | updated: card - testimony (app::component)

--- personal_website/src/components/card/testimony/testimony.card.tsx
+++ personal_website/src/components/card/testimony/testimony.card.tsx
@@ -3,13 +3,17 @@ import { TestimonyContent } from './content/content.component';
 import { TestimonyFooter } from './footer/footer.component';
 import { TestimonyHeader } from './header/header.component';
 
+import './testimony.style.scss';
+
 export const TestimonyCard = () => {
   return (
     <div className="testimony-card">
       <TestimonyAvatar />
-      <TestimonyHeader />
-      <TestimonyContent />
-      <TestimonyFooter />
+      <div className="content-block">
+        <TestimonyHeader />
+        <TestimonyContent />
+        <TestimonyFooter />
+      </div>
     </div>
   );
 };
