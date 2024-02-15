630772db23649d9600e02b37fe0372a04fc5867f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-15 10:31:20 -0300

    [ :art: ] | update: card - testimony > footer (app::component)

--- personal_website/src/components/card/testimony/footer/footer.component.tsx
+++ personal_website/src/components/card/testimony/footer/footer.component.tsx
@@ -1,13 +1,17 @@
+import './footer.style.scss';
+
 import { NavigateButton } from '@/components/button/navigate/button.component';
 import { GithubLogo } from '@/icons/logo/github/icon.component';
 
 export const TestimonyFooter = () => {
   return (
     <footer className="testimony-footer">
-      <button>
-        <GithubLogo />
-      </button>
-      <NavigateButton link={{ href: './' }}>Portfolio</NavigateButton>
+      <div className="wrapper">
+        <NavigateButton link={{ href: './' }}>Portfolio</NavigateButton>
+        <button>
+          <GithubLogo />
+        </button>
+      </div>
     </footer>
   );
 };
