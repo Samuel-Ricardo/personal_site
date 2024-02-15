420d654291471474a07d30e6eddf677c74edb653
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-15 10:47:40 -0300

    [ :recycle: ] | refact: card - i work with => simple card

---
+++ personal_website/src/@types/props/card/simple.ts
@@ -0,0 +1,4 @@
+export interface ISimpleCardProps extends React.PropsWithChildren {
+  title: string;
+  image: string;
+}
--- personal_website/src/@types/props/card/work_with.ts
+++
@@ -1,4 +0,0 @@
-export interface IIWorkWithProps extends React.PropsWithChildren {
-  title: string;
-  image: string;
-}
