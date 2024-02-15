92ef37797b15c3f751fac90128c0679f77702d46
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-15 10:46:43 -0300

    [ :recycle: ] | refact: card - i work with

--- personal_website/src/app/sections/home/work_with/works.section.tsx
+++ personal_website/src/app/sections/home/work_with/works.section.tsx
@@ -1,7 +1,7 @@
 import { InfinityHorizontalCarousel } from '@/components/carousel/horizontal/infity.component';
 
 import './works.style.scss';
-import { IWorkWithCard } from '@/components/card/work_with/card.component';
+import { SimpleCard } from '@/components/card/simple/card.component';
 import { MODULES } from '@/modules/app.factory';
 import { FrostedGlassCard } from '@/components/card/glass.card';
 
@@ -14,51 +14,51 @@ export const WorkWithSection = () => {
         <h1 className="title">I Work With</h1>
       </FrostedGlassCard>
       <InfinityHorizontalCarousel>
-        <IWorkWithCard
+        <SimpleCard
           image="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
           title="Open Source"
         >
           Building, sharing and contributing to open source.
-        </IWorkWithCard>
+        </SimpleCard>
 
-        <IWorkWithCard
+        <SimpleCard
           title="Freelancer"
           image="https://th.bing.com/th/id/OIG2.S1ygsRnG1jar4RwHFbgG?pid=ImgGn"
         >
           The Best Solutions for your bussines
-        </IWorkWithCard>
-        <IWorkWithCard
+        </SimpleCard>
+        <SimpleCard
           image="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg"
           title="Frontend"
         >
           {' '}
           Building websites with the best technologies.
-        </IWorkWithCard>
-        <IWorkWithCard
+        </SimpleCard>
+        <SimpleCard
           title="Backend"
           image="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg"
         >
           Scalable API&#39;s with healthy architecture.
-        </IWorkWithCard>
-        <IWorkWithCard
+        </SimpleCard>
+        <SimpleCard
           title="Devops"
           image="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
         >
           Containerization and CI/CD.
-        </IWorkWithCard>
-        <IWorkWithCard
+        </SimpleCard>
+        <SimpleCard
           title="Microsservice"
           image="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-plain.svg"
         >
           Building scalable microservices.
-        </IWorkWithCard>
+        </SimpleCard>
 
-        <IWorkWithCard
+        <SimpleCard
           title="Mobile"
           image="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg"
         >
           Building responsive and fast mobile applications.
-        </IWorkWithCard>
+        </SimpleCard>
       </InfinityHorizontalCarousel>
     </section>
   );
