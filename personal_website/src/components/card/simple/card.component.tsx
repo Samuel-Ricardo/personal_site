7c851226573d27d9b73bf0b0da7cbf25ed241a34
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-15 11:34:58 -0300

    [ :recycle: ] | refact: card - simple (app::component)

--- personal_website/src/components/card/simple/card.component.tsx
+++ personal_website/src/components/card/simple/card.component.tsx
@@ -2,19 +2,14 @@ import { ISimpleCardProps } from '@/@types/props/card/simple';
 import { FrostedGlassCard } from '../glass.card';
 
 import './card.style.scss';
-import { HorizontalLine } from '@/components/line/horizontal/horizontal.component';
 import { SimpleCardImage } from './image/image.component';
+import { SimpleCardContent } from './content/content.component';
 
 export const SimpleCard = ({ children, image, title }: ISimpleCardProps) => {
   return (
     <FrostedGlassCard>
       <SimpleCardImage image={image} />
-
-      <div className="content-container">
-        <h1 className="card-title">{title}</h1>
-        <HorizontalLine />
-        <p className="content">{children}</p>
-      </div>
+      <SimpleCardContent title={title}>{children}</SimpleCardContent>
     </FrostedGlassCard>
   );
 };
