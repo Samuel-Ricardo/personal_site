541bf86b61e7f306fe2f45a3fb912ecf2ae2662b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-16 15:18:59 -0300

    [ :triangular_ruler: ] | update: schemas - sanity  (sanity)

--- personal_website/sanity/schema.ts
+++ personal_website/sanity/schema.ts
@@ -2,9 +2,17 @@ import { type SchemaTypeDefinition } from 'sanity';
 import { PageSchema } from './schemas/page.schema';
 import { LangSchema } from './schemas/lang.schema';
 import { OccupationsSchema } from './schemas/sections/home/hero/occupations.schema';
-import { TextSchema } from './schemas/text.schema';
+import { TextSchema } from './schemas/typography/text.schema';
 import { PersonSchema } from './schemas/person.schema';
+import { TitleSchema } from './schemas/typography/title.schema';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [PageSchema, LangSchema, OccupationsSchema, TextSchema, PersonSchema],
+  types: [
+    PageSchema,
+    LangSchema,
+    OccupationsSchema,
+    TextSchema,
+    TitleSchema,
+    PersonSchema,
+  ],
 };
