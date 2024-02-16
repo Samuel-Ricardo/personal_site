2d1e378d9f7d4ee5fe60a57234b62d667695f290
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-16 16:27:12 -0300

    [ :triangular_ruler: ] | create: schemas - tech (sanity)

---
+++ personal_website/sanity/schemas/item/tech.schema.ts
@@ -0,0 +1,24 @@
+export const TechSchema = {
+  name: 'tech',
+  title: 'Tech',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+    },
+    {
+      name: 'icon',
+      type: 'image',
+    },
+    {
+      name: 'preview',
+      type: 'image',
+    },
+    {
+      name: 'description',
+      type: 'reference',
+      to: [{ type: 'tp_text' }],
+    },
+  ],
+};
