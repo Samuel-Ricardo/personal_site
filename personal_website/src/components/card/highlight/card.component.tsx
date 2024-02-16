ffdcd4ba7a9c369ff2e85f2a32639a4e63dad369
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-15 22:00:57 -0300

    [ :recycle: ] | refact: card - social_proof => Highlight Card (app::component)

---
+++ personal_website/src/components/card/highlight/card.component.tsx
@@ -0,0 +1,28 @@
+import { ISocialProofProps } from '@/@types/props/card/social_proof';
+import { FrostedGlassCard } from '../glass.card';
+
+import './card.style.scss';
+
+export const HighlightCard = ({
+  Icon,
+  title,
+  numbers,
+  k = false,
+  children,
+  description,
+}: ISocialProofProps) => {
+  return (
+    <FrostedGlassCard id="sp-card">
+      {Icon ?? <div className="image-container">{Icon}</div>}
+
+      <h1 className={`sp-title ${!Icon ?? 'mt-0'}`}>{title}</h1>
+
+      <p className="numbers">
+        +<span>{numbers}</span>
+        {k && 'k'}
+      </p>
+
+      <p className="description">{description}</p>
+    </FrostedGlassCard>
+  );
+};
