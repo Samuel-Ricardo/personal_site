444e2523a5e02ecb3eded67ae1c2a80d5ea3a67b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-17 18:05:02 -0300

    [ :sparkles: ] | create: entity - occupation > toDTO (module::core)

--- personal_website/src/modules/@core/occupations/entity/occupation.entity.ts
+++ personal_website/src/modules/@core/occupations/entity/occupation.entity.ts
@@ -1,4 +1,6 @@
 import { injectable } from 'inversify';
+import { IOccupationDTO } from '../DTO/occupation.dto';
+import { Gaegu } from 'next/font/google';
 
 @injectable()
 export class Occupation {
@@ -7,6 +9,14 @@ export class Occupation {
     private readonly _description: string,
     private readonly _image: string,
   ) {}
+
+  toDTO(): IOccupationDTO {
+    return {
+      title: this._title,
+      description: this._description,
+      image: this._image,
+    };
+  }
 
   get title(): string {
     return this._title;
