901cb6e39fd54928bd85c03be5974a8531d9fc63
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-17 14:44:04 -0300

    [ :syringe: ] | update: module - app > engine (app::modules)

--- personal_website/src/modules/app.registry.ts
+++ personal_website/src/modules/app.registry.ts
@@ -1,7 +1,9 @@
 import { ANIMATION_REGISTRY as ANIMATION } from './animations/animation.registry';
+import { ENGINE_REGISTRY as ENGINE } from './engine/engine.registry';
 import { FONTS_REGISTRY as FONTS } from './fonts/fonts.registry';
 
 export const MODULE = {
   FONTS,
   ANIMATION,
+  ENGINE,
 };
