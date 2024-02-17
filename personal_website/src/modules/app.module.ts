92cb3f23e3c60f7ca7c18d4a8b23d988a1104e87
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-17 14:44:26 -0300

    [ :syringe: ] | update: registry - app > engine (app::modules)

--- personal_website/src/modules/app.module.ts
+++ personal_website/src/modules/app.module.ts
@@ -1,6 +1,7 @@
 import { Container } from 'inversify';
 import { FONTS_MODULE } from './fonts/fonts.module';
 import { ANIMATION_MODULE } from './animations/animation.module';
+import { ENGINE_MODULE } from './engine/engine.module';
 
 const MODULES = new Container();
 
@@ -8,4 +9,5 @@ export const APP_MODULE = Container.merge(
   MODULES,
   FONTS_MODULE,
   ANIMATION_MODULE,
+  ENGINE_MODULE,
 );
