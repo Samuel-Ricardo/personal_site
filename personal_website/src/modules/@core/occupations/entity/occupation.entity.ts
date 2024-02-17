ed8fd11a40047eb3b1ffb64c6a1ada7b0b1810d9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-17 18:04:15 -0300

    [ :sparkles: ] | create: entity - occupation > getters (module::core)

--- personal_website/src/modules/@core/occupations/entity/occupation.entity.ts
+++ personal_website/src/modules/@core/occupations/entity/occupation.entity.ts
@@ -7,4 +7,16 @@ export class Occupation {
     private readonly _description: string,
     private readonly _image: string,
   ) {}
+
+  get title(): string {
+    return this._title;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  get image(): string {
+    return this._image;
+  }
 }
