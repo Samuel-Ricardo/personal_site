82ef2012d933ed37a717f981217ea09f6493ab75
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 14:28:02 -0300

    [ :syringe: ] | update: factory - assembler > service (app::modules)

--- personal_website/src/modules/@core/assembler/assembler.factory.ts
+++ personal_website/src/modules/@core/assembler/assembler.factory.ts
@@ -1,8 +1,20 @@
+import { ASSEMBLER_MODULE } from './assembler.module';
+import { ASSEMBLER_REGISTRY } from './assembler.registry';
+import { AssemblerService } from './service/assembler.service';
+
 export const ASSEMBLER_FACTORY = {
+  SERVICE: {
+    MAIN: () =>
+      ASSEMBLER_MODULE.get<AssemblerService>(ASSEMBLER_REGISTRY.SERVICE.MAIN),
+  },
   USE_CASE: {
     FIND: {
-      TITLE: Symbol.for('MODULE::ASSEMBLER:USE_CASE:FIND:TITLE'),
-      TEXT: Symbol.for('MODULE::ASSEMBLER:USE_CASE:FIND:TEXT'),
+      TITLE: ASSEMBLER_MODULE.get<AssemblerService>(
+        ASSEMBLER_REGISTRY.USE_CASE.FIND.TITLE,
+      ),
+      TEXT: ASSEMBLER_MODULE.get<AssemblerService>(
+        ASSEMBLER_REGISTRY.USE_CASE.FIND.TEXT,
+      ),
     },
   },
 };
