1a6920edcfebd2254d8da363f11820483c71997b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 13:53:29 -0300

    [ :sparkles: ] | create: use-case - find > text (module::core:assembler)

---
+++ personal_website/src/modules/@core/assembler/use_case/find/text.use_case.ts
@@ -0,0 +1,8 @@
+import { IAssemblerFindDTO } from '../../DTO/gateway/find/index.dto';
+import { AssemblerGatewaySupport } from '../../gateway/gateway.support';
+
+export class FindTextUseCase extends AssemblerGatewaySupport {
+  async execute(DTO: IAssemblerFindDTO) {
+    return this.gateway.fintText(DTO);
+  }
+}
