3daf0f3a5ee6a448b711d5c3b0cfb6f9dca330b4
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 11:37:24 -0300

    [ :syringe: ] | update: registry - occupation > service (app::modules)

--- personal_website/src/modules/@core/occupations/occupation.registry.ts
+++ personal_website/src/modules/@core/occupations/occupation.registry.ts
@@ -1,4 +1,7 @@
 export const OCCUPATION_REGISTRY = {
+  SERVICE: {
+    MAIN: Symbol.for('MODULE::CORE:OCCUPATION:SERVICE:MAIN'),
+  },
   USE_CASE: {
     FIND: {
       ALL: Symbol.for('MODULE::CORE:OCCUPATION:USE_CASE:FIND:ALL'),
