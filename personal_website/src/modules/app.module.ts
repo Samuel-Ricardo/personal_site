e1b8712875529b6c28716a6585065e40d8261761
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 11:32:58 -0300

    [ :syringe: ] | update: module - app > occupation (app::modules)

--- personal_website/src/modules/app.module.ts
+++ personal_website/src/modules/app.module.ts
@@ -3,6 +3,7 @@ import { FONTS_MODULE } from './fonts/fonts.module';
 import { ANIMATION_MODULE } from './animations/animation.module';
 import { ENGINE_MODULE } from './engine/engine.module';
 import { GATEWAY_MODULE } from './gateway/gateway.module';
+import { OCCUPATION_MODULE } from './@core/occupations/occupation.module';
 
 const MODULES = new Container();
 
@@ -12,4 +13,5 @@ export const APP_MODULE = Container.merge(
   ANIMATION_MODULE,
   ENGINE_MODULE,
   GATEWAY_MODULE,
+  OCCUPATION_MODULE,
 );
