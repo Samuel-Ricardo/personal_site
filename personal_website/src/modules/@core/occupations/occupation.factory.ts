5e109d1fbd5b49ff110a2689ca1e60726e7104b9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 11:45:30 -0300

    [ :syringe: ] | update: factory - occupation > service (app::modules)

--- personal_website/src/modules/@core/occupations/occupation.factory.ts
+++ personal_website/src/modules/@core/occupations/occupation.factory.ts
@@ -1,8 +1,15 @@
 import { OCCUPATION_MODULE } from './occupation.module';
 import { OCCUPATION_REGISTRY } from './occupation.registry';
+import { OccupationService } from './service/occupation.service';
 import { FindAllOccupationsUseCase } from './use_case/find/all.use_case';
 
 export const OCCUPATION_FACTORY = {
+  SERVICE: {
+    MAIN: () =>
+      OCCUPATION_MODULE.get<OccupationService>(
+        OCCUPATION_REGISTRY.SERVICE.MAIN,
+      ),
+  },
   USE_CASE: {
     FIND: {
       ALL: () =>
