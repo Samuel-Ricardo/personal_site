7cfea94b49e9b4101c23b0e11aa707a6a789f1ba
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 14:13:53 -0300

    [ :sparkles: ] | create: service - assembler > findTitle (module::core)

--- personal_website/src/modules/@core/assembler/service/assembler.service.ts
+++ personal_website/src/modules/@core/assembler/service/assembler.service.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'inversify';
 import { FindTitleUseCase } from '../use_case/find/title.use_case';
 import { FindTextUseCase } from '../use_case/find/text.use_case';
 import { MODULE } from '@/modules/app.registry';
+import { IAssemblerFindDTO } from '../DTO/gateway/find/index.dto';
 
 @injectable()
 export class AssemblerService {
@@ -11,4 +12,8 @@ export class AssemblerService {
     @inject(MODULE.ASSEMBLER.USE_CASE.FIND.TEXT)
     private readonly _findText: FindTextUseCase,
   ) {}
+
+  async findTitle(DTO: IAssemblerFindDTO) {
+    return await this._findTitle.execute(DTO);
+  }
 }
