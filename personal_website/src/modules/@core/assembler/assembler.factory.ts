90edf6b16e01b175b60e691cd881a23994f04fcd
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 14:02:57 -0300

    [ :syringe: ] | create: factory - assembler (app::modules)

---
+++ personal_website/src/modules/@core/assembler/assembler.factory.ts
@@ -0,0 +1,8 @@
+export const ASSEMBLER_FACTORY = {
+  USE_CASE: {
+    FIND: {
+      TITLE: Symbol.for('MODULE::ASSEMBLER:USE_CASE:FIND:TITLE'),
+      TEXT: Symbol.for('MODULE::ASSEMBLER:USE_CASE:FIND:TEXT'),
+    },
+  },
+};
