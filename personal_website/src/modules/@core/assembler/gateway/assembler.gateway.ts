179f54f9db7747a94076d55fa395597a56258ed9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 15:40:36 -0300

    [ :pencil2: ] | fix: typo

--- personal_website/src/modules/@core/assembler/gateway/assembler.gateway.ts
+++ personal_website/src/modules/@core/assembler/gateway/assembler.gateway.ts
@@ -1,6 +1,6 @@
 import { IAssemblerFindDTO } from '../DTO/gateway/find/index.dto';
 
 export interface IAssemblerGateway {
-  findTitle(DTO: IAssemblerFindDTO): string;
-  fintText(DTO: IAssemblerFindDTO): string;
+  findTitle(DTO: IAssemblerFindDTO): Promise<string>;
+  findText(DTO: IAssemblerFindDTO): Promise<string>;
 }
