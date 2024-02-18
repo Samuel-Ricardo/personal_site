7e694dab2699b4a8b8bb61d854049c23c51f2aa1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 14:47:38 -0300

    [ :sparkles: ] | create: controller - assembler > findTitle  (module::core)

--- personal_website/src/modules/@core/assembler/controller/assembler.controller.ts
+++ personal_website/src/modules/@core/assembler/controller/assembler.controller.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'inversify';
 import { AssemblerService } from '../service/assembler.service';
 import { MODULE } from '@/modules/app.registry';
+import { IAssemblerFindDTO } from '../DTO/gateway/find/index.dto';
 
 @injectable()
 export class AssemblerController {
@@ -11,5 +12,9 @@ export class AssemblerController {
 
   async assembleOccupation() {
     return await this.service.assembleOccupation();
+  }
+
+  async findTitle(DTO: IAssemblerFindDTO) {
+    return await this.service.findTitle(DTO);
   }
 }
