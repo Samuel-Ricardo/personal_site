8ba9456f597f481d4c11ce1c4be8386bbe5e3b1a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 11:32:43 -0300

    [ :syringe: ] | update: registry - app > occupation (app::modules)

--- personal_website/src/modules/app.registry.ts
+++ personal_website/src/modules/app.registry.ts
@@ -2,10 +2,12 @@ import { ANIMATION_REGISTRY as ANIMATION } from './animations/animation.registry
 import { ENGINE_REGISTRY as ENGINE } from './engine/engine.registry';
 import { FONTS_REGISTRY as FONTS } from './fonts/fonts.registry';
 import { GATEWAY_REGISTRY as GATEWAY } from './gateway/gateway.registry';
+import { OCCUPATION_REGISTRY as OCCUPATION } from './@core/occupations/occupation.registry';
 
 export const MODULE = {
   FONTS,
   ANIMATION,
   ENGINE,
   GATEWAY,
+  OCCUPATION,
 };
