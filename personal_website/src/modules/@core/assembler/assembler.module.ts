fecbdb853d840e682f172e6acad96dfce58071f9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 14:25:39 -0300

    [ :syringe: ] | update: module - assembler > service (app::modules)

--- personal_website/src/modules/@core/assembler/assembler.module.ts
+++ personal_website/src/modules/@core/assembler/assembler.module.ts
@@ -4,6 +4,7 @@ import { OCCUPATION_MODULE } from '../occupations/occupation.module';
 import { ASSEMBLER_REGISTRY } from './assembler.registry';
 import { FindTitleUseCase } from './use_case/find/title.use_case';
 import { FindTextUseCase } from './use_case/find/text.use_case';
+import { AssemblerService } from './service/assembler.service';
 
 const MODULE = new Container({
   defaultScope: 'Singleton',
@@ -23,3 +24,5 @@ ASSEMBLER_MODULE.bind(ASSEMBLER_REGISTRY.USE_CASE.FIND.TITLE).to(
 ASSEMBLER_MODULE.bind(ASSEMBLER_REGISTRY.USE_CASE.FIND.TEXT).to(
   FindTextUseCase,
 );
+
+ASSEMBLER_MODULE.bind(ASSEMBLER_REGISTRY.SERVICE.MAIN).to(AssemblerService);
