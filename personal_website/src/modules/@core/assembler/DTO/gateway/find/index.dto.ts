2feb6ac235e57ce7b56f35bb6ddc3c86c3d15331
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 12:35:14 -0300

    [ :label: ] | create: DTO - assembler > find (module::core)

---
+++ personal_website/src/modules/@core/assembler/DTO/gateway/find/index.dto.ts
@@ -0,0 +1,5 @@
+import { ICMSGatewayConfig } from '@/@types/module/gateway/cms/sanity';
+
+export interface IAssemblerFindDTO extends ICMSGatewayConfig {
+  identifier: string;
+}
--- personal_website/src/modules/gateway/gateway.module.ts
+++ personal_website/src/modules/gateway/gateway.module.ts
@@ -1,6 +1,16 @@
+import 'reflect-metadata';
+
 import { Container } from 'inversify';
 import { CMS_GATEWAY_MODULE } from './CMS/cms.module';
+import { ENGINE_MODULE } from '../engine/engine.module';
 
-const MODULE = new Container({ defaultScope: 'Singleton' });
+const MODULE = new Container({
+  defaultScope: 'Singleton',
+  autoBindInjectable: true,
+});
 
-export const GATEWAY_MODULE = Container.merge(MODULE, CMS_GATEWAY_MODULE);
+export const GATEWAY_MODULE = Container.merge(
+  MODULE,
+  CMS_GATEWAY_MODULE,
+  ENGINE_MODULE,
+);
