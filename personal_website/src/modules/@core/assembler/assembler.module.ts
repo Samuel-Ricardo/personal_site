24f7c80b1aefb7547e15cceff73c0447a1adfb20
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 14:25:10 -0300

    [ :syringe: ] | update: module - assembler > use-case (app::modules)

--- personal_website/src/modules/@core/assembler/assembler.module.ts
+++ personal_website/src/modules/@core/assembler/assembler.module.ts
@@ -1,9 +1,25 @@
 import { GATEWAY_MODULE } from '@/modules/gateway/gateway.module';
 import { Container } from 'inversify';
+import { OCCUPATION_MODULE } from '../occupations/occupation.module';
+import { ASSEMBLER_REGISTRY } from './assembler.registry';
+import { FindTitleUseCase } from './use_case/find/title.use_case';
+import { FindTextUseCase } from './use_case/find/text.use_case';
 
 const MODULE = new Container({
   defaultScope: 'Singleton',
   autoBindInjectable: true,
 });
 
-export const ASSEMBLER_MODULE = Container.merge(MODULE, GATEWAY_MODULE);
+export const ASSEMBLER_MODULE = Container.merge(
+  MODULE,
+  GATEWAY_MODULE,
+  OCCUPATION_MODULE,
+);
+
+ASSEMBLER_MODULE.bind(ASSEMBLER_REGISTRY.USE_CASE.FIND.TITLE).to(
+  FindTitleUseCase,
+);
+
+ASSEMBLER_MODULE.bind(ASSEMBLER_REGISTRY.USE_CASE.FIND.TEXT).to(
+  FindTextUseCase,
+);
