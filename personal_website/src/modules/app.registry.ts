841967726e7ff5b05c94cee9e772d1cd15213d2b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 10:20:45 -0300

    [ :syringe: ] | update: registry - app > gateway (app::modules)

--- personal_website/src/modules/app.registry.ts
+++ personal_website/src/modules/app.registry.ts
@@ -1,9 +1,11 @@
 import { ANIMATION_REGISTRY as ANIMATION } from './animations/animation.registry';
 import { ENGINE_REGISTRY as ENGINE } from './engine/engine.registry';
 import { FONTS_REGISTRY as FONTS } from './fonts/fonts.registry';
+import { GATEWAY_REGISTRY as GATEWAY } from './gateway/gateway.registry';
 
 export const MODULE = {
   FONTS,
   ANIMATION,
   ENGINE,
+  GATEWAY,
 };
