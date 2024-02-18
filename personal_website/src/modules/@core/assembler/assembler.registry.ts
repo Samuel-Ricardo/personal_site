2b46ebd77975bab39d125d5757176cf561cf969c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-18 13:56:35 -0300

    [ :syringe: ] | create: registry - assembler (app::modules)

---
+++ personal_website/src/modules/@core/assembler/assembler.registry.ts
@@ -0,0 +1,8 @@
+export const ASSEMBLER_REGISTRY = {
+  USE_CASE: {
+    FIND: {
+      TITLE: Symbol.for('MODULE::CORE:ASSEMBLER:USE_CASE:FIND:TITLE'),
+      TEXT: Symbol.for('MODULE::CORE:ASSEMBLER:USE_CASE:FIND:TEXT'),
+    },
+  },
+};
