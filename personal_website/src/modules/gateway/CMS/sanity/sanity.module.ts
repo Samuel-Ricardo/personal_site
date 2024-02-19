ac55430ebcf0f2f99651fbf31e985d5492019111
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-19 11:13:25 -0300

    [ :syringe: ] | update: module - sanity > person (module::cms:gateway)

--- personal_website/src/modules/gateway/CMS/sanity/sanity.module.ts
+++ personal_website/src/modules/gateway/CMS/sanity/sanity.module.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { SANITY_CMS_GATEWAY_REGISTRY } from './sanity.registry';
 import { SanityOccupationGateway } from './occupation/occupation.gateway';
 import { SanityAssemblerGateway } from './assembler/assembler.gateway';
+import { SanityPersonGateway } from './person/person.gateway';
 
 export const SANITY_CMS_GATEWAY_MODULE = new Container({
   defaultScope: 'Singleton',
@@ -13,4 +14,8 @@ SANITY_CMS_GATEWAY_MODULE.bind(SANITY_CMS_GATEWAY_REGISTRY.OCCUPATION).to(
 
 SANITY_CMS_GATEWAY_MODULE.bind(SANITY_CMS_GATEWAY_REGISTRY.ASSEMBLER).to(
   SanityAssemblerGateway,
+);
+
+SANITY_CMS_GATEWAY_MODULE.bind(SANITY_CMS_GATEWAY_REGISTRY.PERSON).to(
+  SanityPersonGateway,
 );
