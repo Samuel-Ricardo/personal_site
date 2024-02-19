a8bb3a7dc7f82802461a5299c00e1a8eee17c875
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-19 11:45:54 -0300

    [ :syringe: ] | update: module - person > controller (app::modules)

--- personal_website/src/modules/@core/person/person.module.ts
+++ personal_website/src/modules/@core/person/person.module.ts
@@ -4,6 +4,7 @@ import { PERSON_REGISTRY } from './person.registry';
 import { FindAllPersonUseCase } from './use_case/find/all.use_case';
 import { FindOnePersonUseCase } from './use_case/find/one.use_case';
 import { PersonService } from './service/person.service';
+import { PersonController } from './controller/person.controller';
 
 const MODULE = new Container({
   defaultScope: 'Singleton',
@@ -15,3 +16,4 @@ export const PERSON_MODULE = Container.merge(MODULE, GATEWAY_MODULE);
 PERSON_MODULE.bind(PERSON_REGISTRY.USE_CASE.FIND.ALL).to(FindAllPersonUseCase);
 PERSON_MODULE.bind(PERSON_REGISTRY.USE_CASE.FIND.ONE).to(FindOnePersonUseCase);
 PERSON_MODULE.bind(PERSON_REGISTRY.SERVICE.MAIN).to(PersonService);
+PERSON_MODULE.bind(PERSON_REGISTRY.CONTROLLER.MAIN).to(PersonController);
