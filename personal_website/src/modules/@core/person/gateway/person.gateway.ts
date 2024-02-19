66414eb1ea065c8da1c9e74a18e3add181b6c539
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-19 10:55:41 -0300

    [ :door: ] | create: gateway - person (module::core)

---
+++ personal_website/src/modules/@core/person/gateway/person.gateway.ts
@@ -0,0 +1,8 @@
+import { Person } from '../entity/person.entity';
+import { IFindAllPersonDTO } from '../DTO/gateway/find/all.dto';
+import { IFindOnePersonDTO } from '../DTO/gateway/find/one.dto';
+
+export interface IPersonGateway {
+  findAll(DTO: IFindAllPersonDTO): Promise<Person[]>;
+  findOne(DTO: IFindOnePersonDTO): Promise<Person>;
+}
