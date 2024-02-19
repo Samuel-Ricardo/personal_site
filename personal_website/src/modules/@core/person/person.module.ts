ad9d3eb59266e49f1917f85a86d6c808eccee1a5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-19 11:38:52 -0300

    [ :syringe: ] | update: module - person (app::modules)

--- personal_website/src/modules/@core/person/person.module.ts
+++ personal_website/src/modules/@core/person/person.module.ts
@@ -3,6 +3,7 @@ import { Container } from 'inversify';
 import { PERSON_REGISTRY } from './person.registry';
 import { FindAllPersonUseCase } from './use_case/find/all.use_case';
 import { FindOnePersonUseCase } from './use_case/find/one.use_case';
+import { PersonService } from './service/person.service';
 
 const MODULE = new Container({
   defaultScope: 'Singleton',
@@ -11,9 +12,6 @@ const MODULE = new Container({
 
 export const PERSON_MODULE = Container.merge(MODULE, GATEWAY_MODULE);
 
-PERSON_MODULE.bind(PERSON_REGISTRY.USE_CASE.FIND.ALL).toConstantValue(
-  FindAllPersonUseCase,
-);
-PERSON_MODULE.bind(PERSON_REGISTRY.USE_CASE.FIND.ONE).toConstantValue(
-  FindOnePersonUseCase,
-);
+PERSON_MODULE.bind(PERSON_REGISTRY.USE_CASE.FIND.ALL).to(FindAllPersonUseCase);
+PERSON_MODULE.bind(PERSON_REGISTRY.USE_CASE.FIND.ONE).to(FindOnePersonUseCase);
+PERSON_MODULE.bind(PERSON_REGISTRY.SERVICE.MAIN).to(PersonService);
