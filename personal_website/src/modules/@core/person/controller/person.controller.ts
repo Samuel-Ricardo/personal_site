77757287dba19d468532045d591464efd4716025
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-19 11:44:53 -0300

    [ :vertical_traffic_light: ] | create: controller - person > findOne (module::core)

--- personal_website/src/modules/@core/person/controller/person.controller.ts
+++ personal_website/src/modules/@core/person/controller/person.controller.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'inversify';
 import { PersonService } from '../service/person.service';
 import { MODULE } from '@/modules/app.registry';
 import { IFindAllPersonDTO } from '../DTO/gateway/find/all.dto';
+import { IFindOnePersonDTO } from '../DTO/gateway/find/one.dto';
 
 @injectable()
 export class PersonController {
@@ -12,5 +13,9 @@ export class PersonController {
 
   async findAll(DTO?: IFindAllPersonDTO) {
     return await this.service.findAll(DTO);
+  }
+
+  async findOne(DTO: IFindOnePersonDTO) {
+    return await this.service.findOne(DTO);
   }
 }
