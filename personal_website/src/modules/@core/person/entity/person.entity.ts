fbf3e4dd82c9d8a138dadd1c926e7458816d110a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-19 10:31:22 -0300

    [ :sparkles: ] | create: entity - person > getters (module::core)

--- personal_website/src/modules/@core/person/entity/person.entity.ts
+++ personal_website/src/modules/@core/person/entity/person.entity.ts
@@ -4,4 +4,16 @@ export class Person {
     protected readonly _title: string,
     protected readonly _avatar: string,
   ) {}
+
+  get name() {
+    return this._name;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get avatar() {
+    return this._avatar;
+  }
 }
