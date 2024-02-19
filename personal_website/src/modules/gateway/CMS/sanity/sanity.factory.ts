a3f98b65275dab36b44a40683b8d13b44a14cb2a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-19 11:13:56 -0300

    [ :syringe: ] | update: factory - sanity > person (module::cms:gateway)

--- personal_website/src/modules/gateway/CMS/sanity/sanity.factory.ts
+++ personal_website/src/modules/gateway/CMS/sanity/sanity.factory.ts
@@ -2,8 +2,13 @@ import { IOccupationGateway } from '@/modules/@core/occupations/gateway/occupati
 import { SANITY_CMS_GATEWAY_MODULE } from './sanity.module';
 import { SANITY_CMS_GATEWAY_REGISTRY } from './sanity.registry';
 import { IAssemblerGateway } from '@/modules/@core/assembler/gateway/assembler.gateway';
+import { IPersonGateway } from '@/modules/@core/person/gateway/person.gateway';
 
 export const SANITY_CMS_GATEWAY_FACTORY = {
+  PERSON: () =>
+    SANITY_CMS_GATEWAY_MODULE.get<IPersonGateway>(
+      SANITY_CMS_GATEWAY_REGISTRY.PERSON,
+    ),
   OCCUPATION: () =>
     SANITY_CMS_GATEWAY_MODULE.get<IOccupationGateway>(
       SANITY_CMS_GATEWAY_REGISTRY.OCCUPATION,
