9934c17c1cc216d2bcfbcb9bb9dd7f4d276b136c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-19 10:31:49 -0300

    [ :sparkles: ] | create: entity - person > toDTO (module::core)

--- personal_website/src/modules/@core/person/entity/person.entity.ts
+++ personal_website/src/modules/@core/person/entity/person.entity.ts
@@ -1,9 +1,19 @@
+import { IPersonDTO } from '../DTO/person.dto';
+
 export class Person {
   constructor(
     protected readonly _name: string,
     protected readonly _title: string,
     protected readonly _avatar: string,
   ) {}
+
+  toDTO(): IPersonDTO {
+    return {
+      name: this._name,
+      title: this._title,
+      avatar: this._avatar,
+    };
+  }
 
   get name() {
     return this._name;
