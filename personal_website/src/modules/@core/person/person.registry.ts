16552552a3e4300f10ddb27027f4e3135fc3e42f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-19 11:22:16 -0300

    [ :syringe: ] | create: registry - person (app::modules)

---
+++ personal_website/src/modules/@core/person/person.registry.ts
@@ -0,0 +1,8 @@
+export const PERSON_REGISTRY = {
+  USE_CASE: {
+    FIND: {
+      ALL: Symbol.for('MODULE::USE_CASE::PERSON::FIND::ALL'),
+      ONE: Symbol.for('MODULE::USE_CASE::PERSON::FIND::ONE'),
+    },
+  },
+};
