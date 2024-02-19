d7dcd06cbed5d04996369dd73a5b92c0c8ace762
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-19 11:29:02 -0300

    [ :syringe: ] | update: registry - app > person (app::modules)

--- personal_website/src/modules/app.registry.ts
+++ personal_website/src/modules/app.registry.ts
@@ -4,6 +4,7 @@ import { FONTS_REGISTRY as FONTS } from './fonts/fonts.registry';
 import { GATEWAY_REGISTRY as GATEWAY } from './gateway/gateway.registry';
 import { OCCUPATION_REGISTRY as OCCUPATION } from './@core/occupations/occupation.registry';
 import { ASSEMBLER_REGISTRY as ASSEMBLER } from './@core/assembler/assembler.registry';
+import { PERSON_REGISTRY as PERSON } from './@core/person/person.registry';
 
 export const MODULE = {
   FONTS,
@@ -12,4 +13,5 @@ export const MODULE = {
   GATEWAY,
   OCCUPATION,
   ASSEMBLER,
+  PERSON,
 };
