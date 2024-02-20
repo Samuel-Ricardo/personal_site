2b77f4f464c50271edfdb2ce1e3d5a6c03274c2d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-20 10:36:33 -0300

    [ :door: ] | update: gateway - sanity (module::cms)

--- personal_website/src/modules/gateway/CMS/sanity/sanity.gateway.ts
+++ personal_website/src/modules/gateway/CMS/sanity/sanity.gateway.ts
@@ -1,11 +1,14 @@
 import { MODULE } from '@/modules/app.registry';
 import { inject, injectable } from 'inversify';
 import { type SanityClient } from 'next-sanity';
+import { type ImageUrlBuilder } from 'sanity';
 
 @injectable()
 export abstract class SanitySupport {
   constructor(
     @inject(MODULE.ENGINE.CMS.SANITY)
     protected readonly client: SanityClient,
+    @inject(MODULE.ENGINE.CMS.BUILDER.IMAGE)
+    protected readonly imageBuilder: ImageUrlBuilder,
   ) {}
 }
