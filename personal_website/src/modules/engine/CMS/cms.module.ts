8f7da82f34a2bd5ad4facfba3db234b85b48aa63
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-20 10:28:21 -0300

    [ :syringe: ] | update: module - sanity > builder (module::engine:cms)

--- personal_website/src/modules/engine/CMS/cms.module.ts
+++ personal_website/src/modules/engine/CMS/cms.module.ts
@@ -1,7 +1,11 @@
 import { Container } from 'inversify';
 import { CMS_ENGINE_REGISTRY } from './cms.registry';
 import { SANITY } from './sanity/sanity.engine';
+import { SANITY_IMAGE_BUILDER } from './sanity/img.engine';
 
 export const CMS_ENGINE_MODULE = new Container({ defaultScope: 'Singleton' });
 
 CMS_ENGINE_MODULE.bind(CMS_ENGINE_REGISTRY.SANITY).toConstantValue(SANITY);
+CMS_ENGINE_MODULE.bind(CMS_ENGINE_REGISTRY.BUILDER.IMAGE)
+  .toDynamicValue(SANITY_IMAGE_BUILDER)
+  .inSingletonScope();
