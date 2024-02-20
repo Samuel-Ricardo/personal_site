a7c61cd99c941edc2574f680ddd04c72fe5e4628
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-20 11:03:29 -0300

    [ :door: ] | update: gateway - person (module::core)

--- personal_website/src/modules/@core/person/gateway/person.gateway.ts
+++ personal_website/src/modules/@core/person/gateway/person.gateway.ts
@@ -3,6 +3,6 @@ import { IFindAllPersonDTO } from '../DTO/gateway/find/all.dto';
 import { IFindOnePersonDTO } from '../DTO/gateway/find/one.dto';
 
 export interface IPersonGateway {
-  findAll(DTO: IFindAllPersonDTO): Promise<Person[]>;
-  findOne(DTO: IFindOnePersonDTO): Promise<Person>;
+  findAll(DTO?: IFindAllPersonDTO): Promise<Person[]>;
+  findOne(DTO: IFindOnePersonDTO): Promise<Person | undefined>;
 }
