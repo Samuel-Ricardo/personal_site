a54e236ac820c144eb7078f7d9bc1c8474d664f8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-20 11:27:33 -0300

    [ :art: ] | update: card - hero (app::component)

--- personal_website/src/components/section/home/hero/card.component.tsx
+++ personal_website/src/components/section/home/hero/card.component.tsx
@@ -2,12 +2,27 @@ import { FrostedGlassCard } from '@/components/card/glass.card';
 import './card.style.scss';
 import { HeroAvatar } from './card/avatar/avatar.component';
 import { HeroCardContentContainer } from './card/content/container/container.component';
+import { MODULES } from '@/modules/app.factory';
 
-export const HeroCard = () => {
+export const HeroCard = async () => {
+  const {
+    person: { avatar, name, title },
+    paragraph,
+    resume,
+    contact,
+  } = await MODULES.ASSEMBLER.SERVICE.MAIN().assembleHero();
+
   return (
     <FrostedGlassCard className="z-50">
-      <HeroAvatar />
-      <HeroCardContentContainer />
+      <HeroAvatar src={avatar} />
+      <HeroCardContentContainer
+        title={name}
+        subtitle={title}
+        resume={resume}
+        contact={contact}
+      >
+        {paragraph}
+      </HeroCardContentContainer>
     </FrostedGlassCard>
   );
 };
