9abea10c3940c456aa5857284cd97bc13e74b06e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-20 10:33:17 -0300

    [ :syringe: ] | update: factory - sanity > builder (module::engine:cms)

--- personal_website/src/modules/engine/CMS/cms.factory.ts
+++ personal_website/src/modules/engine/CMS/cms.factory.ts
@@ -1,7 +1,12 @@
 import { SanityClient } from 'next-sanity';
 import { CMS_ENGINE_MODULE } from './cms.module';
 import { CMS_ENGINE_REGISTRY } from './cms.registry';
+import { ImageUrlBuilder } from 'sanity';
 
 export const CMS_ENGINE_FACTORY = {
   SANITY: () => CMS_ENGINE_MODULE.get<SanityClient>(CMS_ENGINE_REGISTRY.SANITY),
+  BUILDER: {
+    IMAGE: () =>
+      CMS_ENGINE_MODULE.get<ImageUrlBuilder>(CMS_ENGINE_REGISTRY.BUILDER.IMAGE),
+  },
 };
