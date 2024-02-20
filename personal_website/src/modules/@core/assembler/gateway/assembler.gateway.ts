e172396a34f476243f27992a82a33bd2efa258ba
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-20 11:06:17 -0300

    [ :door: ] | update: gateway - assembler (module::core)

--- personal_website/src/modules/@core/assembler/gateway/assembler.gateway.ts
+++ personal_website/src/modules/@core/assembler/gateway/assembler.gateway.ts
@@ -1,6 +1,6 @@
 import { IAssemblerFindDTO } from '../DTO/gateway/find/index.dto';
 
 export interface IAssemblerGateway {
-  findTitle(DTO: IAssemblerFindDTO): Promise<string>;
-  findText(DTO: IAssemblerFindDTO): Promise<string>;
+  findTitle(DTO: IAssemblerFindDTO): Promise<string | undefined>;
+  findText(DTO: IAssemblerFindDTO): Promise<string | undefined>;
 }
