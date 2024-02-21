3bd6b9bd1437c62f5e78027ffaf9dee3e9262155
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 10:26:31 -0300

    [ :syringe: ] | update: module - cms > static (module::gateway)

--- personal_website/src/modules/gateway/CMS/cms.module.ts
+++ personal_website/src/modules/gateway/CMS/cms.module.ts
@@ -1,9 +1,11 @@
 import { Container } from 'inversify';
 import { SANITY_CMS_GATEWAY_MODULE } from './sanity/sanity.module';
+import { STATIC_CMS_GATEWAY_MODULE } from './static/static.module';
 
 const MODULE = new Container({ defaultScope: 'Singleton' });
 
 export const CMS_GATEWAY_MODULE = Container.merge(
   MODULE,
   SANITY_CMS_GATEWAY_MODULE,
+  STATIC_CMS_GATEWAY_MODULE,
 );
