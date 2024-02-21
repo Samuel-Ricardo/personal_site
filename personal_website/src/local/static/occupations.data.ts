04aad1bc8d331b66f523ed166a7043c6a8fe861a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 09:09:13 -0300

    [ :card_file_box: ] | create: data - occupations (data::static)

---
+++ personal_website/src/local/static/occupations.data.ts
@@ -0,0 +1,10 @@
+import { type IOccupationDTO } from '@/modules/@core/occupations/DTO/occupation.dto';
+
+export const OCCUPATIONS: IOccupationDTO[] = [
+  {
+    title: 'Open Source',
+    description: 'Building, sharing and contributing to open source.',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
+  },
+];
