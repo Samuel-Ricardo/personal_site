3db51498d0b4b59bcd4f3093f10a97d54bb611b6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 11:34:37 -0300

    [ :syringe: ] | update: module - person > use_case (module::core:static)

--- personal_website/src/modules/@core/person/person.module.ts
+++ personal_website/src/modules/@core/person/person.module.ts
@@ -1,20 +1,31 @@
-import { GATEWAY_MODULE } from '@/modules/gateway/gateway.module';
 import { Container } from 'inversify';
 import { PERSON_REGISTRY } from './person.registry';
 import { FindAllPersonUseCase } from './use_case/find/all.use_case';
 import { FindOnePersonUseCase } from './use_case/find/one.use_case';
 import { PersonService } from './service/person.service';
 import { PersonController } from './controller/person.controller';
+import { FindOneStaticPersonUseCase } from './use_case/static/find/one.use_case';
+import { FindAllStaticPersonUseCase } from './use_case/static/find/all.use_case';
 
 const MODULE = new Container({
   defaultScope: 'Singleton',
   autoBindInjectable: true,
 });
 
-export const PERSON_MODULE = Container.merge(MODULE, GATEWAY_MODULE);
+//export const PERSON_MODULE = Container.merge(MODULE, GATEWAY_MODULE);
+
+export const PERSON_MODULE = MODULE;
 
 PERSON_MODULE.bind(PERSON_REGISTRY.USE_CASE.FIND.ALL).to(FindAllPersonUseCase);
 PERSON_MODULE.bind(PERSON_REGISTRY.USE_CASE.FIND.ONE).to(FindOnePersonUseCase);
+
+PERSON_MODULE.bind(PERSON_REGISTRY.USE_CASE.STATIC.FIND.ALL).to(
+  FindAllStaticPersonUseCase,
+);
+PERSON_MODULE.bind(PERSON_REGISTRY.USE_CASE.STATIC.FIND.ALL).to(
+  FindOneStaticPersonUseCase,
+);
+
 PERSON_MODULE.bind(PERSON_REGISTRY.SERVICE.MAIN).to(PersonService);
 PERSON_MODULE.bind(PERSON_REGISTRY.CONTROLLER.MAIN).to(PersonController);
 PERSON_MODULE.bind(PERSON_REGISTRY.MAIN).to(PersonController);
