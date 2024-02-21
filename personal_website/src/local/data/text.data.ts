c2fd9b640fe41c403aa6fa64f0ccf7fa4277da97
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 08:45:17 -0300

    [ :card_file_box: ] | create: data - text > home_hero_paragraph (data::local)

---
+++ personal_website/src/local/data/text.data.ts
@@ -0,0 +1,12 @@
+export const home_hero_paragraph = [
+  {
+    lang: 'pt-br',
+    content:
+      '👨‍💻| Sou um Programador apaixonado por desbloquear 🔓 as barreiras entre o real 🧠 e o imaginário 🌌, converter sonhos em realidade e claro... Café ☕ em soluções 💡',
+  },
+  {
+    lang: 'en',
+    content:
+      "👨‍💻| I'm a Programmer passionated about unlocking 🔓 the barriers between the real 🧠 and the imaginary 🌌, convert dreams into reality and of course... Coffee ☕ into solutions 💡",
+  },
+];
