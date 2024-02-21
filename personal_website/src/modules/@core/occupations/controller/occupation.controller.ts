b7b1251c626bb93b5967f7c0ab3b748eac76601b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 11:13:08 -0300

    [ :sparkles: ] | create: controller - occupation > interface (module::core)

--- personal_website/src/modules/@core/occupations/controller/occupation.controller.ts
+++ personal_website/src/modules/@core/occupations/controller/occupation.controller.ts
@@ -1,15 +1,16 @@
 import { inject, injectable } from 'inversify';
 import { OccupationService } from '../service/occupation.service';
 import { MODULE } from '@/modules/app.registry';
+import { IOccupationController } from './controller.interface';
 
 @injectable()
-export class OccupationController {
+export class OccupationController implements IOccupationController {
   constructor(
     @inject(MODULE.OCCUPATION.SERVICE.MAIN)
     private readonly service: OccupationService,
   ) {}
 
-  async findAll() {
+  async findAllAsync() {
     return await this.service.findAll();
   }
 }
