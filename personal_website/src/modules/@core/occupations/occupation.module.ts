840ce8555318b7b1fc8f8888136556ed36a4e194
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 10:56:42 -0300

    [ :syringe: ] | update: module - occupation > static (module::core)

--- personal_website/src/modules/@core/occupations/occupation.module.ts
+++ personal_website/src/modules/@core/occupations/occupation.module.ts
@@ -4,16 +4,23 @@ import { OCCUPATION_REGISTRY } from './occupation.registry';
 import { FindAllOccupationsUseCase } from './use_case/find/all.use_case';
 import { OccupationService } from './service/occupation.service';
 import { OccupationController } from './controller/occupation.controller';
+import { FindAllStaticOccupationsUseCase } from './use_case/find/static/all.use_case';
 
 const MODULE = new Container({
   defaultScope: 'Singleton',
   autoBindInjectable: true,
 });
 
-export const OCCUPATION_MODULE = Container.merge(MODULE, GATEWAY_MODULE);
+//export const OCCUPATION_MODULE = Container.merge(MODULE, GATEWAY_MODULE);
+
+export const OCCUPATION_MODULE = MODULE;
 
 OCCUPATION_MODULE.bind(OCCUPATION_REGISTRY.USE_CASE.FIND.ALL).to(
   FindAllOccupationsUseCase,
+);
+
+OCCUPATION_MODULE.bind(OCCUPATION_REGISTRY.USE_CASE.STATIC.FIND.ALL).to(
+  FindAllStaticOccupationsUseCase,
 );
 
 OCCUPATION_MODULE.bind(OCCUPATION_REGISTRY.SERVICE.MAIN).to(OccupationService);
