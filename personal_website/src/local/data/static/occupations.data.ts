f6a409d5ccd465e5976ecb95530b4db4620fcea1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 09:23:57 -0300

    [ :card_file_box: ] | update: data - occupations (data::static)

--- personal_website/src/local/data/static/occupations.data.ts
+++ personal_website/src/local/data/static/occupations.data.ts
@@ -20,8 +20,26 @@ export const OCCUPATIONS: IOccupationDTO[] = [
   },
   {
     title: 'Frontend',
-    description: 'Building responsive websites with the best technologies.',
+    description: 'Building websites with the best technologies.',
     image:
       'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg',
+  },
+  {
+    title: 'Mobile',
+    description: 'Building responsive and fast mobile applications.',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg',
+  },
+  {
+    title: 'DevOps',
+    description: 'Application Life Cycle, Containerization and CI/CD.',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
+  },
+  {
+    title: 'Microsservices',
+    description: 'Building scalable microservices.',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nestjs/nestjs-original.svg',
   },
 ];
