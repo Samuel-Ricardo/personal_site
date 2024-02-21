e73c3e6d8df78fe2307b105982583166713a7636
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 10:26:06 -0300

    [ :syringe: ] | update: registy - cms > static (module::gateway)

--- personal_website/src/modules/gateway/CMS/cms.registry.ts
+++ personal_website/src/modules/gateway/CMS/cms.registry.ts
@@ -1,5 +1,7 @@
 import { SANITY_CMS_GATEWAY_REGISTRY as SANITY } from './sanity/sanity.registry';
+import { STATIC_CMS_GATEWAY_REGISTRY as STATIC } from './static/static.registry';
 
 export const CMS_GATEWAT_REGISTRY = {
   SANITY,
+  STATIC,
 };
