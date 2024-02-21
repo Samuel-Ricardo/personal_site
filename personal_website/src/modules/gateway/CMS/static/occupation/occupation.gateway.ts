ec7df9293e2bf1d5b0adcebecdb8bc2442f850f3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 09:57:03 -0300

    [ :door: ] | implements: gateway - occupation (module::cms:static)

--- personal_website/src/modules/gateway/CMS/static/occupation/occupation.gateway.ts
+++ personal_website/src/modules/gateway/CMS/static/occupation/occupation.gateway.ts
@@ -1,10 +1,11 @@
+import { OCCUPATIONS } from '@/local/data/static/occupations.data';
 import { Occupation } from '@/modules/@core/occupations/entity/occupation.entity';
 import { IOccupationGateway } from '@/modules/@core/occupations/gateway/occupation.gateway';
 import { injectable } from 'inversify';
 
 @injectable()
 export class StaticOccupationGateway implements IOccupationGateway {
-  findAll(): Promise<Occupation[]> {
-    throw new Error('Method not implemented.');
+  async findAll() {
+    return Occupation.fromDTOList(OCCUPATIONS);
   }
 }
