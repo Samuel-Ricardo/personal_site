b5853794fda6ade0f67c34fd3b6d5ce77a91d365
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 10:50:29 -0300

    [ :syringe: ] | update: registy - occupation > static (module::core)

--- personal_website/src/modules/@core/occupations/occupation.registry.ts
+++ personal_website/src/modules/@core/occupations/occupation.registry.ts
@@ -7,6 +7,11 @@ export const OCCUPATION_REGISTRY = {
     MAIN: Symbol.for('MODULE::CORE:OCCUPATION:SERVICE:MAIN'),
   },
   USE_CASE: {
+    STATIC: {
+      FIND: {
+        ALL: Symbol.for('MODULE::CORE:OCCUPATION:USE_CASE:STATIC:FIND:ALL'),
+      },
+    },
     FIND: {
       ALL: Symbol.for('MODULE::CORE:OCCUPATION:USE_CASE:FIND:ALL'),
     },
