74f7b95b7f5d5dea9ff7c84ee9554b3dede6e3f8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 11:52:16 -0300

    [ :syringe: ] | update: registry - person > controller (module::core:static)

--- personal_website/src/modules/@core/person/person.registry.ts
+++ personal_website/src/modules/@core/person/person.registry.ts
@@ -1,7 +1,9 @@
 export const PERSON_REGISTRY = {
   MAIN: Symbol.for('MODULE::PERSON::MAIN'),
+  STATIC: Symbol.for('MODULE::PERSON::STATIC'),
   CONTROLLER: {
     MAIN: Symbol.for('MODULE::CONTROLLER::PERSON::MAIN'),
+    STATIC: Symbol.for('MODULE::CONTROLLER::PERSON::STATIC'),
   },
   SERVICE: {
     MAIN: Symbol.for('MODULE::SERVICE::PERSON::MAIN'),
