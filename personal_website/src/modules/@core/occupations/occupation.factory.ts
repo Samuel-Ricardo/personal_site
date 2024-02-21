b9a66745456af8a9d326891f18a2d1b34cf32c66
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 11:16:39 -0300

    [ :syringe: ] | update: factory - occupation > static (module::core)

--- personal_website/src/modules/@core/occupations/occupation.factory.ts
+++ personal_website/src/modules/@core/occupations/occupation.factory.ts
@@ -2,6 +2,7 @@ import { OccupationController } from './controller/occupation.controller';
 import { OCCUPATION_MODULE } from './occupation.module';
 import { OCCUPATION_REGISTRY } from './occupation.registry';
 import { OccupationService } from './service/occupation.service';
+import { StaticOccupationService } from './service/static/occupation.service';
 import { FindAllOccupationsUseCase } from './use_case/find/all.use_case';
 import { FindAllStaticOccupationsUseCase } from './use_case/find/static/all.use_case';
 
@@ -18,6 +19,10 @@ export const OCCUPATION_FACTORY = {
     MAIN: () =>
       OCCUPATION_MODULE.get<OccupationService>(
         OCCUPATION_REGISTRY.SERVICE.MAIN,
+      ),
+    STATIC: () =>
+      OCCUPATION_MODULE.get<StaticOccupationService>(
+        OCCUPATION_REGISTRY.SERVICE.STATIC,
       ),
   },
   USE_CASE: {
