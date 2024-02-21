6ac181930c63fc69464923e51bf961fb21657a3d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 11:51:39 -0300

    [ :sparkles: ] | implements: controller - person (module::core:static)

--- personal_website/src/modules/@core/person/controller/static/person.controller.ts
+++ personal_website/src/modules/@core/person/controller/static/person.controller.ts
@@ -2,6 +2,8 @@ import { inject, injectable } from 'inversify';
 import { IPersonController } from '../controller.interface';
 import { type IPersonService } from '../../service/service.interface';
 import { MODULE } from '@/modules/app.registry';
+import { IFindAllPersonDTO } from '../../DTO/gateway/find/all.dto';
+import { IFindOnePersonDTO } from '../../DTO/gateway/find/one.dto';
 
 @injectable()
 export class StaticPersonController implements IPersonController {
@@ -9,4 +11,12 @@ export class StaticPersonController implements IPersonController {
     @inject(MODULE.PERSON.SERVICE.STATIC)
     protected readonly service: IPersonService,
   ) {}
+
+  async findAllAsync(DTO?: IFindAllPersonDTO) {
+    return await this.service.findAllAsync(DTO);
+  }
+
+  async findOneAsync(DTO: IFindOnePersonDTO) {
+    return await this.service.findOneAsync(DTO);
+  }
 }
