657db63430339ebff73651647d6f5bbfe29d973f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 09:15:46 -0300

    [ :card_file_box: ] | update: data - occupations (data::static)

--- personal_website/src/local/data/static/occupations.data.ts
+++ personal_website/src/local/data/static/occupations.data.ts
@@ -7,4 +7,21 @@ export const OCCUPATIONS: IOccupationDTO[] = [
     image:
       'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
   },
+  {
+    title: 'Freelancer',
+    description: 'The Best Solutions for your bussines',
+    image: 'https://th.bing.com/th/id/OIG2.S1ygsRnG1jar4RwHFbgG?pid=ImgGn',
+  },
+  {
+    title: 'Backend',
+    description: "Scalable API's with healthy architecture.",
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg',
+  },
+  {
+    title: 'Frontend',
+    description: 'Building responsive websites with the best technologies.',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg',
+  },
 ];
