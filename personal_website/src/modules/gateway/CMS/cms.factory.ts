4cecb093a8ef5eea6793c566d01d163b76546014
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 10:25:46 -0300

    [ :syringe: ] | update: factory - cms > static (module::gateway)

--- personal_website/src/modules/gateway/CMS/cms.factory.ts
+++ personal_website/src/modules/gateway/CMS/cms.factory.ts
@@ -1,5 +1,7 @@
 import { SANITY_CMS_GATEWAY_FACTORY } from './sanity/sanity.factory';
+import { STATIC_CMS_GATEWAY_FACTORY } from './static/static.factory';
 
 export const CMS_GATEWAY_FACTORY = {
   SANITY: SANITY_CMS_GATEWAY_FACTORY,
+  STATIC: STATIC_CMS_GATEWAY_FACTORY,
 };
