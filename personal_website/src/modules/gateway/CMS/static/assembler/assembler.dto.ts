f2429716e5523017f15b5cfe25c256a2bd484f37
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 10:16:14 -0300

    [ :door: ] | implements: gateway - assembler > findTitle (module::cms:static)

--- personal_website/src/modules/gateway/CMS/static/assembler/assembler.dto.ts
+++ personal_website/src/modules/gateway/CMS/static/assembler/assembler.dto.ts
@@ -1,3 +1,4 @@
+import { TEXT } from '@/local/data/static/text.data';
 import { TITLES } from '@/local/data/static/title.data';
 import { IAssemblerFindDTO } from '@/modules/@core/assembler/DTO/gateway/find/index.dto';
 import { IAssemblerGateway } from '@/modules/@core/assembler/gateway/assembler.gateway';
@@ -9,6 +10,6 @@ export class StaticAssemblerGateway implements IAssemblerGateway {
   }
 
   async findText(DTO: IAssemblerFindDTO) {
-    throw new Error('Method not implemented.');
+    return TEXT.find(async text => text.identifier === DTO.identifier)?.content;
   }
 }
