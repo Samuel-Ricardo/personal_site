274806c5deda7f22b707ba8daaec97ed713e5770
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 11:26:04 -0300

    [ :syringe: ] | update: registry - person > use_case (module::core:static)

--- personal_website/src/modules/@core/person/person.registry.ts
+++ personal_website/src/modules/@core/person/person.registry.ts
@@ -7,6 +7,12 @@ export const PERSON_REGISTRY = {
     MAIN: Symbol.for('MODULE::SERVICE::PERSON::MAIN'),
   },
   USE_CASE: {
+    STATIC: {
+      FIND: {
+        ALL: Symbol.for('MODULE::USE_CASE::PERSON::STATIC::FIND::ALL'),
+        ONE: Symbol.for('MODULE::USE_CASE::PERSON::STATIC::FIND::ONE'),
+      },
+    },
     FIND: {
       ALL: Symbol.for('MODULE::USE_CASE::PERSON::FIND::ALL'),
       ONE: Symbol.for('MODULE::USE_CASE::PERSON::FIND::ONE'),
