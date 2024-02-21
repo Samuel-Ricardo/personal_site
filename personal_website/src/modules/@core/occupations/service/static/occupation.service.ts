b927b743f146516f86775d545696b9de4b01ccc2
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 11:02:41 -0300

    [ :sparkles: ] | implements: service - occupation (module::core:static)

--- personal_website/src/modules/@core/occupations/service/static/occupation.service.ts
+++ personal_website/src/modules/@core/occupations/service/static/occupation.service.ts
@@ -1,10 +1,16 @@
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { IOccupationService } from '../service.interface';
-import { Occupation } from '../../entity/occupation.entity';
+import { FindAllStaticOccupationsUseCase } from '../../use_case/find/static/all.use_case';
+import { MODULE } from '@/modules/app.registry';
 
 @injectable()
 export class StaticOccupationService implements IOccupationService {
-  findAll(): Promise<Occupation[]> {
-    throw new Error('Method not implemented.');
+  constructor(
+    @inject(MODULE.OCCUPATION.USE_CASE.STATIC.FIND.ALL)
+    protected readonly _findAll: FindAllStaticOccupationsUseCase,
+  ) {}
+
+  async findAll() {
+    return this._findAll.execute();
   }
 }
