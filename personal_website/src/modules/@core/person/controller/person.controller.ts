83bcd91e77d407836606b4ee067766992c58e7e6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 11:46:58 -0300

    [ :sparkles: ] | implements: controller - person > interface (module::core:static)

--- personal_website/src/modules/@core/person/controller/person.controller.ts
+++ personal_website/src/modules/@core/person/controller/person.controller.ts
@@ -3,19 +3,20 @@ import { PersonService } from '../service/person.service';
 import { MODULE } from '@/modules/app.registry';
 import { IFindAllPersonDTO } from '../DTO/gateway/find/all.dto';
 import { IFindOnePersonDTO } from '../DTO/gateway/find/one.dto';
+import { IPersonController } from './controller.interface';
 
 @injectable()
-export class PersonController {
+export class PersonController implements IPersonController {
   constructor(
     @inject(MODULE.PERSON.SERVICE.MAIN)
     protected readonly service: PersonService,
   ) {}
 
-  async findAll(DTO?: IFindAllPersonDTO) {
-    return await this.service.findAll(DTO);
+  async findAllAsync(DTO?: IFindAllPersonDTO) {
+    return await this.service.findAllAsync(DTO);
   }
 
-  async findOne(DTO: IFindOnePersonDTO) {
-    return await this.service.findOne(DTO);
+  async findOneAsync(DTO: IFindOnePersonDTO) {
+    return await this.service.findOneAsync(DTO);
   }
 }
