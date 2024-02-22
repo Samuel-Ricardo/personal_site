0badfc546c8637370d6ca50cf2d93d59f78dc104
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 21:29:58 -0300

    [ :syringe: ] | update: facotry - assembler > use_case (module::core::static)

--- personal_website/src/modules/@core/assembler/assembler.factory.ts
+++ personal_website/src/modules/@core/assembler/assembler.factory.ts
@@ -2,6 +2,8 @@ import { ASSEMBLER_MODULE } from './assembler.module';
 import { ASSEMBLER_REGISTRY } from './assembler.registry';
 import { AssemblerController } from './controller/assembler.controller';
 import { AssemblerService } from './service/assembler.service';
+import { FindStaticTextUseCase } from './use_case/static/find/text.use_case';
+import { FindStaticTitleUseCase } from './use_case/static/find/title.use_case';
 
 export const ASSEMBLER_FACTORY = {
   MAIN: () =>
@@ -17,6 +19,18 @@ export const ASSEMBLER_FACTORY = {
       ASSEMBLER_MODULE.get<AssemblerService>(ASSEMBLER_REGISTRY.SERVICE.MAIN),
   },
   USE_CASE: {
+    STATIC: {
+      FIND: {
+        TITLE: () =>
+          ASSEMBLER_MODULE.get<FindStaticTitleUseCase>(
+            ASSEMBLER_REGISTRY.USE_CASE.STATIC.FIND.TITLE,
+          ),
+        TEXT: () =>
+          ASSEMBLER_MODULE.get<FindStaticTextUseCase>(
+            ASSEMBLER_REGISTRY.USE_CASE.STATIC.FIND.TEXT,
+          ),
+      },
+    },
     FIND: {
       TITLE: ASSEMBLER_MODULE.get<AssemblerService>(
         ASSEMBLER_REGISTRY.USE_CASE.FIND.TITLE,
