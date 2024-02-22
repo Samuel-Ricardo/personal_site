cb68bb0cd224941ff99e8c35cd9f6215b61fe7f7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-21 22:50:34 -0300

    [ :video_game: ] | create: controller - assembler > assembleHero (module::core)

--- personal_website/src/modules/@core/assembler/controller/assembler.controller.ts
+++ personal_website/src/modules/@core/assembler/controller/assembler.controller.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'inversify';
 import { AssemblerService } from '../service/assembler.service';
 import { MODULE } from '@/modules/app.registry';
 import { IAssemblerFindDTO } from '../DTO/gateway/find/index.dto';
+import { Gaegu } from 'next/font/google';
 
 @injectable()
 export class AssemblerController {
@@ -12,6 +13,10 @@ export class AssemblerController {
 
   async assembleOccupation() {
     return await this.service.assembleOccupation();
+  }
+
+  async assembleHero() {
+    return await this.service.assembleHero();
   }
 
   async findTitle(DTO: IAssemblerFindDTO) {
