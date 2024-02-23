647e1f91e1493299f5a8538c7bb6ba1651dcd2ce
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-23 11:08:48 -0300

    [ :syringe: ] | update: factory - app > highlight (app::modules)

--- personal_website/src/modules/app.factory.ts
+++ personal_website/src/modules/app.factory.ts
@@ -5,6 +5,7 @@ import { GATEWAY_FACTORY as GATEWAY } from './gateway/gateway.factory';
 import { OCCUPATION_FACTORY as OCCUPATION } from './@core/occupations/occupation.factory';
 import { ASSEMBLER_FACTORY as ASSEMBLER } from './@core/assembler/assembler.factory';
 import { PERSON_FACTORY as PERSON } from './@core/person/person.factory';
+import { HIGHLIGHT_FACTORY as HIGHLIGHT } from './@core/highlight/highlight.factory';
 
 export const MODULES = {
   FONTS,
@@ -14,4 +15,5 @@ export const MODULES = {
   OCCUPATION,
   ASSEMBLER,
   PERSON,
+  HIGHLIGHT,
 };
