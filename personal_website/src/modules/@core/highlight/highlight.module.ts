a2be6fee41639b4e944760c93fc02be89d1c7822
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-24 08:05:58 -0300

    [ :syringe: ] | update: module - highlight > service (module::core)

--- personal_website/src/modules/@core/highlight/highlight.module.ts
+++ personal_website/src/modules/@core/highlight/highlight.module.ts
@@ -1,6 +1,7 @@
 import { Container } from 'inversify';
 import { HIGHLIGHT_REGISTRY } from './highlight.registry';
 import { FindAllHighlightsUseCase } from './use_case/find/all.use_case';
+import { HighlightService } from './service/highlight.service';
 
 const MODULE = new Container({
   defaultScope: 'Singleton',
@@ -12,3 +13,5 @@ export const HIGHLIGHT_MODULE = MODULE;
 HIGHLIGHT_MODULE.bind(HIGHLIGHT_REGISTRY.USE_CASE.FIND.ALL).to(
   FindAllHighlightsUseCase,
 );
+
+HIGHLIGHT_MODULE.bind(HIGHLIGHT_REGISTRY.SERVICE.MAIN).to(HighlightService);
