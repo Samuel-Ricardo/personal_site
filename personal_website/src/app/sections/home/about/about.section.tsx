0486ef576417c77a7f4e5e3a8048e8674d678c1f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-24 11:46:03 -0300

    [ :newspaper: ] | update: section - about (app::component::cms)

--- personal_website/src/app/sections/home/about/about.section.tsx
+++ personal_website/src/app/sections/home/about/about.section.tsx
@@ -1,12 +1,15 @@
 import './about.style.scss';
 import { AboutMeTitle } from '@/components/section/home/about/title/title.component';
 import { AboutMeContent } from '@/components/section/home/about/content/content.component';
+import { MODULES } from '@/modules/app.factory';
 
-export const AboutMe = () => {
+export const AboutMe = async () => {
+  const { title, image, subtitle, paragraph, highlights } =
+    await MODULES.ASSEMBLER.MAIN().assembleAbout();
   return (
     <section id="about-me" className="section">
-      <AboutMeTitle />
-      <AboutMeContent />
+      <AboutMeTitle>{await title}</AboutMeTitle>
+      <AboutMeContent data={{ image, subtitle, paragraph, highlights }} />
     </section>
   );
 };
