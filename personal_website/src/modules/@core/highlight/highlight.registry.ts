b675cf3397cc7a79f5056a8ea06c63a0a28c1fe0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-24 08:05:07 -0300

    [ :syringe: ] | update: registry - highlight > service (module::core)

--- personal_website/src/modules/@core/highlight/highlight.registry.ts
+++ personal_website/src/modules/@core/highlight/highlight.registry.ts
@@ -1,4 +1,7 @@
 export const HIGHLIGHT_REGISTRY = {
+  SERVICE: {
+    MAIN: Symbol.for('MODULE::HIGHLIGHT::SERVICE::MAIN'),
+  },
   USE_CASE: {
     FIND: {
       ALL: Symbol.for('MODULE::USE_CASE::CMS::HIGHLIGHT::FIND::ALL'),
--- personal_website/src/modules/gateway/CMS/sanity/highlight/highlight.gateway.ts
+++ personal_website/src/modules/gateway/CMS/sanity/highlight/highlight.gateway.ts
@@ -1,16 +1,26 @@
 import { IHighlightGateway } from '@/modules/@core/highlight/gateway/highlight.gateway';
 import { SanitySupport } from '../sanity.gateway';
 import { injectable } from 'inversify';
-import { IHighlightDTO } from '@/modules/@core/highlight/gateway/DTO/highlight.dto';
+import { Highlight } from '@/modules/@core/highlight/entity/highlight.entity';
 
 @injectable()
 export class SanityHighlightGateway
   extends SanitySupport
   implements IHighlightGateway
 {
-  findAll(): Promise<IHighlightDTO[]> {
-    return this.client.fetch(
+  async findAll(): Promise<Highlight[]> {
+    const result: any[] = await this.client.fetch(
       `*[_type == "hightlight" && lang == 'en' ]{identifier, positon, title, numbers, k, description} | order(positon asc)`,
+    );
+
+    return Highlight.fromDTOList(
+      result.map(res => ({
+        title: res.title,
+        description: res.description,
+        numbers: res.numbers,
+        k: res.k,
+        identifier: res.identifier,
+      })),
     );
   }
 }
