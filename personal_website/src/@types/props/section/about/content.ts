0594b12c5e5eb7f397970f17876319624955da3a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-24 11:32:50 -0300

    [ :label: ] | update: props - about > content (@types::props)

--- personal_website/src/@types/props/section/about/content.ts
+++ personal_website/src/@types/props/section/about/content.ts
@@ -1,5 +1,12 @@
 import { IAssembleAboutDTO } from '@/modules/@core/assembler/DTO/service/assemble/about.dto';
+import { Highlight } from '@/modules/@core/highlight/entity/highlight.entity';
 
 export interface IAboutContentProps {
   data: Omit<IAssembleAboutDTO, 'title'>;
+}
+
+export interface IAboutContentText {
+  title: Promise<string | undefined>;
+  paragraph: Promise<string | undefined>;
+  highlights: Promise<Highlight[] | undefined>;
 }
