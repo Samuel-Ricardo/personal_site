7ce198dcdeb64ebb406a128c5612d8c7a2006c40
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-24 08:13:36 -0300

    [ :syringe: ] | update: factory - highlight > controller (module::core)

--- personal_website/src/modules/@core/highlight/highlight.factory.ts
+++ personal_website/src/modules/@core/highlight/highlight.factory.ts
@@ -1,9 +1,16 @@
+import { HighlightController } from './controller/highlight.controller';
 import { HIGHLIGHT_MODULE } from './highlight.module';
 import { HIGHLIGHT_REGISTRY } from './highlight.registry';
 import { HighlightService } from './service/highlight.service';
 import { FindAllHighlightsUseCase } from './use_case/find/all.use_case';
 
 export const HIGHLIGHT_FACTORY = {
+  MAIN: () =>
+    HIGHLIGHT_MODULE.get<HighlightController>(HIGHLIGHT_REGISTRY.MAIN),
+  CONTROLLER: {
+    MAIN: () =>
+      HIGHLIGHT_MODULE.get<HighlightController>(HIGHLIGHT_REGISTRY.MAIN),
+  },
   SERVICE: {
     MAIN: () =>
       HIGHLIGHT_MODULE.get<HighlightService>(HIGHLIGHT_REGISTRY.SERVICE.MAIN),
