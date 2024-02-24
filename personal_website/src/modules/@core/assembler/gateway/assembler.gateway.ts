88615956c4a8715967c863afc2e6bd4d548c2a66
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-24 08:19:43 -0300

    [ :door: ] | update: gateway - assembler > findImage (module::core)

--- personal_website/src/modules/@core/assembler/gateway/assembler.gateway.ts
+++ personal_website/src/modules/@core/assembler/gateway/assembler.gateway.ts
@@ -3,4 +3,5 @@ import { IAssemblerFindDTO } from '../DTO/gateway/find/index.dto';
 export interface IAssemblerGateway {
   findTitle(DTO: IAssemblerFindDTO): Promise<string | undefined>;
   findText(DTO: IAssemblerFindDTO): Promise<string | undefined>;
+  findImage(DTO: IAssemblerFindDTO): Promise<string | undefined>;
 }
