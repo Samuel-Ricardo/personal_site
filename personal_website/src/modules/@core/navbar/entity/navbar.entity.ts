3e9c864b795ce54ed933aba9f98a96c64ae1b64c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-24 16:34:44 -0300

    [ :sparkles: ] | create: entity - navbar (module::core)

---
+++ personal_website/src/modules/@core/navbar/entity/navbar.entity.ts
@@ -0,0 +1,8 @@
+export class Navbar {
+  constructor(
+    protected readonly _identifier: string,
+    protected readonly _title: string,
+    protected readonly _link: string,
+    protected readonly _position: number,
+  ) {}
+}
