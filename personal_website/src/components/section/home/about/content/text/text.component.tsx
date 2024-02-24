e7cbda4539aefa7c8defcfaf5155e31e8ca94862
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-24 11:35:10 -0300

    [ :newspaper: ] | update: section - about > content (app::component::cms)

--- personal_website/src/components/section/home/about/content/text/text.component.tsx
+++ personal_website/src/components/section/home/about/content/text/text.component.tsx
@@ -1,53 +1,29 @@
-'use client';
-
 import { HighlightCard } from '@/components/card/highlight/card.component';
 import './text.style.scss';
 import { SocialProof } from './social_proof/proof.component';
-import { GithubLogo } from '@/assets/icons/logo/github/icon.component';
 import { MODULES } from '@/modules/app.factory';
-import { StarIcon } from '@/assets/icons/star/icon.component';
+import { IAboutContentText } from '@/@types/props/section/about/content';
 
-export const AbouteMeContentText = () => {
+export const AbouteMeContentText = async ({
+  title,
+  paragraph,
+  highlights,
+}: IAboutContentText) => {
   const animate = MODULES.ANIMATION.FRAMER_MOTION.SVG.DRAW();
-  const drawAndFill = animate({
-    initial: { pathLength: 0, fillOpacity: 0 },
-    animate: {
-      pathLength: 1.1,
-      fillOpacity: 0.6,
-    },
-    transition: { duration: 4, ease: 'easeInOut' },
-  });
   return (
     <div className="text-container">
-      <h2 className="text-title">ABOUT ME</h2>
-
-      <p className="text">
-        Loren ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
+      <h2 className="text-title">{await title}</h2>
+      <p className="text">{await paragraph} </p>
 
       <SocialProof>
-        <HighlightCard
-          title="Projects"
-          numbers={130}
-          Icon={<GithubLogo Paths={drawAndFill} />}
-        />
-        <HighlightCard
-          title="Stars"
-          numbers={120}
-          Icon={<StarIcon Paths={drawAndFill} />}
-        />
-        <HighlightCard title="Commits" numbers={16} k />
-        <HighlightCard title="Coding at" numbers={5} description="Years" />
-        <HighlightCard title="LikedIn Follows" numbers={500} />
-        <HighlightCard
-          title="Impact On LikedIn"
-          numbers={72}
-          k
-          description="over the year"
-        />
-        <HighlightCard title="Certificates" numbers={120} />
-        <HighlightCard title="Articles" numbers={10} />
+        {(await highlights)?.map(highlight => (
+          <HighlightCard
+            key={highlight.title}
+            title={highlight.title}
+            numbers={highlight.numbers}
+            description={highlight.description}
+          />
+        ))}
       </SocialProof>
     </div>
   );
