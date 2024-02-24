dfaf1b621f3e39aa8a60d2ae0e37b27404a4173e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-24 08:07:24 -0300

    [ :syringe: ] | update: factory - highlight > service (module::core)

--- personal_website/src/modules/@core/highlight/highlight.factory.ts
+++ personal_website/src/modules/@core/highlight/highlight.factory.ts
@@ -1,7 +1,19 @@
+import { HIGHLIGHT_MODULE } from './highlight.module';
+import { HIGHLIGHT_REGISTRY } from './highlight.registry';
+import { HighlightService } from './service/highlight.service';
+import { FindAllHighlightsUseCase } from './use_case/find/all.use_case';
+
 export const HIGHLIGHT_FACTORY = {
+  SERVICE: {
+    MAIN: () =>
+      HIGHLIGHT_MODULE.get<HighlightService>(HIGHLIGHT_REGISTRY.SERVICE.MAIN),
+  },
   USE_CASE: {
     FIND: {
-      ALL: Symbol.for('MODULE::USE_CASE::CMS::HIGHLIGHT::FIND::ALL'),
+      ALL: () =>
+        HIGHLIGHT_MODULE.get<FindAllHighlightsUseCase>(
+          HIGHLIGHT_REGISTRY.USE_CASE.FIND.ALL,
+        ),
     },
   },
 };
