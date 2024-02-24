6edd26dd41b68cabd4fe10cd56fdefadda8e44eb
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-24 16:35:25 -0300

    [ :sparkles: ] | create: entity - navbar > getter (module::core)

--- personal_website/src/modules/@core/navbar/entity/navbar.entity.ts
+++ personal_website/src/modules/@core/navbar/entity/navbar.entity.ts
@@ -5,4 +5,20 @@ export class Navbar {
     protected readonly _link: string,
     protected readonly _position: number,
   ) {}
+
+  get identifier(): string {
+    return this._identifier;
+  }
+
+  get title(): string {
+    return this._title;
+  }
+
+  get link(): string {
+    return this._link;
+  }
+
+  get position(): number {
+    return this._position;
+  }
 }
