1e1061321d474914e2ac76f9c2ddbff7dfccfdac
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-24 09:59:37 -0300

    [ :label: ] | update: props - about > content (@types::props)

--- personal_website/src/@types/props/section/about/content.ts
+++ personal_website/src/@types/props/section/about/content.ts
@@ -1,3 +1,5 @@
 import { IAssembleAboutDTO } from '@/modules/@core/assembler/DTO/service/assemble/about.dto';
 
-export interface IAboutContentProps extends IAssembleAboutDTO {}
+export interface IAboutContentProps {
+  data: Omit<IAssembleAboutDTO, 'title'>;
+}
