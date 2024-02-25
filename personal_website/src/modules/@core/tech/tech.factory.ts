21b103af48945c43f59bcb514ff4435a0cd2fb77
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-25 10:43:03 -0300

    [ :syringe: ] | update: factory - tech > service (module::core)

--- personal_website/src/modules/@core/tech/tech.factory.ts
+++ personal_website/src/modules/@core/tech/tech.factory.ts
@@ -1,8 +1,12 @@
+import { TechService } from './service/tech.service';
 import { TECH_MODULE } from './tech.module';
 import { TECH_REGISTRY } from './tech.registry';
 import { FindAllTechsUseCase } from './use_case/find/all.use_case';
 
 export const TECH_FACTORY = {
+  SERVICE: {
+    MAIN: () => TECH_MODULE.get<TechService>(TECH_REGISTRY.SERVICE.MAIN),
+  },
   USE_CASE: {
     FIND: {
       ALL: () =>
