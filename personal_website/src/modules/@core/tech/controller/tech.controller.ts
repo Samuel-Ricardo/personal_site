0597b5918eefb454737d59e31e2e230a76101467
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-25 10:48:38 -0300

    [ :video_game: ] | create: controller - tech (module::core)

---
+++ personal_website/src/modules/@core/tech/controller/tech.controller.ts
@@ -0,0 +1,16 @@
+import { inject, injectable } from 'inversify';
+import { ITechModule } from '../tech.interface';
+import { type ITechService } from '../service/service.interface';
+import { MODULE } from '@/modules/app.registry';
+
+@injectable()
+export class TechController implements ITechModule {
+  constructor(
+    @inject(MODULE.TECH.SERVICE.MAIN)
+    protected readonly service: ITechService,
+  ) {}
+
+  async findAll() {
+    return this.service.findAll();
+  }
+}
