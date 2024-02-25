f5044993992283704a4061e3cee1033747f6622e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-25 10:55:15 -0300

    [ :syringe: ] | update: factory - tech > copntroller (module::core)

--- personal_website/src/modules/@core/tech/tech.factory.ts
+++ personal_website/src/modules/@core/tech/tech.factory.ts
@@ -1,9 +1,15 @@
+import { TechController } from './controller/tech.controller';
 import { TechService } from './service/tech.service';
+import { ITechModule } from './tech.interface';
 import { TECH_MODULE } from './tech.module';
 import { TECH_REGISTRY } from './tech.registry';
 import { FindAllTechsUseCase } from './use_case/find/all.use_case';
 
 export const TECH_FACTORY = {
+  MAIN: () => TECH_MODULE.get<ITechModule>(TECH_REGISTRY.MAIN),
+  CONTROLLER: {
+    MAIN: () => TECH_MODULE.get<TechController>(TECH_REGISTRY.CONTROLLER.MAIN),
+  },
   SERVICE: {
     MAIN: () => TECH_MODULE.get<TechService>(TECH_REGISTRY.SERVICE.MAIN),
   },
