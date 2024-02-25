44a2ccdebd9879247a1a647b8023d82463cf4be7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-25 10:41:59 -0300

    [ :syringe: ] | update: registry - tech > service (module::core)

--- personal_website/src/modules/@core/tech/tech.registry.ts
+++ personal_website/src/modules/@core/tech/tech.registry.ts
@@ -1,4 +1,7 @@
 export const TECH_REGISTRY = {
+  SERVICE: {
+    MAIN: Symbol.for('MODULE::TECH::SERVICE::MAIN'),
+  },
   USE_CASE: {
     FIND: {
       ALL: Symbol.for('MODULE::TECH::USE_CASE::FIND::ALL'),
