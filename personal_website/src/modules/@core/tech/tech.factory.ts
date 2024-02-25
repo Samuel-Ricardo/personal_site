e051976fcf4941df41d5015f42022a0911d4808e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-25 16:16:23 -0300

    [ :syringe: ] | update: factory - tech > use-case (module::core)

--- personal_website/src/modules/@core/tech/tech.factory.ts
+++ personal_website/src/modules/@core/tech/tech.factory.ts
@@ -4,6 +4,7 @@ import { ITechModule } from './tech.interface';
 import { TECH_MODULE } from './tech.module';
 import { TECH_REGISTRY } from './tech.registry';
 import { FindAllTechsUseCase } from './use_case/find/all.use_case';
+import { FindTechByContextUseCase } from './use_case/find/by/context.use_case';
 
 export const TECH_FACTORY = {
   MAIN: () => TECH_MODULE.get<ITechModule>(TECH_REGISTRY.MAIN),
@@ -17,6 +18,12 @@ export const TECH_FACTORY = {
     FIND: {
       ALL: () =>
         TECH_MODULE.get<FindAllTechsUseCase>(TECH_REGISTRY.USE_CASE.FIND.ALL),
+      BY: {
+        CONTEXT: () =>
+          TECH_MODULE.get<FindTechByContextUseCase>(
+            TECH_REGISTRY.USE_CASE.FIND.BY.CONTEXT,
+          ),
+      },
     },
   },
 };
