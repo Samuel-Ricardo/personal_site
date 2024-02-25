74cb540b1f9ce930c544ac872fff9d9027c968b5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-25 10:54:37 -0300

    [ :syringe: ] | update: module - tech > copntroller (module::core)

--- personal_website/src/modules/@core/tech/tech.module.ts
+++ personal_website/src/modules/@core/tech/tech.module.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { FindAllTechsUseCase } from './use_case/find/all.use_case';
 import { TECH_REGISTRY } from './tech.registry';
 import { TechService } from './service/tech.service';
+import { TechController } from './controller/tech.controller';
 
 const MODULE = new Container({
   autoBindInjectable: true,
@@ -13,3 +14,6 @@ export const TECH_MODULE = MODULE;
 TECH_MODULE.bind(TECH_REGISTRY.USE_CASE.FIND.ALL).to(FindAllTechsUseCase);
 
 TECH_MODULE.bind(TECH_REGISTRY.SERVICE.MAIN).to(TechService);
+
+TECH_MODULE.bind(TECH_REGISTRY.CONTROLLER.MAIN).to(TechController);
+TECH_MODULE.bind(TECH_REGISTRY.MAIN).to(TechController);
