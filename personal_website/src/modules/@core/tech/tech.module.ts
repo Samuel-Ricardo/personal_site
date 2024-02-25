787a8308a242e10b6b56d70d802abf3bce85bc87
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-25 16:15:59 -0300

    [ :syringe: ] | update: module - tech > use-case (module::core)

--- personal_website/src/modules/@core/tech/tech.module.ts
+++ personal_website/src/modules/@core/tech/tech.module.ts
@@ -3,6 +3,7 @@ import { FindAllTechsUseCase } from './use_case/find/all.use_case';
 import { TECH_REGISTRY } from './tech.registry';
 import { TechService } from './service/tech.service';
 import { TechController } from './controller/tech.controller';
+import { FindTechByContextUseCase } from './use_case/find/by/context.use_case';
 
 const MODULE = new Container({
   autoBindInjectable: true,
@@ -12,6 +13,9 @@ const MODULE = new Container({
 export const TECH_MODULE = MODULE;
 
 TECH_MODULE.bind(TECH_REGISTRY.USE_CASE.FIND.ALL).to(FindAllTechsUseCase);
+TECH_MODULE.bind(TECH_REGISTRY.USE_CASE.FIND.BY.CONTEXT).to(
+  FindTechByContextUseCase,
+);
 
 TECH_MODULE.bind(TECH_REGISTRY.SERVICE.MAIN).to(TechService);
 
