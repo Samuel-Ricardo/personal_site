2840638b5192f1c36b4c046847e582854fe05e0b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-25 16:15:32 -0300

    [ :syringe: ] | update: registry - tech > use-case (module::core)

--- personal_website/src/modules/@core/tech/tech.registry.ts
+++ personal_website/src/modules/@core/tech/tech.registry.ts
@@ -9,6 +9,9 @@ export const TECH_REGISTRY = {
   USE_CASE: {
     FIND: {
       ALL: Symbol.for('MODULE::TECH::USE_CASE::FIND::ALL'),
+      BY: {
+        CONTEXT: Symbol.for('MODULE::TECH::USE_CASE::FIND::BY::CONTEXT'),
+      },
     },
   },
 };
