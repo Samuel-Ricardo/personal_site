b436b88b43b2d96d5a253dac13e3f631a0df4ab1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-26 16:47:05 -0300

    [ :sparkles: ] | create: entity - project > getters (module::core)

--- personal_website/src/modules/@core/project/entity/project.entity.ts
+++ personal_website/src/modules/@core/project/entity/project.entity.ts
@@ -14,4 +14,36 @@ export class Project {
     private _link?: string,
     private _main?: boolean,
   ) {}
+
+  get title() {
+    return this._title;
+  }
+
+  get preview() {
+    return this._preview;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get repository() {
+    return this._repository;
+  }
+
+  get demo() {
+    return this._demo;
+  }
+
+  get link() {
+    return this._link;
+  }
+
+  get techs() {
+    return this._techs;
+  }
+
+  get main() {
+    return this._main;
+  }
 }
