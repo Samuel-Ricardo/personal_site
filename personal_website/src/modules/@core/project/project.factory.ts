ddb00395896246b2ed478c09bec3b79b1aa9804a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-26 16:20:26 -0300

    [ :syringe: ] | update: factory - project > controller (module::core)

--- personal_website/src/modules/@core/project/project.factory.ts
+++ personal_website/src/modules/@core/project/project.factory.ts
@@ -1,3 +1,5 @@
+import { ProjectController } from './controller/project.controller';
+import { IProjectModule } from './project.interface';
 import { PROJECT_MODULE } from './project.module';
 import { PROJECT_REGISTRY } from './project.registry';
 import { IProjectService } from './service/service.interface';
@@ -5,6 +7,11 @@ import { FindAllProjectsUseCase } from './use_case/find/all.use_case';
 import { FindMainProjectsUseCase } from './use_case/find/main.use_case';
 
 export const PROJECT_FACTORY = {
+  MAIN: () => PROJECT_MODULE.get<IProjectModule>(PROJECT_REGISTRY.MAIN),
+  CONTROLLER: {
+    MAIN: () =>
+      PROJECT_MODULE.get<ProjectController>(PROJECT_REGISTRY.CONTROLLER.MAIN),
+  },
   SERVICE: {
     MAIN: () =>
       PROJECT_MODULE.get<IProjectService>(PROJECT_REGISTRY.SERVICE.MAIN),
