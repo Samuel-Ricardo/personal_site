44d3a102ad1c5aca50d726f213f0ee639131439b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-26 11:18:54 -0300

    [ :syringe: ] | create: registry - project (module::core)

---
+++ personal_website/src/modules/@core/project/project.registry.ts
@@ -0,0 +1,8 @@
+export const PROJECT_REGISTRY = {
+  USE_CASE: {
+    FIND: {
+      ALL: Symbol.for('MODULE::USE_CASE::FIND::ALL'),
+      MAIN: Symbol.for('MODULE::USE_CASE::FIND::MAIN'),
+    },
+  },
+};
