738b3fd3298a1281a0550ec21300cf010ccfd227
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-26 15:43:32 -0300

    [ :syringe: ] | update: module - project > service (module::core)

--- personal_website/src/modules/@core/project/project.module.ts
+++ personal_website/src/modules/@core/project/project.module.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { PROJECT_REGISTRY } from './project.registry';
 import { FindAllProjectsUseCase } from './use_case/find/all.use_case';
 import { FindMainProjectsUseCase } from './use_case/find/main.use_case';
+import { ProjectService } from './service/project.service';
 
 const MODULE = new Container({
   defaultScope: 'Singleton',
@@ -16,3 +17,5 @@ PROJECT_MODULE.bind(PROJECT_REGISTRY.USE_CASE.FIND.ALL).to(
 PROJECT_MODULE.bind(PROJECT_REGISTRY.USE_CASE.FIND.MAIN).to(
   FindMainProjectsUseCase,
 );
+
+PROJECT_MODULE.bind(PROJECT_REGISTRY.SERVICE.MAIN).to(ProjectService);
