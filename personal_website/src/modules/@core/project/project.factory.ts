914b2dc124069eb2add999be119dcf3a391f5d80
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-26 15:44:32 -0300

    [ :syringe: ] | update: factory - project > service (module::core)

--- personal_website/src/modules/@core/project/project.factory.ts
+++ personal_website/src/modules/@core/project/project.factory.ts
@@ -1,9 +1,14 @@
 import { PROJECT_MODULE } from './project.module';
 import { PROJECT_REGISTRY } from './project.registry';
+import { IProjectService } from './service/service.interface';
 import { FindAllProjectsUseCase } from './use_case/find/all.use_case';
 import { FindMainProjectsUseCase } from './use_case/find/main.use_case';
 
 export const PROJECT_FACTORY = {
+  SERVICE: {
+    MAIN: () =>
+      PROJECT_MODULE.get<IProjectService>(PROJECT_REGISTRY.SERVICE.MAIN),
+  },
   USE_CASE: {
     FIND: {
       ALL: () =>
