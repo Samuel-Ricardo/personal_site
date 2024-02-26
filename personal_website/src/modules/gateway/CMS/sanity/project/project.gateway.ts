a863ea15c4fac0c01255325b6fda0c3ec29ec486
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-26 10:46:18 -0300

    [ :door: ] | create: gateway - project (module::cms::sanity)

---
+++ personal_website/src/modules/gateway/CMS/sanity/project/project.gateway.ts
@@ -0,0 +1,29 @@
+import { IProjectGateway } from '@/modules/@core/project/gateway/project.gateway';
+import { SanitySupport } from '../sanity.gateway';
+import { injectable } from 'inversify';
+import { Project } from '@/modules/@core/project/entity/project.entity';
+
+@injectable()
+export class SanityProjectGateway
+  extends SanitySupport
+  implements IProjectGateway
+{
+  async findAll() {
+    const result = await this.client.fetch(`
+      *[_type == "project"] {
+        
+        main,
+        title,
+        preview,
+        description,
+        repository,
+        demo,
+        link, 
+        'techs': techs[]{identifier, star}
+
+      }
+    `);
+
+    return Project.fromDTOs(result);
+  }
+}
