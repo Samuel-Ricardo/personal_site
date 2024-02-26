62cc72a0ffa0ca2b78b95ce9eb2e50055bfcdba9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-26 15:42:38 -0300

    [ :syringe: ] | update: registry - project > service (module::core)

--- personal_website/src/modules/@core/project/project.registry.ts
+++ personal_website/src/modules/@core/project/project.registry.ts
@@ -1,4 +1,7 @@
 export const PROJECT_REGISTRY = {
+  SERVICE: {
+    MAIN: Symbol.for('MODULE::SERVICE::MAIN'),
+  },
   USE_CASE: {
     FIND: {
       ALL: Symbol.for('MODULE::USE_CASE::FIND::ALL'),
