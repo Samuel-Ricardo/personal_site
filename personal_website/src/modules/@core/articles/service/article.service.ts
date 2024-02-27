eae32c4d5e2d1aea74ae6dbb01fea7c888d0f779
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 16:32:54 -0300

    [ :sparkles: ] | create: service - article (module::core)

---
+++ personal_website/src/modules/@core/articles/service/article.service.ts
@@ -0,0 +1,15 @@
+import { inject, injectable } from 'inversify';
+import { IArticleService } from './service.interface';
+import { FindAllArticleUseCase } from '../use_case/find/all.use_case';
+import { FindMainArticleUseCase } from '../use_case/find/main.use_case';
+import { MODULE } from '@/modules/app.registry';
+
+@injectable()
+export class ArticleService implements IArticleService {
+  constructor(
+    @inject(MODULE.ARTICLE.USE_CASE.FIND.ALL)
+    protected readonly _findAll: FindAllArticleUseCase,
+    @inject(MODULE.ARTICLE.USE_CASE.FIND.MAIN)
+    protected readonly _findMain: FindMainArticleUseCase,
+  ) {}
+}
