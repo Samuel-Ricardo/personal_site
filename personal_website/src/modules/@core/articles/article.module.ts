aee1d590efb376c8694f78a2bb51c92f12bc9d52
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 19:31:07 -0300

    [ :syringe: ] | update: module - article > controller (module::core)

--- personal_website/src/modules/@core/articles/article.module.ts
+++ personal_website/src/modules/@core/articles/article.module.ts
@@ -3,6 +3,7 @@ import { ARTICLE_REGISTRY } from './article.registry';
 import { FindAllArticleUseCase } from './use_case/find/all.use_case';
 import { FindMainArticleUseCase } from './use_case/find/main.use_case';
 import { ArticleService } from './service/article.service';
+import { ArticleController } from './controller/article.controller';
 
 export const ARTICLE_MODULE = new Container({
   defaultScope: 'Singleton',
@@ -17,3 +18,7 @@ ARTICLE_MODULE.bind(ARTICLE_REGISTRY.USE_CASE.FIND.MAIN).to(
 );
 
 ARTICLE_MODULE.bind(ARTICLE_REGISTRY.SERVICE.MAIN).to(ArticleService);
+
+ARTICLE_MODULE.bind(ARTICLE_REGISTRY.CONTROLLER.MAIN).to(ArticleController);
+
+ARTICLE_MODULE.bind(ARTICLE_REGISTRY.MAIN).to(ArticleController);
