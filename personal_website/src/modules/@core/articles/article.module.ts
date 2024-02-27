4d7940bfc647698cf1acf2f11d260859153b7baa
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 16:51:51 -0300

    [ :syringe: ] | update: module - article > service (module::core)

--- personal_website/src/modules/@core/articles/article.module.ts
+++ personal_website/src/modules/@core/articles/article.module.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { ARTICLE_REGISTRY } from './article.registry';
 import { FindAllArticleUseCase } from './use_case/find/all.use_case';
 import { FindMainArticleUseCase } from './use_case/find/main.use_case';
+import { ArticleService } from './service/article.service';
 
 export const ARTICLE_MODULE = new Container({
   defaultScope: 'Singleton',
@@ -14,3 +15,5 @@ ARTICLE_MODULE.bind(ARTICLE_REGISTRY.USE_CASE.FIND.ALL).to(
 ARTICLE_MODULE.bind(ARTICLE_REGISTRY.USE_CASE.FIND.MAIN).to(
   FindMainArticleUseCase,
 );
+
+ARTICLE_MODULE.bind(ARTICLE_REGISTRY.SERVICE.MAIN).to(ArticleService);
