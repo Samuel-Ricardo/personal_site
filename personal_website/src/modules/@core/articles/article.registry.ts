a96a9ffa65559523d1163a6947746be29bca4104
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 16:20:05 -0300

    [ :syringe: ] | create: registry - article (module::core)

---
+++ personal_website/src/modules/@core/articles/article.registry.ts
@@ -0,0 +1,8 @@
+export const ARTICLE_REGISTRY = {
+  USE_CASE: {
+    FIND: {
+      ALL: Symbol.for('MODULE::ARTICLE:USE_CASE:FIND:ALL'),
+      MAIN: Symbol.for('MODULE::ARTICLE:USE_CASE:FIND:MAIN'),
+    },
+  },
+};
