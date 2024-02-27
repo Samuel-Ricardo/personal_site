063e7adccc15b4fa55ddb29376df2fcab2c0cebd
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 16:41:20 -0300

    [ :syringe: ] | update: registry - article > service (module::core)

--- personal_website/src/modules/@core/articles/article.registry.ts
+++ personal_website/src/modules/@core/articles/article.registry.ts
@@ -1,4 +1,7 @@
 export const ARTICLE_REGISTRY = {
+  SERVICE: {
+    MAIN: Symbol.for('MODULE::ARTICLE:SERVICE:MAIN'),
+  },
   USE_CASE: {
     FIND: {
       ALL: Symbol.for('MODULE::ARTICLE:USE_CASE:FIND:ALL'),
