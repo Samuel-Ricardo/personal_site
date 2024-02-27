b35f0dc43a1cfe34c924d0416fee74f710bf8e3e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 10:38:23 -0300

    [ :door: ] | create: gateway - platform (module::core)

---
+++ personal_website/src/modules/@core/platforms/gateway/platform.gateway.ts
@@ -0,0 +1,7 @@
+import { IFindOnePlatformDTO } from '../DTO/gateway/find/one.dto';
+import { Platform } from '../entity/platform.entity';
+
+export interface IPlatformGateway {
+  findAll(): Promise<Platform>;
+  findOne(DTO: IFindOnePlatformDTO): Promise<Platform>;
+}
