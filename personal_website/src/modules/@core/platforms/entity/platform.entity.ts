015474dc22d571ef63cd498d8359d0c2df3270f3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 10:28:55 -0300

    [ :label: ] | create: entity - platform > getters (module::core)

--- personal_website/src/modules/@core/platforms/entity/platform.entity.ts
+++ personal_website/src/modules/@core/platforms/entity/platform.entity.ts
@@ -1,7 +1,19 @@
 export class Platform {
   constructor(
-    private readonly name: string,
-    private readonly icon: string,
-    private readonly link: string,
+    private readonly _name: string,
+    private readonly _icon: string,
+    private readonly _link: string,
   ) {}
+
+  get name() {
+    return this._name;
+  }
+
+  get icon() {
+    return this._icon;
+  }
+
+  get link() {
+    return this._link;
+  }
 }
