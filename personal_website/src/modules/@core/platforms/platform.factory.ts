f2b525c25f91ccdb75123a5a4cd7897e105ddd2c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 14:21:17 -0300

    [ :syringe: ] | create: factory - platform > controller (module::core)

--- personal_website/src/modules/@core/platforms/platform.factory.ts
+++ personal_website/src/modules/@core/platforms/platform.factory.ts
@@ -1,4 +1,8 @@
 export const PLATFORM_FACTORY = {
+  MAIN: Symbol.for('MODULE::PLATFORM::MAIN'),
+  CONTROLLER: {
+    MAIN: Symbol.for('MODULE::CONTROLLER:PLATFORM::MAIN'),
+  },
   SERVICE: {
     MAIN: Symbol.for('MODULE::SERVICE:PLATFORM::MAIN'),
   },
