c9b1accb27e26be2be3d82b8407cb8c89576fe88
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 11:59:40 -0300

    [ :syringe: ] | create: factory - platform > service (module::core)

--- personal_website/src/modules/@core/platforms/platform.factory.ts
+++ personal_website/src/modules/@core/platforms/platform.factory.ts
@@ -1,4 +1,7 @@
 export const PLATFORM_FACTORY = {
+  SERVICE: {
+    MAIN: Symbol.for('MODULE::SERVICE:PLATFORM::MAIN'),
+  },
   USE_CASE: {
     FIND: {
       ALL: Symbol.for('MODULE::USE_CASE:PLATFORM:FIND:ALL'),
