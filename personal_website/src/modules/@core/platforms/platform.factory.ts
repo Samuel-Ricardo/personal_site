35aaee788a473e4a36a803ca163b6569c2e7af05
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 14:23:09 -0300

    [ :syringe: ] | update: factory - platform (module::core)

--- personal_website/src/modules/@core/platforms/platform.factory.ts
+++ personal_website/src/modules/@core/platforms/platform.factory.ts
@@ -1,15 +1,30 @@
+import { IPlatformModule } from './platform.interace';
+import { PLATFORM_MODULE } from './platform.module';
+import { PLATFORM_REGISTRY } from './platform.registry';
+import { IPlatformService } from './service/service.interface';
+import { FindAllPlatformUseCase } from './use_case/find/all.use_case';
+import { FindOnePlatformUseCase } from './use_case/find/one.use_case';
+
 export const PLATFORM_FACTORY = {
-  MAIN: Symbol.for('MODULE::PLATFORM::MAIN'),
+  MAIN: () => PLATFORM_MODULE.get<IPlatformModule>(PLATFORM_REGISTRY.MAIN),
   CONTROLLER: {
-    MAIN: Symbol.for('MODULE::CONTROLLER:PLATFORM::MAIN'),
+    MAIN: () =>
+      PLATFORM_MODULE.get<IPlatformModule>(PLATFORM_REGISTRY.CONTROLLER.MAIN),
   },
   SERVICE: {
-    MAIN: Symbol.for('MODULE::SERVICE:PLATFORM::MAIN'),
+    MAIN: () =>
+      PLATFORM_MODULE.get<IPlatformService>(PLATFORM_REGISTRY.SERVICE.MAIN),
   },
   USE_CASE: {
     FIND: {
-      ALL: Symbol.for('MODULE::USE_CASE:PLATFORM:FIND:ALL'),
-      ONE: Symbol.for('MODULE::USE_CASE:PLATFORM:FIND:ONE'),
+      ALL: () =>
+        PLATFORM_MODULE.get<FindAllPlatformUseCase>(
+          PLATFORM_REGISTRY.USE_CASE.FIND.ALL,
+        ),
+      ONE: () =>
+        PLATFORM_MODULE.get<FindOnePlatformUseCase>(
+          PLATFORM_REGISTRY.USE_CASE.FIND.ONE,
+        ),
     },
   },
 };
