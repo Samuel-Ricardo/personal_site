f60ada9d7966f5f187aa096d789a65c5868be6d0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 17:16:52 -0300

    [ :syringe: ] | update: factory - article > service (module::core)

--- personal_website/src/modules/@core/articles/article.factory.ts
+++ personal_website/src/modules/@core/articles/article.factory.ts
@@ -1,9 +1,14 @@
 import { ARTICLE_MODULE } from './article.module';
 import { ARTICLE_REGISTRY } from './article.registry';
+import { IArticleService } from './service/service.interface';
 import { FindAllArticleUseCase } from './use_case/find/all.use_case';
 import { FindMainArticleUseCase } from './use_case/find/main.use_case';
 
 export const ARTICLE_FACTORY = {
+  SERVICE: {
+    MAIN: () =>
+      ARTICLE_MODULE.get<IArticleService>(ARTICLE_REGISTRY.SERVICE.MAIN),
+  },
   USE_CASE: {
     FIND: {
       ALL: () =>
