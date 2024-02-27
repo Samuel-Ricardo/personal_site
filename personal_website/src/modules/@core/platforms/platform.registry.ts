c6f681c3a6e38ef76e5baf39e13d5d594b2e341c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 11:58:38 -0300

    [ :syringe: ] | create: registry - platform > service (module::core)

--- personal_website/src/modules/@core/platforms/platform.registry.ts
+++ personal_website/src/modules/@core/platforms/platform.registry.ts
@@ -1,4 +1,7 @@
 export const PLATFORM_REGISTRY = {
+  SERVICE: {
+    MAIN: Symbol.for('MODULE::SERVICE:PLATFORM'),
+  },
   USE_CASE: {
     FIND: {
       ALL: Symbol.for('MODULE::USE_CASE:PLATFORM:FIND:ALL'),
