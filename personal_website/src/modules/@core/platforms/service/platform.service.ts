7d859f76fc1180afc830a710b6769ee4189401d9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 11:56:59 -0300

    [ :sparkles: ] | implements: service - platform > findAll (module::core)

--- personal_website/src/modules/@core/platforms/service/platform.service.ts
+++ personal_website/src/modules/@core/platforms/service/platform.service.ts
@@ -2,13 +2,18 @@ import { inject, injectable } from 'inversify';
 import { FindAllPlatformUseCase } from '../use_case/find/all.use_case';
 import { FindOnePlatformUseCase } from '../use_case/find/one.use_case';
 import { MODULE } from '@/modules/app.registry';
+import { IPlatformService } from './service.interface';
 
 @injectable()
-export class PlatformService {
+export class PlatformService implements IPlatformService {
   constructor(
     @inject(MODULE.PLATFORM.USE_CASE.FIND.ALL)
     protected readonly _findAll: FindAllPlatformUseCase,
     @inject(MODULE.PLATFORM.USE_CASE.FIND.ONE)
     protected readonly _findOne: FindOnePlatformUseCase,
   ) {}
+
+  async findAll() {
+    return this._findAll.execute();
+  }
 }
