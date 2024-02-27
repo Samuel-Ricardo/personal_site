401ddd4db8f2662681b0b2aa94fb1531c5b59470
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-27 10:29:53 -0300

    [ :label: ] | create: entity - platform > toDTO (module::core)

--- personal_website/src/modules/@core/platforms/entity/platform.entity.ts
+++ personal_website/src/modules/@core/platforms/entity/platform.entity.ts
@@ -1,9 +1,19 @@
+import { IPlatformDTO } from '../DTO/platform.dto';
+
 export class Platform {
   constructor(
     private readonly _name: string,
     private readonly _icon: string,
     private readonly _link: string,
   ) {}
+
+  toDTO(): IPlatformDTO {
+    return {
+      name: this._name,
+      icon: this._icon,
+      link: this._link,
+    };
+  }
 
   get name() {
     return this._name;
