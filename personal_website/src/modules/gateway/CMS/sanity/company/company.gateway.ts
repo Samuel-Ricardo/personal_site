3b74cdc98a171bd1260f26d558acf110111415b9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-28 12:16:39 -0300

    [ :door: ] | implements: gateway - company > findOne (module::cms::sanity)

--- personal_website/src/modules/gateway/CMS/sanity/company/company.gateway.ts
+++ personal_website/src/modules/gateway/CMS/sanity/company/company.gateway.ts
@@ -2,7 +2,7 @@ import { ICompanyGateway } from '@/modules/@core/company/gateway/company.gateway
 import { SanitySupport } from '../sanity.gateway';
 import { injectable } from 'inversify';
 import { Company } from '@/modules/@core/company/entity/commpany.entity';
-import { IFindOnePersonDTO } from '@/modules/@core/person/DTO/gateway/find/one.dto';
+import { IFindOneComapanyDTO } from '@/modules/@core/company/DTO/find/one.dto';
 
 @injectable()
 export class SanityCompanyGateway
@@ -15,7 +15,11 @@ export class SanityCompanyGateway
     );
   }
 
-  findOne(DTO: IFindOnePersonDTO): Promise<Company> {
-    throw new Error('Method not implemented.');
+  async findOne({ id }: IFindOneComapanyDTO): Promise<Company> {
+    return Company.fromDTO(
+      await this.client.fetch(
+        `*[_type == "company" && _id == "${id}"]{name,logo,link}`,
+      ),
+    );
   }
 }
