202eb86ef887325b8b5ea06794297d8fab27eea4
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-28 11:49:16 -0300

    [ :sparkles: ] | create: entity - company > getters (module::core)

--- personal_website/src/modules/@core/company/entity/commpany.entity.ts
+++ personal_website/src/modules/@core/company/entity/commpany.entity.ts
@@ -4,4 +4,16 @@ export class Company {
     private readonly _logo: string,
     private readonly _link: string,
   ) {}
+
+  get name() {
+    return this._name;
+  }
+
+  get logo() {
+    return this._logo;
+  }
+
+  get link() {
+    return this._link;
+  }
 }
