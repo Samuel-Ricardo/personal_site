de3a81b73d41a8300fff7e6d669b2bef29dc3331
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 15:42:35 -0300

    [ :syringe: ] | update: factory - experience > service (module::core)

--- personal_website/src/modules/@core/experience/experience.factory.ts
+++ personal_website/src/modules/@core/experience/experience.factory.ts
@@ -1,9 +1,16 @@
 import { EXPERIENCE_MODULE } from './experience.module';
 import { EXPERIENCE_REGISTRY } from './experience.registry';
+import { IExperienceService } from './service/service.interface';
 import { FindAllExperiencesUseCase } from './use_case/find/all.use_case';
 import { FindExperienceByContextUseCase } from './use_case/find/by/context.use_case';
 
 export const EXPERIENCE_FACTORY = {
+  SERVICE: {
+    MAIN: () =>
+      EXPERIENCE_MODULE.get<IExperienceService>(
+        EXPERIENCE_REGISTRY.SERVICE.MAIN,
+      ),
+  },
   USE_CASE: {
     FIND: {
       ALL: () =>
