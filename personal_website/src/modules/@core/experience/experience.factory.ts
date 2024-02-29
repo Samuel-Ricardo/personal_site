8811ed72bf86f417421e36494de9b69231f6734b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 17:50:26 -0300

    [ :syringe: ] | update: factory - experience > controller (module::core)

--- personal_website/src/modules/@core/experience/experience.factory.ts
+++ personal_website/src/modules/@core/experience/experience.factory.ts
@@ -1,3 +1,5 @@
+import { ExperienceController } from './controller/experience.controller';
+import { IExperienceModule } from './experience.interface';
 import { EXPERIENCE_MODULE } from './experience.module';
 import { EXPERIENCE_REGISTRY } from './experience.registry';
 import { IExperienceService } from './service/service.interface';
@@ -5,6 +7,14 @@ import { FindAllExperiencesUseCase } from './use_case/find/all.use_case';
 import { FindExperienceByContextUseCase } from './use_case/find/by/context.use_case';
 
 export const EXPERIENCE_FACTORY = {
+  MAIN: () =>
+    EXPERIENCE_MODULE.get<IExperienceModule>(EXPERIENCE_REGISTRY.MAIN),
+  CONTROLLER: {
+    MAIN: () =>
+      EXPERIENCE_MODULE.get<ExperienceController>(
+        EXPERIENCE_REGISTRY.CONTROLLER.MAIN,
+      ),
+  },
   SERVICE: {
     MAIN: () =>
       EXPERIENCE_MODULE.get<IExperienceService>(
