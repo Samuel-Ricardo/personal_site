329fc0313526b3c403efb1d0c0dc8cfb01fe5ca0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 15:10:57 -0300

    [ :syringe: ] | update: registry - app > experience (app::modules)

--- personal_website/src/modules/app.registry.ts
+++ personal_website/src/modules/app.registry.ts
@@ -11,6 +11,7 @@ import { PROJECT_REGISTRY as PROJECT } from './@core/project/project.registry';
 import { PLATFORM_REGISTRY as PLATFORM } from './@core/platforms/platform.registry';
 import { ARTICLE_REGISTRY as ARTICLE } from './@core/articles/article.registry';
 import { COMPANY_REGISTRY as COMPANY } from './@core/company/company.registry';
+import { EXPERIENCE_REGISTRY as EXPERIENCE } from './@core/experience/experience.registry';
 
 export const MODULE = {
   FONTS,
@@ -26,4 +27,5 @@ export const MODULE = {
   PLATFORM,
   ARTICLE,
   COMPANY,
+  EXPERIENCE,
 };
