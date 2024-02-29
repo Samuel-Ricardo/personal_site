e140daa1f1822209234cc9466ede20df18217ad6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 08:43:59 -0300

    [ :sparkles: ] | implements: service - company (module::core)

--- personal_website/src/modules/@core/company/service/company.service.ts
+++ personal_website/src/modules/@core/company/service/company.service.ts
@@ -1,12 +1,22 @@
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { ICompanyService } from './service.interface';
 import { FindAllCompanyUseCase } from '../use_case/find/all.use_case';
 import { FindOneCompanyUseCase } from '../use_case/find/one.use_case';
+import { IFindOneComapanyDTO } from '../DTO/find/one.dto';
+import { MODULE } from '@/modules/app.registry';
 
 @injectable()
 export class ComapnyService implements ICompanyService {
   constructor(
+    @inject(MODULE.COMPANY.USE_CASE.FIND.ALL)
     private readonly findAllCompanyUseCase: FindAllCompanyUseCase,
+    @inject(MODULE.COMPANY.USE_CASE.FIND.ONE)
     private readonly findOneCompanyUseCase: FindOneCompanyUseCase,
   ) {}
+  async findAll() {
+    return await this.findAllCompanyUseCase.execute();
+  }
+  async findOne(DTO: IFindOneComapanyDTO) {
+    return await this.findOneCompanyUseCase.execute(DTO);
+  }
 }
