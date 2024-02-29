61ea943f0dac852e93e81f0fcb1426fbe437c7a6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 15:41:18 -0300

    [ :syringe: ] | update: module - experience > service (module::core)

--- personal_website/src/modules/@core/experience/experience.module.ts
+++ personal_website/src/modules/@core/experience/experience.module.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { FindAllExperiencesUseCase } from './use_case/find/all.use_case';
 import { EXPERIENCE_REGISTRY } from './experience.registry';
 import { FindExperienceByContextUseCase } from './use_case/find/by/context.use_case';
+import { ExperienceService } from './service/experience.service';
 
 export const EXPERIENCE_MODULE = new Container({
   defaultScope: 'Singleton',
@@ -13,4 +14,8 @@ EXPERIENCE_MODULE.bind(EXPERIENCE_REGISTRY.USE_CASE.FIND.ALL).to(
 );
 EXPERIENCE_MODULE.bind(EXPERIENCE_REGISTRY.USE_CASE.FIND.BY.CONTEXT).to(
   FindExperienceByContextUseCase,
+);
+
+EXPERIENCE_MODULE.bind(EXPERIENCE_REGISTRY.SERVICE.MAIN).toConstantValue(
+  ExperienceService,
 );
