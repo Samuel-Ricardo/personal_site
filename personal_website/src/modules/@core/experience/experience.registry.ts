ba160778e01b37eeba13564ee5fde4b8360d15b4
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 15:03:15 -0300

    [ :syringe: ] | create: registry - experience (module::core)

---
+++ personal_website/src/modules/@core/experience/experience.registry.ts
@@ -0,0 +1,10 @@
+export const EXPERIENCE_REGISTRY = {
+  USE_CASE: {
+    FIND: {
+      ALL: Symbol.for('MODULE::USE_CASE::EXPERIENCE::FIND::ALL'),
+      BY: {
+        CONTEXT: Symbol.for('MODULE::USE_CASE::EXPERIENCE::FIND::BY::CONTEXT'),
+      },
+    },
+  },
+};
