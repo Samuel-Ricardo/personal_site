9b178538b21e64a9222d2f81ddb31bf2f24ae737
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 09:11:43 -0300

    [ :syringe: ] | update: factory - company > controller (module::core)

--- personal_website/src/modules/@core/company/company.factory.ts
+++ personal_website/src/modules/@core/company/company.factory.ts
@@ -1,3 +1,4 @@
+import { ICompanyModule } from './company.interface';
 import { COMPANY_MODULE } from './company.module';
 import { COMPANY_REGISTRY } from './company.registry';
 import { ICompanyService } from './service/service.interface';
@@ -5,6 +6,11 @@ import { FindAllCompanyUseCase } from './use_case/find/all.use_case';
 import { FindOneCompanyUseCase } from './use_case/find/one.use_case';
 
 export const COMPANY_FACTORY = {
+  MAIN: () => COMPANY_MODULE.get<ICompanyModule>(COMPANY_REGISTRY.MAIN),
+  CONTROLLER: {
+    MAIN: () =>
+      COMPANY_MODULE.get<ICompanyModule>(COMPANY_REGISTRY.CONTROLLER.MAIN),
+  },
   SERVICE: {
     MAIN: () =>
       COMPANY_MODULE.get<ICompanyService>(COMPANY_REGISTRY.SERVICE.MAIN),
