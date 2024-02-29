9a3022809f26a1c991694b9c0a8e6f05ccb2443c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 15:40:35 -0300

    [ :syringe: ] | update: registry - experience > service (module::core)

--- personal_website/src/modules/@core/experience/experience.registry.ts
+++ personal_website/src/modules/@core/experience/experience.registry.ts
@@ -1,4 +1,7 @@
 export const EXPERIENCE_REGISTRY = {
+  SERVICE: {
+    MAIN: Symbol.for('MODULE::SERVICE::EXPERIENCE'),
+  },
   USE_CASE: {
     FIND: {
       ALL: Symbol.for('MODULE::USE_CASE::EXPERIENCE::FIND::ALL'),
