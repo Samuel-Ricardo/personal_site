632da1eb2b0498f87fcc389f45e9863c87b69629
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 14:45:00 -0300

    [ :door: ] | implements: gateway - experience > find by [context] (module::cms::sanity)

--- personal_website/src/modules/gateway/CMS/sanity/experience/experience.gateway.ts
+++ personal_website/src/modules/gateway/CMS/sanity/experience/experience.gateway.ts
@@ -2,6 +2,7 @@ import { IExperienceGateway } from '@/modules/@core/experience/gateway/experienc
 import { SanitySupport } from '../sanity.gateway';
 import { injectable } from 'inversify';
 import { Experience } from '@/modules/@core/experience/entity/experience.entity';
+import { IFindExperienceByContextDTO } from '@/modules/@core/experience/DTO/find/by/context.dto';
 
 @injectable()
 export class SanityExperienceGateway
@@ -11,7 +12,17 @@ export class SanityExperienceGateway
   async findAll(): Promise<Experience[]> {
     return Experience.fromDTOs(
       await this.client.fetch(
-        `*[_type == "experience"]{ context, title, description, startDate, endDate, company -> {link,name,logo} }`,
+        `*[_type == "experience"]{ context, title, description, startDate, endDate, company -> {link,name,logo} }`,
+      ),
+    );
+  }
+
+  async findByContext({
+    context,
+  }: IFindExperienceByContextDTO): Promise<Experience[]> {
+    return Experience.fromDTOs(
+      await this.client.fetch(
+        `*[_type == "experience" && context == "${context}" ]{ context, title, description, startDate, endDate, company -> {link,name,logo} }`,
       ),
     );
   }
