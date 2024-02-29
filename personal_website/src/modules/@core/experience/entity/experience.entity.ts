2a52a1ec6bf6a5d0399ee97fd7eee8c1224812b2
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 11:51:52 -0300

    [ :sparkles: ] | create: entity - experience > getters (module::core)

--- personal_website/src/modules/@core/experience/entity/experience.entity.ts
+++ personal_website/src/modules/@core/experience/entity/experience.entity.ts
@@ -9,4 +9,28 @@ export class Experience {
     private readonly _context: 'work' | 'academic',
     private readonly _company: Promise<Company>,
   ) {}
+
+  get title() {
+    return this._title;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get startDate() {
+    return this._startDate;
+  }
+
+  get endDate() {
+    return this._endDate;
+  }
+
+  get context() {
+    return this._context;
+  }
+
+  get company() {
+    return this._company;
+  }
 }
