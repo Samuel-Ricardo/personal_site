9c81cc5a46a1e44a8ad13f107c318dae1c381215
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 15:37:05 -0300

    [ :sparkles: ] | create: service - experience (module::core)

---
+++ personal_website/src/modules/@core/experience/service/experience.service.ts
@@ -0,0 +1,15 @@
+import { inject, injectable } from 'inversify';
+import { IExperienceService } from './service.interface';
+import { FindAllExperiencesUseCase } from '../use_case/find/all.use_case';
+import { FindExperienceByContextUseCase } from '../use_case/find/by/context.use_case';
+import { MODULE } from '@/modules/app.registry';
+
+@injectable()
+export class ExperienceService implements IExperienceService {
+  constructor(
+    @inject(MODULE.EXPERIENCE.USE_CASE.FIND.ALL)
+    protected readonly _findAll: FindAllExperiencesUseCase,
+    @inject(MODULE.EXPERIENCE.USE_CASE.FIND.BY.CONTEXT)
+    protected readonly _findByContext: FindExperienceByContextUseCase,
+  ) {}
+}
