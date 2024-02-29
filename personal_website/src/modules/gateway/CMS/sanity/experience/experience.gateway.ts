dc7bca16d5d5baccce9d9c7193b25e41a8fedb1f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 14:29:48 -0300

    [ :door: ] | implements: gateway - experience > findAll (module::cms::sanity)

--- personal_website/src/modules/gateway/CMS/sanity/experience/experience.gateway.ts
+++ personal_website/src/modules/gateway/CMS/sanity/experience/experience.gateway.ts
@@ -1,8 +1,18 @@
 import { IExperienceGateway } from '@/modules/@core/experience/gateway/experience.gateway';
 import { SanitySupport } from '../sanity.gateway';
 import { injectable } from 'inversify';
+import { Experience } from '@/modules/@core/experience/entity/experience.entity';
 
 @injectable()
 export class SanityExperienceGateway
   extends SanitySupport
-  implements IExperienceGateway {}
+  implements IExperienceGateway
+{
+  async findAll(): Promise<Experience[]> {
+    return Experience.fromDTOs(
+      await this.client.fetch(
+        `*[_type == "experience"]{ context, title, description, startDate, endDate, company -> {link,name,logo} }`,
+      ),
+    );
+  }
+}
