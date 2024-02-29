6232763c366978490505760a785f21626aa53509
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 08:08:09 -0300

    [ :syringe: ] | create: registry - company (module::core)

---
+++ personal_website/src/modules/@core/company/company.registry.ts
@@ -0,0 +1,8 @@
+export const COMPANY_REGISTRY = {
+  USE_CASE: {
+    FIND: {
+      ALL: Symbol.for('MODULES::COMPANY::USE_CASE::FIND::ALL'),
+      ONE: Symbol.for('MODULES::COMPANY::USE_CASE::FIND::ONE'),
+    },
+  },
+};
