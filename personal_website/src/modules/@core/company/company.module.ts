8497f50d34591ff58ee8d5d691510e435d2a4a93
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 08:10:20 -0300

    [ :syringe: ] | create: module - company (module::core)

---
+++ personal_website/src/modules/@core/company/company.module.ts
@@ -0,0 +1,18 @@
+import { Container } from 'inversify';
+import { COMPANY_REGISTRY } from './company.registry';
+import { FindAllCompanyUseCase } from './use_case/find/all.use_case';
+import { FindOneCompanyUseCase } from './use_case/find/one.use_case';
+
+const MODULE = new Container({
+  autoBindInjectable: true,
+  defaultScope: 'Singleton',
+});
+
+export const COMPANY_MODULE = MODULE;
+
+COMPANY_MODULE.bind(COMPANY_REGISTRY.USE_CASE.FIND.ALL).to(
+  FindAllCompanyUseCase,
+);
+COMPANY_MODULE.bind(COMPANY_REGISTRY.USE_CASE.FIND.ONE).to(
+  FindOneCompanyUseCase,
+);
