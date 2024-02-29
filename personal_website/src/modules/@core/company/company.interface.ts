4fdd7d7690d0ab97b46640d3338e0f9a5cdfc51f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-02-29 09:02:33 -0300

    [ :video_game: ] | implements: module - company > controller (module::core)

--- personal_website/src/modules/@core/company/company.interface.ts
+++ personal_website/src/modules/@core/company/company.interface.ts
@@ -2,6 +2,6 @@ import { IFindOneComapanyDTO } from './DTO/find/one.dto';
 import { Company } from './entity/commpany.entity';
 
 export interface ICompanyModule {
-  findAll(): Promise<Company[]>;
+  findAll(): Promise<Promise<Company>[]>;
   findOne(DTO: IFindOneComapanyDTO): Promise<Company>;
 }
--- personal_website/src/modules/@core/company/controller/company.controller.ts
+++ personal_website/src/modules/@core/company/controller/company.controller.ts
@@ -1,6 +1,9 @@
 import { inject, injectable } from 'inversify';
 import { type ICompanyService } from '../service/service.interface';
 import { MODULE } from '@/modules/app.registry';
+import { ICompanyModule } from '../company.interface';
+import { IFindOneComapanyDTO } from '../DTO/find/one.dto';
+import { Company } from '../entity/commpany.entity';
 
 @injectable()
 export class CompanyController implements ICompanyModule {
@@ -8,4 +11,12 @@ export class CompanyController implements ICompanyModule {
     @inject(MODULE.COMPANY.SERVICE.MAIN)
     protected service: ICompanyService,
   ) {}
+
+  async findAll(): Promise<Promise<Company>[]> {
+    return await this.service.findAll();
+  }
+
+  async findOne(DTO: IFindOneComapanyDTO): Promise<Company> {
+    return await this.service.findOne(DTO);
+  }
 }
