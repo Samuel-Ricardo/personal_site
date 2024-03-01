e9bca0eae7d729ddfbaf23d422bd90be6a98afba
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-01 16:52:20 -0300

    [ :door: ] | implements: gateway - testimonial > find [all] (module::cms::sanity)

--- personal_website/src/modules/gateway/CMS/sanity/testimonial/testimonial.gateway.ts
+++ personal_website/src/modules/gateway/CMS/sanity/testimonial/testimonial.gateway.ts
@@ -1,8 +1,18 @@
 import { ITestimonialGateway } from '@/modules/@core/testimonial/gateway/testimonial.gateway';
 import { SanitySupport } from '../sanity.gateway';
 import { injectable } from 'inversify';
+import { Testimonial } from '@/modules/@core/testimonial/entity/testimonial.entity';
 
 @injectable()
 export class SanityTestimonialGateway
   extends SanitySupport
-  implements ITestimonialGateway {}
+  implements ITestimonialGateway
+{
+  async findAll(): Promise<Testimonial[]> {
+    return Testimonial.fromDTOs(
+      await this.client.fetch(
+        `*[_type == "testimonial" ]{content,portfolio,person ->{ title, name, avatar, contacts, company }}`,
+      ),
+    );
+  }
+}
