827675a02d0935091863d381f46d2404b558b6d3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-01 12:01:01 -0300

    [ :newspaper: ] | update: section - academic record (app::component::cms)

--- personal_website/src/app/sections/home/AcademicRecord/academic.section.tsx
+++ personal_website/src/app/sections/home/AcademicRecord/academic.section.tsx
@@ -1,33 +1,19 @@
 import { ExperienceContainer } from '@/components/section/home/experience/container/container.component';
 import './academic.style.scss';
+import { MODULES } from '@/modules/app.factory';
 
-export const AcademicRecord = () => {
+export const AcademicRecord = async () => {
+  const { title, experience } =
+    await MODULES.ASSEMBLER.MAIN().assembleAcademicExperience();
+
   return (
     <section id="academic-section" className="section">
-      <h1>Experience</h1>
+      <h1>{await title}</h1>
       <ExperienceContainer
-        itens={[
-          {
-            title: 'Open Source Dev',
-            company: {
-              name: 'Github',
-              icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
-            },
-            description: '123456789123456789123456789123456789123456789',
-            start: new Date(),
-            left: true,
-          },
-          {
-            title: 'Open Source Dev',
-            company: {
-              name: 'Github',
-              icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
-            },
-            description: '123456789123456789123456789123456789123456789',
-            start: new Date(),
-            finish: new Date(),
-          },
-        ]}
+        itens={(await experience).map(async (e, i) => ({
+          ...e.toViewDTO(),
+          left: i % 2 === 0,
+        }))}
       />
     </section>
   );
