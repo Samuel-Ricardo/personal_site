adb219c8f6211b0478b1d98a75efb694c94a99cb
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-01 16:25:37 -0300

    [ :label: ] | update: DTO - person (module::core)

--- personal_website/src/modules/@core/person/DTO/person.dto.ts
+++ personal_website/src/modules/@core/person/DTO/person.dto.ts
@@ -1,6 +1,11 @@
+import { ICompanyDTO } from '../../company/DTO/company.dto';
+import { IContactDTO } from './contact.dto';
+
 export interface IPersonDTO {
   identifier?: string;
   name: string;
   title: string;
   avatar: string;
+  company: ICompanyDTO;
+  contacts: IContactDTO[];
 }
