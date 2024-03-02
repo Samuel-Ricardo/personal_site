0470dc28f0660a894b1cab3cfdac3d6a79bf037a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-01 21:59:51 -0300

    [ :syringe: ] | update: module - testimonial > controller (module::core)

--- personal_website/src/modules/@core/testimonial/testimonial.module.ts
+++ personal_website/src/modules/@core/testimonial/testimonial.module.ts
@@ -1,16 +1,24 @@
 import { Container } from 'inversify';
 import { FindAllTestimonialUseCase } from './use_case/find/all.use_case';
 import { TestimonialService } from './service/testimonial.service';
+import { TESTIMONIAL_REGISTRY } from './testimonial.registry';
+import { TestimonialController } from './controller/testimonial.controller';
 
 export const TESTIMONIAL_MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
-TESTIMONIAL_MODULE.bind('MODULE::TESTIMONIAL::USE_CASE::FIND::ALL').to(
+TESTIMONIAL_MODULE.bind(TESTIMONIAL_REGISTRY.USE_CASE.FIND.ALL).to(
   FindAllTestimonialUseCase,
 );
 
-TESTIMONIAL_MODULE.bind('MODULE::TESTIMONIAL::SERVICE::MAIN').to(
+TESTIMONIAL_MODULE.bind(TESTIMONIAL_REGISTRY.SERVICE.MAIN).to(
   TestimonialService,
 );
+
+TESTIMONIAL_MODULE.bind(TESTIMONIAL_REGISTRY.CONTROLLER.MAIN).to(
+  TestimonialController,
+);
+
+TESTIMONIAL_MODULE.bind(TESTIMONIAL_REGISTRY.MAIN).to(TestimonialController);
