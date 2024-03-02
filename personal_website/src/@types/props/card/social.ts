ecac2b04b3a2fcc698f99a431f9b83a853f3a23b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-02 08:22:30 -0300

    [ :label: ] | create: props - card > social [cover] (@types::props)

--- personal_website/src/@types/props/card/social.ts
+++ personal_website/src/@types/props/card/social.ts
@@ -1,5 +1,12 @@
+import { IContactDTO } from '@/modules/@core/person/DTO/contact.dto';
 import { ITestimonialDTO } from '@/modules/@core/testimonial/DTO/testimonial.dto';
 
 export interface ISocialCardProps {
   data: ITestimonialDTO;
+}
+
+export interface ISocialCardCoverProps {
+  name: string;
+  title: string;
+  contacts: IContactDTO[];
 }
