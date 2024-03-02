8c6a924f92af836514614cf45803243324c6e5d9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-01 21:45:09 -0300

    [ :syringe: ] | update: registry - testimonial > service (module::core)

--- personal_website/src/modules/@core/testimonial/testimonial.registry.ts
+++ personal_website/src/modules/@core/testimonial/testimonial.registry.ts
@@ -1,4 +1,7 @@
 export const TESTIMONIAL_REGISTRY = {
+  SERVICE: {
+    MAIN: Symbol.for('MODULE::TESTIMONIAL::SERVICE::MAIN'),
+  },
   USE_CASE: {
     FIND: {
       ALL: Symbol.for('MODULE::TESTIMONIAL::USE_CASE::FIND::ALL'),
