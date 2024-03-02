0a3bdd31ba30e21461edc4573907957b337b05e3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-01 22:50:02 -0300

    [ :video_game: ] | create: controller - assemble > testimonial (module::core)

--- personal_website/src/modules/@core/assembler/controller/assembler.controller.ts
+++ personal_website/src/modules/@core/assembler/controller/assembler.controller.ts
@@ -10,6 +10,10 @@ export class AssemblerController {
     private readonly service: AssemblerService,
   ) {}
 
+  async assembbleTestimonials() {
+    return await this.service.assembleTestimonials();
+  }
+
   async assembleArticles() {
     return await this.service.assembleArticles();
   }
