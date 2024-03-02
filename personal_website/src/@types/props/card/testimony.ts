a3f9b658154870f25b22fab1bad26b4aa3076df3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-02 09:35:59 -0300

    [ :label: ] | create: props - card > testimony [header] (@types::props)

--- personal_website/src/@types/props/card/testimony.ts
+++ personal_website/src/@types/props/card/testimony.ts
@@ -1,5 +1,10 @@
+import { IPersonDTO } from '@/modules/@core/person/DTO/person.dto';
 import { ITestimonialDTO } from '@/modules/@core/testimonial/DTO/testimonial.dto';
 
 export interface ITestimonyCardProps {
   data: ITestimonialDTO;
+}
+
+export interface ITestimonyCardHeaderProps {
+  data: IPersonDTO;
 }
