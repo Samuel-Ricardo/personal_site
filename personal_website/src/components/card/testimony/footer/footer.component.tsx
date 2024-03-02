3a55d66750c95217f692ec26eedef0f81ce0d8fe
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-02 09:42:32 -0300

    [ :newspaper: ] | update: card - testimony [footer] (app::component::cms)

--- personal_website/src/components/card/testimony/footer/footer.component.tsx
+++ personal_website/src/components/card/testimony/footer/footer.component.tsx
@@ -2,15 +2,29 @@ import './footer.style.scss';
 
 import { NavigateButton } from '@/components/button/navigate/button.component';
 import { GithubLogo } from '@/assets/icons/logo/github/icon.component';
+import { ITestimonyCardFooterProps } from '@/@types/props/card/testimony';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export const TestimonyFooter = () => {
+export const TestimonyFooter = async ({
+  contacts,
+  portfolio,
+}: ITestimonyCardFooterProps) => {
   return (
     <footer className="testimony-footer">
       <div className="wrapper">
-        <NavigateButton link={{ href: './' }}>Portfolio</NavigateButton>
-        <button>
-          <GithubLogo />
-        </button>
+        <NavigateButton link={{ href: portfolio }}>Portfolio</NavigateButton>
+
+        {contacts.map(contact => (
+          <Link key={contact.link} href={contact.link}>
+            <Image
+              src={contact.icon}
+              alt={'Testimony Contact'}
+              width={24}
+              height={24}
+            />
+          </Link>
+        ))}
       </div>
     </footer>
   );
