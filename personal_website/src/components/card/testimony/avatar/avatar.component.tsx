9f6f7f5bbff3973755b55733e036d5a1adc0c412
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-02 09:06:11 -0300

    [ :newspaper: ] | update: card - testimony [avatar] (app::component::cms)

--- personal_website/src/components/card/testimony/avatar/avatar.component.tsx
+++ personal_website/src/components/card/testimony/avatar/avatar.component.tsx
@@ -1,10 +1,10 @@
 import './avatar.style.scss';
 import Image from 'next/image';
 
-export const TestimonyAvatar = () => {
+export const TestimonyAvatar = ({ src }: { src: string }) => {
   return (
     <div className="testimony-avatar">
-      <Image src="/hero_avatar.jpg" alt="avatar" width={800} height={800} />
+      <Image src={src} alt="avatar" width={800} height={800} />
     </div>
   );
 };
