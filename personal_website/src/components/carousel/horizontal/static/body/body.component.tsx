7b91dc81a40fbf54c590139dcf368b91a92f275b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-02 15:56:48 -0300

    [ :art: ] | create: component - slider > body (app::component)

---
+++ personal_website/src/components/carousel/horizontal/static/body/body.component.tsx
@@ -0,0 +1,17 @@
+export const SliderBody = ({ children }: { children: any[] }) => {
+  return (
+    <ul className="static-carousel-container">
+      {children
+        .concat(children)
+        .concat(children)
+        .concat(children)
+        .concat(children)
+        .concat(children)
+        .map(child => (
+          <li key={child?.toString()} className="h-fit w-fit">
+            {child}
+          </li>
+        ))}
+    </ul>
+  );
+};
