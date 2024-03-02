f729f722a4de905697b866f026a04173c11d79da
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-01 22:00:28 -0300

    [ :syringe: ] | update: factory - testimonial > controller (module::core)

--- personal_website/src/modules/@core/testimonial/testimonial.factory.ts
+++ personal_website/src/modules/@core/testimonial/testimonial.factory.ts
@@ -1,9 +1,19 @@
+import { TestimonialController } from './controller/testimonial.controller';
 import { ITestimonialService } from './service/service.interface';
+import { ITestimonialModule } from './testimonial.interface';
 import { TESTIMONIAL_MODULE } from './testimonial.module';
 import { TESTIMONIAL_REGISTRY } from './testimonial.registry';
 import { FindAllTestimonialUseCase } from './use_case/find/all.use_case';
 
 export const TESTIMONIAL_FACTORY = {
+  MAIN: () =>
+    TESTIMONIAL_MODULE.get<ITestimonialModule>(TESTIMONIAL_REGISTRY.MAIN),
+  CONTROLLER: {
+    MAIN: () =>
+      TESTIMONIAL_MODULE.get<TestimonialController>(
+        TESTIMONIAL_REGISTRY.CONTROLLER.MAIN,
+      ),
+  },
   SERCICE: {
     MAIN: () =>
       TESTIMONIAL_MODULE.get<ITestimonialService>(
