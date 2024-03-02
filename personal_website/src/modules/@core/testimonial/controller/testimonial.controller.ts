bb98e5fcf8bedfb7125a321e1ff8be92ec3b63ef
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-01 21:56:50 -0300

    [ :video_game: ] | create: controller - find [all] > testimonial (module::core)

--- personal_website/src/modules/@core/testimonial/controller/testimonial.controller.ts
+++ personal_website/src/modules/@core/testimonial/controller/testimonial.controller.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from 'inversify';
 import { type ITestimonialService } from '../service/service.interface';
 import { MODULE } from '@/modules/app.registry';
+import { ITestimonialModule } from '../testimonial.interface';
+import { Testimonial } from '../entity/testimonial.entity';
 
 @injectable()
 export class TestimonialController implements ITestimonialModule {
@@ -8,4 +10,7 @@ export class TestimonialController implements ITestimonialModule {
     @inject(MODULE.TESTIMONIAL.SERVICE.MAIN)
     protected readonly service: ITestimonialService,
   ) {}
+  findAll(): Promise<Testimonial[]> {
+    return this.service.findAll();
+  }
 }
