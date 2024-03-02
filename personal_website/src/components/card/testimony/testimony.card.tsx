a0d4bfc8720d1e4828c67d8571f1c779e327c1fd
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-02 09:43:06 -0300

    [ :newspaper: ] | update: card - testimony (app::component::cms)

--- personal_website/src/components/card/testimony/testimony.card.tsx
+++ personal_website/src/components/card/testimony/testimony.card.tsx
@@ -1,3 +1,4 @@
+import { ITestimonyCardProps } from '@/@types/props/card/testimony';
 import { TestimonyAvatar } from './avatar/avatar.component';
 import { TestimonyContent } from './content/content.component';
 import { TestimonyFooter } from './footer/footer.component';
@@ -5,14 +6,17 @@ import { TestimonyHeader } from './header/header.component';
 
 import './testimony.style.scss';
 
-export const TestimonyCard = () => {
+export const TestimonyCard = async ({ data }: ITestimonyCardProps) => {
   return (
     <div className="testimony-card">
-      <TestimonyAvatar />
+      <TestimonyAvatar src={data.person.avatar} />
       <div className="content-block">
-        <TestimonyHeader />
-        <TestimonyContent />
-        <TestimonyFooter />
+        <TestimonyHeader data={data.person} />
+        <TestimonyContent>{data.content}</TestimonyContent>
+        <TestimonyFooter
+          contacts={data.person.contacts}
+          portfolio={data.portfolio}
+        />
       </div>
     </div>
   );
