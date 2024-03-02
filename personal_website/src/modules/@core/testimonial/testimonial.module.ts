9195e8196f6f27372b219b7868b3943703c4e1ce
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-01 21:31:14 -0300

    [ :syringe: ] | create: module - testimonial (module::core)

--- personal_website/src/modules/@core/testimonial/testimonial.module.ts
+++ personal_website/src/modules/@core/testimonial/testimonial.module.ts
@@ -1,6 +1,11 @@
 import { Container } from 'inversify';
+import { FindAllTestimonialUseCase } from './use_case/find/all.use_case';
 
 export const TESTIMONIAL_MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
+
+TESTIMONIAL_MODULE.bind('MODULE::TESTIMONIAL::USE_CASE::FIND::ALL').to(
+  FindAllTestimonialUseCase,
+);
