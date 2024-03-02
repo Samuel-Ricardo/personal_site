e63bea0417de8c3dda0721cc17760781049bf1eb
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-01 21:46:04 -0300

    [ :syringe: ] | update: factory - testimonial > service (module::core)

--- personal_website/src/modules/@core/testimonial/testimonial.factory.ts
+++ personal_website/src/modules/@core/testimonial/testimonial.factory.ts
@@ -1,8 +1,15 @@
+import { ITestimonialService } from './service/service.interface';
 import { TESTIMONIAL_MODULE } from './testimonial.module';
 import { TESTIMONIAL_REGISTRY } from './testimonial.registry';
 import { FindAllTestimonialUseCase } from './use_case/find/all.use_case';
 
 export const TESTIMONIAL_FACTORY = {
+  SERCICE: {
+    MAIN: () =>
+      TESTIMONIAL_MODULE.get<ITestimonialService>(
+        TESTIMONIAL_REGISTRY.SERVICE.MAIN,
+      ),
+  },
   USE_CASE: {
     FIND: {
       ALL: () =>
