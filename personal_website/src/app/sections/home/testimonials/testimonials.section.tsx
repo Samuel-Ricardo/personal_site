b729d307a1788710d9c987190d535bcd3eda8031
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-01 22:51:32 -0300

    [ :newspaper: ] | update: section - testimonials (app::component::cms)

--- personal_website/src/app/sections/home/testimonials/testimonials.section.tsx
+++ personal_website/src/app/sections/home/testimonials/testimonials.section.tsx
@@ -3,13 +3,16 @@ import './testimonials.style.scss';
 import { StaticCarousel } from '@/components/carousel/horizontal/static/static.carousel';
 import { SocialCard } from '@/components/card/social/social.card';
 import { TestimonyCard } from '@/components/card/testimony/testimony.card';
+import { MODULES } from '@/modules/app.factory';
 
-export const Testimonials = () => {
+export const Testimonials = async () => {
+  const { testimonials, title } =
+    await MODULES.ASSEMBLER.MAIN().assembbleTestimonials();
   return (
     <section id="testimonials-section" className="section">
       <TestimonialsBackground />
       <div className="content">
-        <h1>Testimonials</h1>
+        <h1>{await title}</h1>
 
         <StaticCarousel>
           <SocialCard />
