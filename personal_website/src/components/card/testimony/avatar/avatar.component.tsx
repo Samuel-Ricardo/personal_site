4e3de210fda99cfc6b8131a4b765f8d79ebd67ae
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-02 09:09:31 -0300

    [ :zap: ] | improve: card - testimony [avatar] (app::component)

--- personal_website/src/components/card/testimony/avatar/avatar.component.tsx
+++ personal_website/src/components/card/testimony/avatar/avatar.component.tsx
@@ -1,7 +1,7 @@
 import './avatar.style.scss';
 import Image from 'next/image';
 
-export const TestimonyAvatar = ({ src }: { src: string }) => {
+export const TestimonyAvatar = async ({ src }: { src: string }) => {
   return (
     <div className="testimony-avatar">
       <Image src={src} alt="avatar" width={800} height={800} />
