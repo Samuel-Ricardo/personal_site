dd2074caab742a8233e306ce7d9bf04736a3e0ed
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-02 08:17:58 -0300

    [ :newspaper: ] | update: card - social (app::component::cms)

--- personal_website/src/components/card/social/social.card.tsx
+++ personal_website/src/components/card/social/social.card.tsx
@@ -2,13 +2,14 @@ import Image from 'next/image';
 import './social.style.scss';
 import { SocialCardCover } from './cover/cover.card';
 import { SocialCardFooter } from './footer/footer.component';
+import { ISocialCardProps } from '@/@types/props/card/social';
 
-export const SocialCard = () => {
+export const SocialCard = ({ data }: ISocialCardProps) => {
   return (
     <div className="social-card">
       <Image
-        src="/hero_avatar.jpg"
-        alt="Github"
+        src={data.person.avatar}
+        alt="testimony avatar"
         width={800}
         height={800}
         className="avatar"
