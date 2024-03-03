51f39f5d9af9b38a102c78aaac0d2e4db7cdefb8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-03 17:41:00 -0300

    [ :clapper: ] | create: animation - emerge > down (modules::animation::motion)

---
+++ personal_website/src/modules/animations/framer_motion/impl/emerge/down.animation.tsx
@@ -0,0 +1,20 @@
+import { Animator } from '@/@types/animations/motion/animator';
+
+export const EMERGE_UP_ANIMATION: Animator = props => ({
+  ...props,
+  initial: 'inactive',
+  transition: {
+    ease: 'easeInOut',
+    ...props?.transition,
+  },
+  variants: {
+    inactive: {
+      translateY: '-50%',
+      opacity: 0,
+    },
+    active: {
+      translateY: 0,
+      opacity: 1,
+    },
+  },
+});
