f7d778c79062735127a0dc838efcff9fe1a7b7e4
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-03 14:37:23 -0300

    [ :art: ] | update: social - icon (app)

--- personal_website/src/components/social/social.component.tsx
+++ personal_website/src/components/social/social.component.tsx
@@ -1,13 +1,20 @@
+'use client';
+
 import './social.style.scss';
 
 import { CloseIcon } from '@/assets/icons/close/icon.component';
 import { ShareIcon } from '@/assets/icons/share/icon.component';
+import { useCallback, useState } from 'react';
 
 export const Social = () => {
+  const [active, setActive] = useState(false);
+  const handleClick = useCallback(e => setActive(!active), [active]);
+
   return (
-    <div id="social-floating-button">
-      <ShareIcon />
-      <CloseIcon />
+    <div id="social-floating-button" onClick={handleClick}>
+      <div className="social-icon">
+        {active ? <CloseIcon /> : <ShareIcon />}
+      </div>
     </div>
   );
 };
