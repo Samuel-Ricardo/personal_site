a7adb015b2abdb035bf9a017e4cd1e2f2eae7e37
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-03 18:13:10 -0300

    [ :art: ] | create: component - social > item [left] (app::component)

--- personal_website/src/components/social/social.component.tsx
+++ personal_website/src/components/social/social.component.tsx
@@ -1,20 +1,36 @@
 'use client';
 
+import { GithubIcon } from '@/assets/icons/logo/github/2/icon.component';
 import './social.style.scss';
 
 import { CloseIcon } from '@/assets/icons/close/icon.component';
 import { ShareIcon } from '@/assets/icons/share/icon.component';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
+import { MotionDiv } from '../motion/div.component';
+import { MODULES } from '@/modules/app.factory';
+import Link from 'next/link';
 
 export const Social = () => {
   const [active, setActive] = useState(false);
-  const handleClick = useCallback(e => setActive(!active), [active]);
+  const handleClick = e => setActive(!active);
+
+  const { animate } = { animate: `${active ? 'active' : 'inacive'}` };
+
+  const ANIMATION = {
+    LEFT: MODULES.ANIMATION.FRAMER_MOTION.EMERGE.LEFT()({ animate }),
+  };
 
   return (
-    <div id="social-floating-button" onClick={handleClick}>
-      <div className="social-icon">
+    <MotionDiv id="social-floating-button">
+      <MotionDiv className={` social-item si-left`} {...ANIMATION.LEFT}>
+        <Link href="https://github.com/Samuel-Ricardo">
+          <GithubIcon />
+        </Link>
+      </MotionDiv>
+
+      <button className="social-icon" onClick={handleClick}>
         {active ? <CloseIcon /> : <ShareIcon />}
-      </div>
-    </div>
+      </button>
+    </MotionDiv>
   );
 };
