216e0c4bd2ae276e1608ba64f89e9cfdaff8be35
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-03 14:22:25 -0300

    [ :art: ] | create: component - social > icon (app::component)

---
+++ personal_website/src/components/social/icon/icon.component.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+
+export const SocialFloattingButtonIcon = ({
+  children,
+}: {
+  children: any[];
+}) => {
+  const [active, setActive] = useState(false);
+  const handleClick = useCallback(e => setActive(!active), [active]);
+
+  return (
+    <div className="social-icon" onClick={handleClick}>
+      {active ? children[0] : children[1]}
+    </div>
+  );
+};
