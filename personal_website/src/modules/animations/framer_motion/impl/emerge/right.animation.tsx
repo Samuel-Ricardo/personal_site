3dbe14b02cfbe06c84e3cd6e2c81d117ecd66af0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-03 18:07:04 -0300

    [ :syringe: ] | update: registry - emerge > left (modules::animation)

---
+++ personal_website/src/modules/animations/framer_motion/impl/emerge/right.animation.tsx
@@ -0,0 +1,20 @@
+import { Animator } from '@/@types/animations/motion/animator';
+
+export const EMERGE_LEFT_ANIMATION: Animator = props => ({
+  ...props,
+  initial: 'inactive',
+  transition: {
+    ease: 'easeInOut',
+    ...props?.transition,
+  },
+  variants: {
+    inactive: {
+      translateX: '-50%',
+      opacity: 0,
+    },
+    active: {
+      translateX: 0,
+      opacity: 1,
+    },
+  },
+});
