51976a2058b0c42316b20be4f9c40a3190134e0c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-04 15:39:41 -0300

    [ :art: ] | update: component - navmenu (app::component)

--- personal_website/src/components/navigation/menu/menu.component.tsx
+++ personal_website/src/components/navigation/menu/menu.component.tsx
@@ -1,49 +1,31 @@
-import { NavbarItem } from '@/components/navbar/item/item.component';
 import './menu.style.scss';
-import { MODULES } from '@/modules/app.factory';
+import { NavMenuContainer } from './container/container.component';
+import { NavMenuItem } from './item/item.component';
 
-export const NavMenu = () => {
-  const animation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.IN();
+export const NavMenu = async () => {
   return (
-    <nav id="nav-menu">
-      <ul>
-        <NavbarItem li={{ id: 'home', ...animation() }} href="/">
-          Home
-        </NavbarItem>
-        <NavbarItem
-          li={{ id: 'portfolio', ...animation({ transition: { delay: 1 } }) }}
-          href="/portfolio"
-        >
-          Portfolio
-        </NavbarItem>
-        <NavbarItem
-          li={{ id: 'articles', ...animation({ transition: { delay: 2 } }) }}
-          href="/articles"
-        >
-          Articles
-        </NavbarItem>
-        <NavbarItem
-          li={{ id: 'jobs', ...animation({ transition: { delay: 3 } }) }}
-          href="/jobs"
-        >
-          Works
-        </NavbarItem>
-        <NavbarItem
-          li={{ id: 'journey', ...animation({ transition: { delay: 4 } }) }}
-          href="/journey"
-        >
-          Journey
-        </NavbarItem>
+    <NavMenuContainer>
+      <NavMenuItem index={0} li={{ id: 'home' }} href="/">
+        Home
+      </NavMenuItem>
+      <NavMenuItem li={{ id: 'portfolio' }} href="/portfolio" index={1}>
+        Portfolio
+      </NavMenuItem>
+      <NavMenuItem li={{ id: 'articles' }} href="/articles" index={2}>
+        Articles
+      </NavMenuItem>
+      <NavMenuItem li={{ id: 'jobs' }} href="/jobs" index={3}>
+        Works
+      </NavMenuItem>
+      <NavMenuItem li={{ id: 'journey' }} href="/journey" index={4}>
+        Journey
+      </NavMenuItem>
 
-        <NavbarItem
-          li={{ id: 'My World', ...animation({ transition: { delay: 5 } }) }}
-          href="My World"
-        >
-          My World
-        </NavbarItem>
+      <NavMenuItem li={{ id: 'My World' }} href="/my-world" index={5}>
+        My World
+      </NavMenuItem>
 
-        <div></div>
-      </ul>
-    </nav>
+      <div className="line" />
+    </NavMenuContainer>
   );
 };
