b6618a3074ef1664b3b88575486e015796860b77
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-04 14:35:13 -0300

    [ :clapper: ] | update: animation - slideIn > structure (app::modules:framer_motion)

--- personal_website/src/modules/animations/framer_motion/impl/slideIn.animation.ts
+++ personal_website/src/modules/animations/framer_motion/impl/slideIn.animation.ts
@@ -2,7 +2,22 @@ import { Animator } from '@/@types/animations/motion/animator';
 
 export const SLIDE_IN_ANIMATION: Animator = props => ({
   ...props,
-  initial: { x: -100, opacity: 0, filter: 'blur(5px)' },
-  animate: { x: 0, opacity: 1, filter: 'blur(0px)' },
+  //  initial: { x: -100, opacity: 0, filter: 'blur(5px)' },
+  //  animate: { x: 0, opacity: 1, filter: 'blur(0px)' },
+  initial: 'inactive',
+  animate: 'active',
   transition: { duration: 1.5, ...props?.transition },
+
+  variants: {
+    active: {
+      x: 0,
+      opacity: 1,
+      filter: 'blur(0px)',
+    },
+    inactive: {
+      x: -100,
+      opacity: 0,
+      filter: 'blur(5px)',
+    },
+  },
 });
