93a3405e61ebfc95e16c05234377efd87fe7b90c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-04 15:40:01 -0300

    [ :art: ] | update: component - navmenu > item (app::component)

---
+++ personal_website/src/components/navigation/menu/item/item.component.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { INavMenuItemProps } from '@/@types/props/navigation/menu/item';
+import { NavbarItem } from '@/components/navbar/item/item.component';
+import { useAppAnimation } from '@/hooks/animation/animation.hook';
+import { useMenu } from '@/hooks/navigation/menu.hook';
+import { useMemo } from 'react';
+
+export const NavMenuItem = (props: INavMenuItemProps) => {
+  const { index, children } = props;
+  const { open } = useMenu();
+
+  const animation = useAppAnimation().SLIDE.IN();
+
+  const delay = useMemo(() => (index + 1) * 5 * 0.01, [index]);
+  const active = open ? 'active' : 'inactive';
+
+  return (
+    <NavbarItem
+      {...props}
+      li={{
+        ...props.li,
+        ...animation({ transition: { delay }, whileInView: active }),
+      }}
+    >
+      {children}
+    </NavbarItem>
+  );
+};
