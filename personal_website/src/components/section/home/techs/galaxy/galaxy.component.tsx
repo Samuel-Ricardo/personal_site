b270a880f74fb4ea5a0128e7d8b412475b678f10
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 19:30:48 -0300

    [ :clapper: ] | animate: techs - galaxy (app::component)

--- personal_website/src/components/section/home/techs/galaxy/galaxy.component.tsx
+++ personal_website/src/components/section/home/techs/galaxy/galaxy.component.tsx
@@ -2,16 +2,27 @@ import { ITechGalaxyProps } from '@/@types/props/section/techs/galaxy';
 
 import './galaxy.style.scss';
 import { TechsGalaxyItem } from './item/item.component';
+import { MODULES } from '@/modules/app.factory';
+import { MotionDiv } from '@/components/motion/div.component';
 
-export const TechsGalaxy = ({ items, title, className }: ITechGalaxyProps) => {
+export const TechsGalaxy = async ({
+  items,
+  title,
+  className,
+}: ITechGalaxyProps) => {
+  const animation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.UP()();
   return (
-    <div id="techs-galaxy" className={'frosted-glass-lg ' + className}>
+    <MotionDiv
+      id="techs-galaxy"
+      className={'frosted-glass-lg ' + className}
+      {...animation}
+    >
       <h1 id="techs-galaxy-title">{title}</h1>
       <ul className="items-container">
         {items?.map((item, index) => (
           <TechsGalaxyItem index={index} key={item?.icon} {...item} />
         ))}
       </ul>
-    </div>
+    </MotionDiv>
   );
 };
