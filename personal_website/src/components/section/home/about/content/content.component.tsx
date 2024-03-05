90fb07dcf7d52906e45fdcf2580009599b68e759
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 15:00:09 -0300

    [ :newspaper: ] | update: component - aboutme > content (app::component::cms)

--- personal_website/src/components/section/home/about/content/content.component.tsx
+++ personal_website/src/components/section/home/about/content/content.component.tsx
@@ -1,12 +1,19 @@
+import { IAboutContentProps } from '@/@types/props/section/about/content';
 import { AboutMeContentContainer } from './container/container.component';
 import { AboutMeContentImage } from './image/image.component';
 import { AbouteMeContentText } from './text/text.component';
 
-export const AboutMeContent = () => {
+export const AboutMeContent = async ({
+  data: { image, highlights, paragraph, subtitle },
+}: IAboutContentProps) => {
   return (
     <AboutMeContentContainer>
-      <AboutMeContentImage />
-      <AbouteMeContentText />
+      <AboutMeContentImage src={image} />
+      <AbouteMeContentText
+        title={subtitle}
+        paragraph={paragraph}
+        highlights={highlights}
+      />
     </AboutMeContentContainer>
   );
 };
