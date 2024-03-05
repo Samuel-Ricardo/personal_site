267d7a0530ecacebbabef93ab40f782757bb39fd
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 15:01:34 -0300

    [ :newspaper: ] | update: component - aboutme > content [image] (app::component::cms)

--- personal_website/src/components/section/home/about/content/image/image.component.tsx
+++ personal_website/src/components/section/home/about/content/image/image.component.tsx
@@ -1,15 +1,19 @@
 import Image from 'next/image';
 import './image.style.scss';
 
-export const AboutMeContentImage = () => {
+export const AboutMeContentImage = async ({ src }: { src?: any }) => {
+  const data = await src;
+
   return (
     <div className="image-container">
       <Image
         className="abm-image"
-        src="/image-2.png"
+        src={data}
         alt="about me image"
         width={1920}
         height={1080}
+        placeholder="blur"
+        blurDataURL="/image-2.png"
       />
     </div>
   );
