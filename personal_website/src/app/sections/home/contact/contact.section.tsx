ef4ff54814fc4c2fcf3ed4f67f31388c7c2fc0a9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-06 12:03:49 -0300

    [ :newspaper: ] | update: sectin - contact (app::component::cms)

--- personal_website/src/app/sections/home/contact/contact.section.tsx
+++ personal_website/src/app/sections/home/contact/contact.section.tsx
@@ -1,9 +1,15 @@
+import { ContactBackground } from '@/assets/images/background/contact/contact.background';
 import './contact.style.scss';
+import { MotionH1 } from '@/components/motion/h1.component';
+import { MODULES } from '@/modules/app.factory';
 
-export const Contact = () => {
+export const Contact = async () => {
+  const { title } = await MODULES.ASSEMBLER.MAIN().assembleContact();
+
   return (
     <section id="contact-section">
-      <h1>Contact</h1>
+      <ContactBackground />
+      <MotionH1>{await title}</MotionH1>
     </section>
   );
 };
