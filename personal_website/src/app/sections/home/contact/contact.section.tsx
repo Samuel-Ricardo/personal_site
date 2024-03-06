fe42d9be088fdc5a81d55ecdf0978008e705be4b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-06 12:49:29 -0300

    [ :clapper: ] | animate: section - contact (app::component)

--- personal_website/src/app/sections/home/contact/contact.section.tsx
+++ personal_website/src/app/sections/home/contact/contact.section.tsx
@@ -1,15 +1,23 @@
 import { ContactBackground } from '@/assets/images/background/contact/contact.background';
 import './contact.style.scss';
 import { MotionH1 } from '@/components/motion/h1.component';
+import { MotionH2 } from '@/components/motion/h2.component';
 import { MODULES } from '@/modules/app.factory';
 
 export const Contact = async () => {
-  const { title } = await MODULES.ASSEMBLER.MAIN().assembleContact();
+  const { title, subtitle } = await MODULES.ASSEMBLER.MAIN().assembleContact();
+
+  const LEFT = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.IN()({
+    animate: 'inactive',
+  });
+  const RIGHT = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.OUT()();
+  const UP = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.UP()();
 
   return (
     <section id="contact-section">
       <ContactBackground />
-      <MotionH1>{await title}</MotionH1>
+      <MotionH1 {...LEFT}>{await title}</MotionH1>
+      <MotionH2 {...RIGHT}>{await subtitle}</MotionH2>
     </section>
   );
 };
