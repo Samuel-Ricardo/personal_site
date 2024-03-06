886f2bc245a2b9c5b7b41f6ffed321e011a5980d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 23:21:13 -0300

    [ :clapper: ] | animate: section - testimonials (app::component)

--- personal_website/src/app/sections/home/testimonials/testimonials.section.tsx
+++ personal_website/src/app/sections/home/testimonials/testimonials.section.tsx
@@ -4,19 +4,22 @@ import { Slider } from '@/components/carousel/horizontal/static/static.carousel'
 import { SocialCard } from '@/components/card/social/social.card';
 import { TestimonyCard } from '@/components/card/testimony/testimony.card';
 import { MODULES } from '@/modules/app.factory';
+import { MotionH1 } from '@/components/motion/h1.component';
 
 export const Testimonials = async () => {
   const { testimonials, title } =
     await MODULES.ASSEMBLER.MAIN().assembbleTestimonials();
+
+  const animation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.DOWN()();
   return (
     <section id="testimonials-section" className="section">
       <TestimonialsBackground />
       <div className="content">
-        <h1>{await title}</h1>
+        <MotionH1 {...animation}>{await title}</MotionH1>
 
         <Slider>
-          {(await testimonials).map(t => (
-            <SocialCard key={t.portfolio} data={t.toDTO()} />
+          {(await testimonials).map((t, i) => (
+            <SocialCard index={i} key={t.portfolio} data={t.toDTO()} />
           ))}
         </Slider>
 
