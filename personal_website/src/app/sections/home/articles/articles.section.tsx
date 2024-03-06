bb7be406242a4a15c4f89bcde19d87be91b49035
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 22:11:42 -0300

    [ :clapper: ] | animate: section - article > title (app::component)

--- personal_website/src/app/sections/home/articles/articles.section.tsx
+++ personal_website/src/app/sections/home/articles/articles.section.tsx
@@ -3,8 +3,14 @@ import './articles.style.scss';
 import { MODULES } from '@/modules/app.factory';
 import { ArticlesContainer } from '@/components/section/home/articles/container/container.component';
 import { ArticlesSectionFooter } from '@/components/section/home/articles/footer/footer.component';
+import { Article } from '@/modules/@core/articles/entity/article.entity';
+import { MotionH1 } from '@/components/motion/h1.component';
 
-export const Articles = () => {
+export const Articles = async () => {
+  const { title, platforms_title, articles, platforms } =
+    await MODULES.ASSEMBLER.MAIN().assembleArticles();
+  const animation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.DOWN()();
+
   return (
     <section
       id="articles-section"
@@ -12,55 +18,15 @@ export const Articles = () => {
     >
       <ArticlesBackground />
       <div className="z-0 flex flex-1 content-container">
-        <h1 className={`section-title`}>Articles that i Wrotte</h1>
+        <MotionH1 {...animation} className={`section-title`}>
+          {await title}
+        </MotionH1>
 
         <ArticlesContainer
-          articles={[
-            {
-              title: 'Docker',
-              platforms: [
-                {
-                  name: 'LinkedIn',
-                  url: './',
-                  icon: 'https://img.icons8.com/?size=48&id=13930&format=png',
-                },
-              ],
-              description: '1234567890-qwertyuiop´pASDFGHJKLZXCVBNM,.RYEUSW,',
-            },
-            {
-              title: 'Docker',
-              platforms: [{ name: 'LinkedIn', url: './' }],
-              description: '1234567890-qwertyuiop´pASDFGHJKLZXCVBNM,.RYEUSW,',
-              image:
-                'https://images.pexels.com/photos/2228559/pexels-photo-2228559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            },
-            {
-              title: 'Docker',
-              platforms: [{ name: 'LinkedIn', url: './' }],
-              description: '1234567890-qwertyuiop´pASDFGHJKLZXCVBNM,.RYEUSW,',
-            },
-          ]}
+          articles={await Article.toViewList(await articles)}
         />
 
-        <ArticlesSectionFooter
-          platforms={[
-            {
-              name: 'LinkedIn',
-              url: './',
-              icon: 'https://img.icons8.com/?size=48&id=13930&format=png',
-            },
-            {
-              name: 'Midiunm',
-              url: './',
-              icon: 'https://img.icons8.com/?size=48&id=13930&format=png',
-            },
-            {
-              name: 'TabNews',
-              url: './',
-              icon: 'https://img.icons8.com/?size=48&id=13930&format=png',
-            },
-          ]}
-        />
+        <ArticlesSectionFooter platforms={await platforms} />
       </div>
     </section>
   );
