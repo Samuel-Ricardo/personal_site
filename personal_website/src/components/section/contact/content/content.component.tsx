37ea5336910547b78ed34cfb8aaac44f6ee4dbfc
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-06 13:08:35 -0300

    [ :art: ] | create: contact - component > paragraph (app::component)

--- personal_website/src/components/section/contact/content/content.component.tsx
+++ personal_website/src/components/section/contact/content/content.component.tsx
@@ -1,11 +1,18 @@
 import { IContactContentProps } from '@/@types/props/section/contact/content';
 import { MotionDiv } from '@/components/motion/div.component';
+import { MotionP } from '@/components/motion/p.component';
+import { MODULES } from '@/modules/app.factory';
 import Image from 'next/image';
 
-export const ContactContent = async ({ image }: IContactContentProps) => {
+export const ContactContent = async ({
+  image,
+  paragraph,
+  contacts,
+}: IContactContentProps) => {
+  const animation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.UP();
   return (
     <div>
-      <MotionDiv>
+      <MotionDiv {...animation()}>
         <Image
           src={(await image) || '/img_place_holder.gif'}
           alt="contact image"
@@ -16,6 +23,10 @@ export const ContactContent = async ({ image }: IContactContentProps) => {
           blurDataURL={'/img_place_holder.gif'}
         />
       </MotionDiv>
+
+      <MotionP {...animation({ transition: { delay: 0.5 } })}>
+        {await paragraph}
+      </MotionP>
     </div>
   );
 };
