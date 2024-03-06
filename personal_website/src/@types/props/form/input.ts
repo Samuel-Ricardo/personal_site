b03c01c9c22d7af80007b702f8deb0f5f4c0ddd7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-06 18:38:29 -0300

    [ :label: ] | create: props - form > input (@types::props)

--- personal_website/src/@types/props/form/input.ts
+++ personal_website/src/@types/props/form/input.ts
@@ -1,7 +1,10 @@
+import { MotionProps } from 'framer-motion';
+
 export interface IInputProps {
   id: string;
   label: string;
   placeholder: string;
   type: string;
   hook?: any;
+  animation?: MotionProps;
 }
