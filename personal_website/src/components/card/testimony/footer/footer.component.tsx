34f5da2b62a4604304c20afa5d2876040840873f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 23:39:23 -0300

    [ :clapper: ] | animate: testimony - contacts(app::component)

--- personal_website/src/components/card/testimony/footer/footer.component.tsx
+++ personal_website/src/components/card/testimony/footer/footer.component.tsx
@@ -1,29 +1,43 @@
 import './footer.style.scss';
 
 import { NavigateButton } from '@/components/button/navigate/button.component';
-import { GithubLogo } from '@/assets/icons/logo/github/icon.component';
 import { ITestimonyCardFooterProps } from '@/@types/props/card/testimony';
 import Image from 'next/image';
 import Link from 'next/link';
+import { MotionDiv } from '@/components/motion/div.component';
+import { MODULES } from '@/modules/app.factory';
 
-export const TestimonyFooter = async ({
+export const TestimonyFooter = ({
   contacts,
   portfolio,
 }: ITestimonyCardFooterProps) => {
+  const animation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.IN();
   return (
     <footer className="testimony-footer">
       <div className="wrapper">
-        <NavigateButton link={{ href: portfolio }}>Portfolio</NavigateButton>
-
-        {contacts.map(contact => (
-          <Link key={contact.link} href={contact.link}>
-            <Image
-              src={contact.icon}
-              alt={'Testimony Contact'}
-              width={24}
-              height={24}
-            />
-          </Link>
+        <MotionDiv
+          {...animation({ animate: 'inactive', transition: { delay: 0.1 } })}
+        >
+          <NavigateButton link={{ href: portfolio }}>Portfolio</NavigateButton>
+        </MotionDiv>
+        {contacts.map((contact, i) => (
+          <MotionDiv
+            {...animation({
+              animate: 'inactive',
+              transition: { delay: (i + 1) * 0.35 },
+            })}
+            key={contact.link}
+            className=""
+          >
+            <Link href={contact.link}>
+              <Image
+                src={contact.icon}
+                alt={'Testimony Contact'}
+                width={24}
+                height={24}
+              />
+            </Link>
+          </MotionDiv>
         ))}
       </div>
     </footer>
