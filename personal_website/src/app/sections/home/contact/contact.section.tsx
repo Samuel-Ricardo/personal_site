97fc12fdf479d338de005116f25dea10436c1271
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-06 18:29:05 -0300

    [ :art: ] | update: section - contact (app::component)

--- personal_website/src/app/sections/home/contact/contact.section.tsx
+++ personal_website/src/app/sections/home/contact/contact.section.tsx
@@ -4,6 +4,7 @@ import { MotionH1 } from '@/components/motion/h1.component';
 import { MotionH2 } from '@/components/motion/h2.component';
 import { MODULES } from '@/modules/app.factory';
 import { ContactContent } from '@/components/section/contact/content/content.component';
+import { ContactForm } from '@/components/section/contact/form/form.component';
 
 export const Contact = async () => {
   const { title, subtitle, contacts, paragraph, image, form } =
@@ -13,10 +14,12 @@ export const Contact = async () => {
     animate: 'inactive',
   });
   const RIGHT = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.OUT()();
-  const UP = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.UP()();
 
   return (
-    <section id="contact-section">
+    <section
+      id="contact-section"
+      className={MODULES.FONTS.CLASSIC.LIBRE_BASKERVILLE().className}
+    >
       <ContactBackground />
       <MotionH1 {...LEFT}>{await title}</MotionH1>
       <MotionH2 {...RIGHT}>{await subtitle}</MotionH2>
@@ -26,6 +29,7 @@ export const Contact = async () => {
           paragraph={paragraph}
           contacts={contacts}
         />
+        <ContactForm {...form} />
       </div>
     </section>
   );
