bb6781618e8f0cdb6011bf8aed5c91cfb0471487
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 23:12:17 -0300

    [ :clapper: ] | animate: section - experiences > title (app::component)

--- personal_website/src/app/sections/home/AcademicRecord/academic.section.tsx
+++ personal_website/src/app/sections/home/AcademicRecord/academic.section.tsx
@@ -1,14 +1,17 @@
 import { ExperienceContainer } from '@/components/section/home/experience/container/container.component';
 import './academic.style.scss';
 import { MODULES } from '@/modules/app.factory';
+import { MotionH1 } from '@/components/motion/h1.component';
 
 export const AcademicRecord = async () => {
   const { title, experience } =
     await MODULES.ASSEMBLER.MAIN().assembleAcademicExperience();
 
+  const animation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.DOWN()();
+
   return (
     <section id="academic-section" className="section">
-      <h1>{await title}</h1>
+      <MotionH1 {...animation}>{await title}</MotionH1>
       <ExperienceContainer
         itens={(await experience).map(async (e, i) => ({
           ...e.toViewDTO(),
