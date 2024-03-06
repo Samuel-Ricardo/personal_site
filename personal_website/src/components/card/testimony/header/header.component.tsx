bf4e09bfdab5decb90b18b2ba4bd1df96a8fd9bb
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 23:29:11 -0300

    [ :clapper: ] | animate: testimony - header (app::component)

--- personal_website/src/components/card/testimony/header/header.component.tsx
+++ personal_website/src/components/card/testimony/header/header.component.tsx
@@ -1,14 +1,29 @@
 import { ITestimonyCardHeaderProps } from '@/@types/props/card/testimony';
 import './header.style.scss';
+import { MotionH3 } from '@/components/motion/h3.component';
+import { MODULES } from '@/modules/app.factory';
+import { MotionDiv } from '@/components/motion/div.component';
+import { MotionP } from '@/components/motion/p.component';
 
-export const TestimonyHeader = async ({ data }: ITestimonyCardHeaderProps) => {
+export const TestimonyHeader = ({ data }: ITestimonyCardHeaderProps) => {
+  const IN = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.IN()({
+    animate: 'inactive',
+  });
+  const OUT = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.OUT()();
+
   return (
     <header className="testimony-header">
-      <h4>{data.name}</h4>
-      <div className="line" />
-      <p>
+      <MotionH3 {...IN}>{data.name}</MotionH3>
+      <MotionDiv
+        initial={{ width: 0 }}
+        animate={{ width: 0 }}
+        whileInView={{ width: '100%' }}
+        transition={{ duration: 1.5 }}
+        className="line"
+      />
+      <MotionP {...OUT}>
         {data.title} • {data.company.name}
-      </p>
+      </MotionP>
     </header>
   );
 };
