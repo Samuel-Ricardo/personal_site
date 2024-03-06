ca4fc30cc929343c7395dc59a9935c4dce738b94
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 22:47:01 -0300

    [ :newspaper: ] | update: component - experience > container (app::component::cms)

--- personal_website/src/components/section/home/experience/container/container.component.tsx
+++ personal_website/src/components/section/home/experience/container/container.component.tsx
@@ -2,12 +2,16 @@ import { ExperienceItem } from '../item/item.component';
 import './container.style.scss';
 import { IExperienceContainerProps } from '@/@types/props/section/experience/container';
 
-export const ExperienceContainer = ({ itens }: IExperienceContainerProps) => {
+export const ExperienceContainer = async ({
+  itens,
+}: IExperienceContainerProps) => {
   return (
     <ul id="experience-section-container">
-      {itens.map(item => (
-        <ExperienceItem data={item} key={item.company.name} />
-      ))}
+      {await Promise.all(
+        (await Promise.all(itens)).map(async item => (
+          <ExperienceItem data={item} key={item.company.name} />
+        )),
+      )}
     </ul>
   );
 };
