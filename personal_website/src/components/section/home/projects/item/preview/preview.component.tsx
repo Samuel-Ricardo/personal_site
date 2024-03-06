fa0f62b0f85efd1fa45aa7d59e2a142fd285412c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 21:36:12 -0300

    [ :clapper: ] | animate: project - preview (app::component)

--- personal_website/src/components/section/home/projects/item/preview/preview.component.tsx
+++ personal_website/src/components/section/home/projects/item/preview/preview.component.tsx
@@ -2,15 +2,25 @@ import { IProjectItemPreviewProps } from '@/@types/props/section/project/item';
 import Image from 'next/image';
 
 import './preview.style.scss';
+import { MODULES } from '@/modules/app.factory';
+import { MotionDiv } from '@/components/motion/div.component';
 
-export const ProjectItemPreview = ({ image }: IProjectItemPreviewProps) => (
-  <div className="pi-image-container">
-    <Image
-      src={image}
-      alt="project item preview"
-      width={1280}
-      height={720}
-      quality={100}
-    />
-  </div>
-);
+export const ProjectItemPreview = async ({
+  image,
+}: IProjectItemPreviewProps) => {
+  const animation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.IN()({
+    animate: 'inactive',
+  });
+
+  return (
+    <MotionDiv className="pi-image-container" {...animation}>
+      <Image
+        src={image}
+        alt="project item preview"
+        width={1280}
+        height={720}
+        quality={100}
+      />
+    </MotionDiv>
+  );
+};
