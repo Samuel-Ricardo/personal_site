0269f49653878510bb2835821a75f8c9e1816656
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 23:07:38 -0300

    [ :zap: ] | improve: experience - map (app::component)

--- personal_website/src/components/section/home/experience/item/item.component.tsx
+++ personal_website/src/components/section/home/experience/item/item.component.tsx
@@ -3,24 +3,42 @@ import Image from 'next/image';
 
 import './item.style.scss';
 import { ExperienceItemCard } from './card/card.component';
+import { MODULES } from '@/modules/app.factory';
+import { MotionDiv } from '@/components/motion/div.component';
 
-export const ExperienceItem = ({ data, key }: IExperienceItemProps) => {
+export const ExperienceItem = async ({
+  index,
+  data,
+  key,
+}: IExperienceItemProps) => {
+  const lineAnimation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.DOWN()({
+    animate: 'inactive',
+    whileInView: 'active',
+    transition: { delay: (index || 0) * 0.25, duration: 0.85 },
+  });
+
+  const logoAnimation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.DOWN()({
+    animate: 'inactive',
+    whileInView: 'active',
+    transition: { delay: (index || 0) * 0.5, duration: 1.5 },
+  });
+
   return (
     <li key={key ?? data.company.name} className={`experience-item `}>
-      <div className="experience-date">
+      <MotionDiv className="experience-date" {...logoAnimation}>
         {(data.finish || new Date()).getFullYear()}
-      </div>
-      <div className="vertical-line" />
-      <Image
-        src={data.company.icon}
-        alt={`${data.company} icon`}
-        width={250}
-        height={250}
-      />
-
+      </MotionDiv>
+      <MotionDiv className="vertical-line" {...lineAnimation} />
+      <MotionDiv {...logoAnimation} className="w-fit h-fit">
+        <Image
+          src={data.company.icon}
+          alt={`${data.company} icon`}
+          width={250}
+          height={250}
+        />
+      </MotionDiv>
       <ExperienceItemCard {...data} />
-
-      <div className="vertical-line" />
+      <MotionDiv className="vertical-line" {...lineAnimation} />
     </li>
   );
 };
