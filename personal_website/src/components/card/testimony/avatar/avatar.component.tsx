776b13f4f34a316e491c2be103fd4e3995ceed52
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 23:24:15 -0300

    [ :clapper: ] | animate: testimony - avatar (app::component)

--- personal_website/src/components/card/testimony/avatar/avatar.component.tsx
+++ personal_website/src/components/card/testimony/avatar/avatar.component.tsx
@@ -1,10 +1,13 @@
+import { MODULES } from '@/modules/app.factory';
 import './avatar.style.scss';
 import Image from 'next/image';
+import { MotionDiv } from '@/components/motion/div.component';
 
-export const TestimonyAvatar = async ({ src }: { src: string }) => {
+export const TestimonyAvatar = ({ src }: { src: string }) => {
+  const animation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.DOWN()();
   return (
-    <div className="testimony-avatar">
+    <MotionDiv {...animation} className="testimony-avatar">
       <Image src={src} alt="avatar" width={600} height={600} />
-    </div>
+    </MotionDiv>
   );
 };
