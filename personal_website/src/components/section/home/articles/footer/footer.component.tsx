df6e2b17839de7687b363d91807918b1bba1b7d2
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-05 22:45:03 -0300

    [ :clapper: ] | animate:  article - platforms (app::component)

--- personal_website/src/components/section/home/articles/footer/footer.component.tsx
+++ personal_website/src/components/section/home/articles/footer/footer.component.tsx
@@ -2,19 +2,32 @@ import { IArticleSectionFooter } from '@/@types/props/section/articles/footer';
 import { Platform } from '@/components/platform/item/item.component';
 
 import './footer.srtyle.scss';
+import { MotionH2 } from '@/components/motion/h2.component';
+import { MODULES } from '@/modules/app.factory';
+import { MotionLI } from '@/components/motion/li.component';
 
-export const ArticlesSectionFooter = ({
+export const ArticlesSectionFooter = async ({
   platforms,
   html,
 }: IArticleSectionFooter) => {
+  const animation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.IN()({
+    animate: 'inactive',
+  });
+
+  const itemAnimation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.UP();
+
   return (
     <footer id="article-section-footer" {...html}>
-      <h2>I writte articles on this platforms</h2>
+      <MotionH2 {...animation}>I writte articles on this platforms</MotionH2>
       <ul>
-        {platforms.map(p => (
-          <li key={p.name} className="w-fit h-fit">
+        {platforms.map((p, i) => (
+          <MotionLI
+            {...itemAnimation({ transition: { delay: i * 0.35 } })}
+            key={p.name}
+            className="w-fit h-fit"
+          >
             <Platform platform={p} />
-          </li>
+          </MotionLI>
         ))}
       </ul>
     </footer>
