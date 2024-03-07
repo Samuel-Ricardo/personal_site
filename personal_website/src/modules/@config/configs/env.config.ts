6d6a1ad858ffee3b733529a5261fdc51671e58f6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 09:11:45 -0300

    [ :gear: ] | create: config - env > mailer (app::modules)

--- personal_website/src/modules/@config/configs/env.config.ts
+++ personal_website/src/modules/@config/configs/env.config.ts
@@ -7,4 +7,15 @@ export const ENV = {
     DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET || '',
     TOKEN: process.env.NEXT_PUBLIC_SANITY_TOKEN || '',
   },
+  MAILER: {
+    KEY: {
+      PUBLIC: process.env.NEXT_PUBLIC_MAILER_PUBLIC_KEY || '',
+    },
+    SERVICE: {
+      ID: process.env.NEXT_PUBLIC_MAILER_SERVICE_ID || '',
+    },
+    TEMPLATE: {
+      ID: process.env.NEXT_PUBLIC_MAILER_TEMPLATE_ID || '',
+    },
+  },
 };
