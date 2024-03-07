9d7a6b3b49859bfb0c549490b1c00cfecfba7d91
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 12:10:23 -0300

    [ :syringe: ] | update: module - gateway > email (module::cms)

--- personal_website/src/modules/gateway/gateway.module.ts
+++ personal_website/src/modules/gateway/gateway.module.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
 
 import { Container } from 'inversify';
+import lazy from 'inversify-inject-decorators';
 import { CMS_GATEWAY_MODULE } from './CMS/cms.module';
 import { ENGINE_MODULE } from '../engine/engine.module';
+import { CONFIG_MODULE } from '../@config/config.module';
+import { GATEWAY_EMAIL_MODULE } from './mailer/email.module';
 
 const MODULE = new Container({
   defaultScope: 'Singleton',
@@ -11,6 +14,10 @@ const MODULE = new Container({
 
 export const GATEWAY_MODULE = Container.merge(
   MODULE,
-  CMS_GATEWAY_MODULE,
   ENGINE_MODULE,
+  CONFIG_MODULE,
+  CMS_GATEWAY_MODULE,
+  GATEWAY_EMAIL_MODULE,
 );
+
+export const { lazyInject: injectGateway } = lazy(GATEWAY_MODULE);
