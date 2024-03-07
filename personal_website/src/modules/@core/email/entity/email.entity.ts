bf827aa1f5f14f5382c4fd5c9b0564f82fb9421a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 11:09:21 -0300

    [ :sparkles: ] | create: entity - email > toDTO (module::core)

--- personal_website/src/modules/@core/email/entity/email.entity.ts
+++ personal_website/src/modules/@core/email/entity/email.entity.ts
@@ -1,9 +1,19 @@
+import { IEmailDTO } from '../DTO/email.dto';
+
 export class Email {
   constructor(
     protected readonly _to: string,
     protected readonly _from: string,
     protected readonly _message: string,
   ) {}
+
+  toDTO(): IEmailDTO {
+    return {
+      to: this._to,
+      from: this._from,
+      message: this._message,
+    };
+  }
 
   get to(): string {
     return this._to;
