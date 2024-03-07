e80bd8b06d1b953166cae3dd6f742cd6fd46511f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 10:57:18 -0300

    [ :syringe: ] | update: registry - engine > mailer (app::modules)

--- personal_website/src/modules/engine/engine.registry.ts
+++ personal_website/src/modules/engine/engine.registry.ts
@@ -1,5 +1,7 @@
 import { CMS_ENGINE_REGISTRY as CMS } from './CMS/cms.registry';
+import { ENGINE_MAILER_REGISTRY as MAILER } from './mailer/mailer.registry';
 
 export const ENGINE_REGISTRY = {
   CMS,
+  MAILER,
 };
