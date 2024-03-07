d41cfcbe08c06c385ce23cb869b0f530938cd51b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 12:09:30 -0300

    [ :syringe: ] | update: registry - gateway > email (module::cms)

--- personal_website/src/modules/gateway/gateway.registry.ts
+++ personal_website/src/modules/gateway/gateway.registry.ts
@@ -1,5 +1,7 @@
 import { CMS_GATEWAT_REGISTRY } from './CMS/cms.registry';
+import { GATEWAY_EMAIL_REGISTRY } from './mailer/email.registry';
 
 export const GATEWAY_REGISTRY = {
   CMS: CMS_GATEWAT_REGISTRY,
+  EMAIL: GATEWAY_EMAIL_REGISTRY,
 };
