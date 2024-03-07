bec1fc19cb6abcf9956fa372a4fdd2c44a513a75
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 15:43:38 -0300

    [ :video_game: ] | create: controller - email > sendEmail (module::core)

--- personal_website/src/modules/@core/email/controller/email.controller.ts
+++ personal_website/src/modules/@core/email/controller/email.controller.ts
@@ -2,6 +2,8 @@ import { inject, injectable } from 'inversify';
 import { IEmailModule } from '../email.interface';
 import { type IEmailService } from '../service/service.interface';
 import { MODULE } from '@/modules/app.registry';
+import { Email } from '../entity/email.entity';
+import { ISendEmailDTO } from '../DTO/send/email.dto';
 
 @injectable()
 export class EmailController implements IEmailModule {
@@ -9,4 +11,8 @@ export class EmailController implements IEmailModule {
     @inject(MODULE.EMAIL.SERVICE.MAIN)
     protected readonly service: IEmailService,
   ) {}
+
+  async sendEmail(email: ISendEmailDTO): Promise<Email> {
+    return await this.service.sendEmail(email);
+  }
 }
