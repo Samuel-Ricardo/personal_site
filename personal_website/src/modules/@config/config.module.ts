97c16044b49f86c9d7a0e7f48cfe29166c14ff9e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 12:04:48 -0300

    [ :syringe: ] | create: module - config > lazy (app::modules)

--- personal_website/src/modules/@config/config.module.ts
+++ personal_website/src/modules/@config/config.module.ts
@@ -1,11 +1,15 @@
 import { Container } from 'inversify';
 import { CONFIG_REGISTRY } from './config.registry';
-import { CONFIG } from './configs/global.config';
+import { CONFIG } from './configs/all.config';
+
+import lazy from 'inversify-inject-decorators';
 
 export const CONFIG_MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
-CONFIG_MODULE.bind(CONFIG_REGISTRY.GLOBAL).toConstantValue(CONFIG);
+CONFIG_MODULE.bind(CONFIG_REGISTRY.ALL).toConstantValue(CONFIG);
 CONFIG_MODULE.bind(CONFIG_REGISTRY.ENV).toConstantValue(CONFIG.ENV);
+
+export const { lazyInject: injectModule } = lazy(CONFIG_MODULE);
