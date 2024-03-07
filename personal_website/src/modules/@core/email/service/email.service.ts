9e14d7b33e987807fedddb873dc916e84d2ce6c5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 15:35:24 -0300

    [ :sparkles: ] | implements: service - email > sendEmail (module::core)

--- personal_website/src/modules/@core/email/service/email.service.ts
+++ personal_website/src/modules/@core/email/service/email.service.ts
@@ -2,11 +2,17 @@ import { inject, injectable } from 'inversify';
 import { IEmailService } from './service.interface';
 import { SendEmailUseCase } from '../use_case/send.use_case';
 import { MODULE } from '@/modules/app.registry';
+import { ISendEmailDTO } from '../DTO/send/email.dto';
+import { Email } from '../entity/email.entity';
 
 @injectable()
 export class EmailService implements IEmailService {
   constructor(
     @inject(MODULE.EMAIL.USE_CASE.SEND)
-    protected readonly sendEmail: SendEmailUseCase,
+    protected readonly send: SendEmailUseCase,
   ) {}
+
+  async sendEmail(email: ISendEmailDTO): Promise<Email> {
+    return await this.send.execute(email);
+  }
 }
