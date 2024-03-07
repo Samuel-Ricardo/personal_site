3f1c00598c68a24967413d2fc788ef9f723f4c4b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 15:36:16 -0300

    [ :syringe: ] | update: registry - email > service (module::core)

--- personal_website/src/modules/@core/email/email.registry.ts
+++ personal_website/src/modules/@core/email/email.registry.ts
@@ -1,4 +1,7 @@
 export const EMAIL_REGISTRY = {
+  SERVICE: {
+    MAIN: Symbol.for('MODULES::EMAIL::SERVICE::MAIN'),
+  },
   USE_CASE: {
     SEND: Symbol.for('MODULES::USE_CASE::EMAIL::SEND'),
   },
