c9f8163dea403f44e4d6f4656a80ecb90730b69b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 12:11:02 -0300

    [ :syringe: ] | update: factory - gateway > email (module::cms)

--- personal_website/src/modules/gateway/gateway.factory.ts
+++ personal_website/src/modules/gateway/gateway.factory.ts
@@ -1,5 +1,7 @@
 import { CMS_GATEWAT_REGISTRY } from './CMS/cms.registry';
+import { GATEWAY_EMAIL_FACTORY } from './mailer/email.factory';
 
 export const GATEWAY_FACTORY = {
   CMS: CMS_GATEWAT_REGISTRY,
+  EMAIL: GATEWAY_EMAIL_FACTORY,
 };
