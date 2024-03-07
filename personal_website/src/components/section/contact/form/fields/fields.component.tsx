ba55218afd7d617a415ed6530c9c724de3ea1f98
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-06 21:59:43 -0300

    [ :scroll: ] | setup: contact - form > fields (app::style)

--- personal_website/src/components/section/contact/form/fields/fields.component.tsx
+++ personal_website/src/components/section/contact/form/fields/fields.component.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { AnimatedInput } from '@/components/form/input/animated/input.component';
+import { AnimatedTextArea } from '@/components/form/textarea/animated/textarea.component';
 import { MotionForm } from '@/components/motion/form.component';
 import { MotionH3 } from '@/components/motion/h3.component';
 import { MODULES } from '@/modules/app.factory';
@@ -8,6 +9,7 @@ import { MODULES } from '@/modules/app.factory';
 export const ContactFormFields = ({ children }: { children: any }) => {
   const OUT = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.OUT()();
   const UP = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.UP();
+
   return (
     <MotionForm {...OUT}>
       <MotionH3 {...UP()}>{children}</MotionH3>
@@ -17,6 +19,23 @@ export const ContactFormFields = ({ children }: { children: any }) => {
         hook={() => 'a' as any}
         animation={UP({ transition: { delay: 0.25 } })}
         input={{ type: 'text', required: true, placeholder: 'Name' }}
+      />
+      <AnimatedInput
+        label="Email"
+        hook={() => 'a' as any}
+        animation={UP({ transition: { delay: 0.5 } })}
+        input={{ type: 'email', required: true, placeholder: 'Email' }}
+      />
+
+      <AnimatedTextArea
+        label="Message"
+        hook={() => 'a' as any}
+        animation={UP({ transition: { delay: 0.75 } })}
+        input={{
+          rows: 10,
+          required: true,
+          placeholder: '🖋️ | Type a message...',
+        }}
       />
     </MotionForm>
   );
