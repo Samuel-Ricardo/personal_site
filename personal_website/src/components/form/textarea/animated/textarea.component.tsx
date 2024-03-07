949a89e3af30bb931b286aa02b60cd097c266ba5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 16:51:23 -0300

    [ :scroll: ] | update: contact - form > textares [error label] (app::style)

--- personal_website/src/components/form/textarea/animated/textarea.component.tsx
+++ personal_website/src/components/form/textarea/animated/textarea.component.tsx
@@ -1,3 +1,5 @@
+import './textarea.style.scss';
+
 import { IAnimatedTextAreaProps } from '@/@types/props/form/textarea';
 import { MotionDiv } from '@/components/motion/div.component';
 
@@ -8,18 +10,18 @@ export const AnimatedTextArea = ({
   error,
   input,
 }: IAnimatedTextAreaProps) => {
-  console.log({ error });
   return (
-    <MotionDiv {...animation} className="animated-input">
+    <MotionDiv {...animation} className="animated-textarea">
       {label && <label className="label">{label}</label>}
       <textarea {...input} {...hook()} />
       <MotionDiv
         className="underline"
+        initial={{ width: 0 }}
         animate={{ width: 0 }}
         whileInView={{ width: '100%' }}
-        transition={{ duration: 3.5 }}
+        transition={{ delay: 0.5, duration: 2.5 }}
       />
-      {error && <label className="error">error</label>}
+      {error && <label className="error">{error}</label>}
     </MotionDiv>
   );
 };
