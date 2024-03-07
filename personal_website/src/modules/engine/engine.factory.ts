6868f16cfa686ec78a5dcb6027d5c3549a17afb8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 10:58:40 -0300

    [ :syringe: ] | update: factory - engine > mailer (app::modules)

--- personal_website/src/modules/engine/engine.factory.ts
+++ personal_website/src/modules/engine/engine.factory.ts
@@ -1,5 +1,7 @@
 import { CMS_ENGINE_FACTORY as CMS } from './CMS/cms.factory';
+import { ENGINE_MAILER_FACTORY as MAILER } from './mailer/mailer.factory';
 
 export const ENGINE_FACTORY = {
   CMS,
+  MAILER,
 };
