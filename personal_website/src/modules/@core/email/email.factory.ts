de7a24b4f5c0724172436f0069fcb250baf62f4e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 15:37:40 -0300

    [ :syringe: ] | update: factory - email > service (module::core)

--- personal_website/src/modules/@core/email/email.factory.ts
+++ personal_website/src/modules/@core/email/email.factory.ts
@@ -1,8 +1,12 @@
 import { EMAIL_MODULE } from './email.module';
 import { EMAIL_REGISTRY } from './email.registry';
+import { IEmailService } from './service/service.interface';
 import { SendEmailUseCase } from './use_case/send.use_case';
 
 export const EMAIL_FACTORY = {
+  SERVICE: {
+    MAIN: () => EMAIL_MODULE.get<IEmailService>(EMAIL_REGISTRY.SERVICE.MAIN),
+  },
   USE_CASE: {
     SEND: () =>
       EMAIL_MODULE.get<SendEmailUseCase>(EMAIL_REGISTRY.USE_CASE.SEND),
