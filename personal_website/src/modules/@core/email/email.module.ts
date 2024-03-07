b99c5204133f3e90f2e127cc15ff5820c8dca6f0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 15:46:44 -0300

    [ :syringe: ] | update: module - email > controller (module::core)

--- personal_website/src/modules/@core/email/email.module.ts
+++ personal_website/src/modules/@core/email/email.module.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { EMAIL_REGISTRY } from './email.registry';
 import { SendEmailUseCase } from './use_case/send.use_case';
 import { EmailService } from './service/email.service';
+import { EmailController } from './controller/email.controller';
 
 const MODULE = new Container({
   defaultScope: 'Singleton',
@@ -12,3 +13,5 @@ export const EMAIL_MODULE = MODULE;
 
 MODULE.bind(EMAIL_REGISTRY.USE_CASE.SEND).to(SendEmailUseCase);
 MODULE.bind(EMAIL_REGISTRY.SERVICE.MAIN).to(EmailService);
+MODULE.bind(EMAIL_REGISTRY.CONTROLLER.MAIN).to(EmailController);
+MODULE.bind(EMAIL_REGISTRY.MAIN).to(EmailController);
