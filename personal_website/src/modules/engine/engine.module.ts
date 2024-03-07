8af8be03af7ca5d20031073ee6702bba28c81efe
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-07 10:58:14 -0300

    [ :syringe: ] | update: module - engine > mailer (app::modules)

--- personal_website/src/modules/engine/engine.module.ts
+++ personal_website/src/modules/engine/engine.module.ts
@@ -1,6 +1,11 @@
 import { Container } from 'inversify';
 import { CMS_ENGINE_MODULE } from './CMS/cms.module';
+import { ENGINE_MAILER_MODULE } from './mailer/mailer.module';
 
 const MODULE = new Container({ defaultScope: 'Singleton' });
 
-export const ENGINE_MODULE = Container.merge(MODULE, CMS_ENGINE_MODULE);
+export const ENGINE_MODULE = Container.merge(
+  MODULE,
+  CMS_ENGINE_MODULE,
+  ENGINE_MAILER_MODULE,
+);
