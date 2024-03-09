0c046f6b48d41a40fab10277701de6d41ee2026e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-08 22:35:53 -0300

    [ :bookmark_tabs: ] | update: page - under development (app::page)

--- personal_website/src/app/pages/under_development.page.tsx
+++ personal_website/src/app/pages/under_development.page.tsx
@@ -11,7 +11,7 @@ const images = [
 
 export const UnderDevelopmentPage = async () => {
   return (
-    <div className="flex flex-col w-screen h-screen justify-center items-center gap-10">
+    <div className="flex flex-col w-screen h-screen justify-center items-center text-center gap-10">
       <h1>Hi! :) This page is under development 🌱</h1>
       <Image
         src={images[Math.floor(Math.random() * images.length)]}
