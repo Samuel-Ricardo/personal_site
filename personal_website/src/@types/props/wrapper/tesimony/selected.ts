f53ab870c9e9fa0a3047c837365164342def6ab9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-08 22:32:08 -0300

    [ :label: ] | update: props - wrapper > testimony (@types::props)

--- personal_website/src/@types/props/wrapper/tesimony/selected.ts
+++ personal_website/src/@types/props/wrapper/tesimony/selected.ts
@@ -1,7 +1,9 @@
 import { ITestimonialDTO } from '@/modules/@core/testimonial/DTO/testimonial.dto';
+import { MotionProps } from 'framer-motion';
 
 export interface ISelectedTestimonyWrapperProps {
   data: ITestimonialDTO;
   children: any;
   className?: string;
+  animation?: MotionProps;
 }
