434de72e5fc6da6547b4622900092fb635b1a115
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-08 22:10:11 -0300

    [ :label: ] | update: type - card > social (app::components)

--- personal_website/src/@types/props/card/social.ts
+++ personal_website/src/@types/props/card/social.ts
@@ -2,6 +2,7 @@ import { IPersonDTO } from '@/modules/@core/person/DTO/person.dto';
 import { ITestimonialDTO } from '@/modules/@core/testimonial/DTO/testimonial.dto';
 
 export interface ISocialCardProps {
+  index?: number;
   data: ITestimonialDTO;
 }
 
