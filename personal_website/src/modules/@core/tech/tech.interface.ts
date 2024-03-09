be0a7ab85eb428a2db6941d27cae836276ab5242
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 11:21:10 -0300

    [ :sparkles: ] | update: interface - tech (module::core)

--- personal_website/src/modules/@core/tech/tech.interface.ts
+++ personal_website/src/modules/@core/tech/tech.interface.ts
@@ -1,6 +1,8 @@
+import { IFindTechByContextDTO } from './DTO/use_case/find/by/context.dto';
 import { Tech } from './entity/tech.entity';
 
 export interface ITechModule {
   findAll(): Promise<Tech[]>;
-  findByContext(context: string): Promise<Tech[]>;
+  //  findOne(DTO: IAssemblerFindDTO): Promise<Tech>;
+  findByContext(context: IFindTechByContextDTO): Promise<Tech[]>;
 }
