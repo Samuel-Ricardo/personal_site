1e8313293b80e759ddbcab646fe23fade3aa2a4a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 10:12:51 -0300

    [ :card_file_box: ] | create: data - techs (data::static)

---
+++ personal_website/src/local/data/static/techs.data.ts
@@ -0,0 +1,10 @@
+export const AND_MORE: any = {
+  icon: 'https://cdn-icons-png.flaticon.com/128/8111/8111042.png',
+  preview: {
+    name: 'And More...',
+    description: 'These are just the highlights, visit projects to see more.',
+    images: [
+      'https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    ],
+  },
+};
