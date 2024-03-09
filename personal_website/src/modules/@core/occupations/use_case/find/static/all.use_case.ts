81121aca6f9417f8df93946f83c2f3036f614974
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 11:40:23 -0300

    [ :label: ] | update: use-case - find [all] > occupation (module::core::static)

--- personal_website/src/modules/@core/occupations/use_case/find/static/all.use_case.ts
+++ personal_website/src/modules/@core/occupations/use_case/find/static/all.use_case.ts
@@ -1,13 +1,12 @@
 import { MODULE } from '@/modules/app.registry';
-import { inject, injectable } from 'inversify';
+import { injectable } from 'inversify';
 import { type IOccupationGateway } from '../../../gateway/occupation.gateway';
+import { injectGateway } from '@/modules/gateway/gateway.module';
 
 @injectable()
 export class FindAllStaticOccupationsUseCase {
-  constructor(
-    @inject(MODULE.GATEWAY.CMS.STATIC.OCCUPATION)
-    protected readonly gateway: IOccupationGateway,
-  ) {}
+  @injectGateway(MODULE.GATEWAY.CMS.STATIC.OCCUPATION)
+  protected readonly gateway: IOccupationGateway;
 
   async execute() {
     return this.gateway.findAll();
