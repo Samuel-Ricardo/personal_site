a1af168e761548cf70862ddbb77c3aa58f1fea61
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 09:52:12 -0300

    [ :zap: ] | improve: projects - container (app::component)

--- personal_website/src/components/section/home/projects/container/container.component.tsx
+++ personal_website/src/components/section/home/projects/container/container.component.tsx
@@ -3,7 +3,7 @@ import { ProjectItem } from '../item/item.component';
 
 import './container.style.scss';
 
-export const ProjectsContainer = (props: IProjectsContainerProps) => {
+export const ProjectsContainer = async (props: IProjectsContainerProps) => {
   const { items } = props;
 
   return (
