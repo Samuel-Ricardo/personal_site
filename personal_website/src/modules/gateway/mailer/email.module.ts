ec290abb9379d2a700968146ea1dd689ef1e7b36
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 10:45:55 -0300

    [ :syringe: ] | update: module - gateway > email (module::cms)

--- personal_website/src/modules/gateway/mailer/email.module.ts
+++ personal_website/src/modules/gateway/mailer/email.module.ts
@@ -1,4 +1,6 @@
 import { Container } from 'inversify';
+import { GATEWAY_EMAIL_REGISTRY } from './email.registry';
+import { EmailJSGateway } from './emailjs/email.gateway';
 
 const MODULE = new Container({
   defaultScope: 'Singleton',
@@ -6,3 +8,5 @@ const MODULE = new Container({
 });
 
 export const GATEWAY_EMAIL_MODULE = MODULE;
+
+GATEWAY_EMAIL_MODULE.bind(GATEWAY_EMAIL_REGISTRY.EMAILJS).to(EmailJSGateway);
