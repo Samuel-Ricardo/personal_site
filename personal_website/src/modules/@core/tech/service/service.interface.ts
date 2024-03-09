89a92c3ac35059c026ed170fdeae78ffaf5340b8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 11:24:29 -0300

    [ :sparkles: ] | update: interface - service > tech (module::core)

--- personal_website/src/modules/@core/tech/service/service.interface.ts
+++ personal_website/src/modules/@core/tech/service/service.interface.ts
@@ -1,6 +1,7 @@
+import { IFindTechByContextDTO } from '../DTO/use_case/find/by/context.dto';
 import { Tech } from '../entity/tech.entity';
 
 export interface ITechService {
   findAll(): Promise<Tech[]>;
-  findByContext(context: string): Promise<Tech[]>;
+  findByContext(context: IFindTechByContextDTO): Promise<Tech[]>;
 }
