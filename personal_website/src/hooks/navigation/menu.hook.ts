ba74c1c7e15d36de8f014e1cc98b8493a0caef64
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 10:06:09 -0300

    [ :art: ] | create: wrapper - toggle > menu (app::component)

--- personal_website/src/hooks/navigation/menu.hook.ts
+++ personal_website/src/hooks/navigation/menu.hook.ts
@@ -4,6 +4,7 @@ import { create } from 'zustand';
 export const useMenu = create<IMenuState>(set => {
   return {
     open: false,
+    toggle: () => set(state => ({ open: !state.open })),
     setOpen: open => set({ open }),
   };
 });
