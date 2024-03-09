198987ed122af606fa2741d4e4950034e1b43e91
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 10:12:33 -0300

    [ :card_file_box: ] | create: data - person (data::static)

--- personal_website/src/local/data/static/person.data.ts
+++ personal_website/src/local/data/static/person.data.ts
@@ -1,8 +1,12 @@
 import { IPersonDTO } from '@/modules/@core/person/DTO/person.dto';
 
-export const PERSON_SAMUEL: IPersonDTO = {
-  name: 'Samuel',
-  title: ' Full Stack',
-  avatar:
-    'https://cdn.sanity.io/images/tk1o3drt/production/5077455964743b0c525b643805aedf8d22fdbd2f-4618x3464.jpg?w=2000&fit=max&auto=format',
-};
+export const PERSONS: IPersonDTO[] = [
+  {
+    name: 'Samuel',
+    title: ' Full Stack',
+    avatar:
+      'https://cdn.sanity.io/images/tk1o3drt/production/5077455964743b0c525b643805aedf8d22fdbd2f-4618x3464.jpg?w=2000&fit=max&auto=format',
+    company: {} as any,
+    contacts: [] as any,
+  },
+];
