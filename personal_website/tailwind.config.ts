fdeffb9130f48d669e6d34f21190f48fe4ca4d86
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 10:20:35 -0300

    [ :gear: ] | update: config - tailwind > plugins (style)

--- personal_website/tailwind.config.ts
+++ personal_website/tailwind.config.ts
@@ -77,6 +77,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar')],
 };
 export default config;
