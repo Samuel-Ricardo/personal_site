b3fc995f27adc6a3da90d385c7dd1f7a636940e5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-08 23:27:48 -0300

    [ :art: ] | update: carrousel - horizontal > static (app::component)

--- personal_website/src/components/carousel/horizontal/static/static.carousel.tsx
+++ personal_website/src/components/carousel/horizontal/static/static.carousel.tsx
@@ -1,27 +1,12 @@
 import { ISliderProps } from '@/@types/props/carousel/horizontal/static/carousel';
 
 import './static.style.scss';
-import { RightArrow } from '@/assets/icons/arrow/right/arrow.icon';
-import { LeftArrow } from '@/assets/icons/arrow/left/arrow.icon';
+import { SliderBody } from './body/body.component';
 
 export const Slider = async ({ children }: ISliderProps) => {
   return (
     <div className="static-carousel">
-      <button className="left-button">
-        <LeftArrow />
-      </button>
-
-      <ul className="static-carousel-container">
-        {children.map(child => (
-          <li key={child?.toString()} className="h-fit w-fit">
-            {child}
-          </li>
-        ))}
-      </ul>
-
-      <button className="right-button">
-        <RightArrow />
-      </button>
+      <SliderBody>{children}</SliderBody>
     </div>
   );
 };
