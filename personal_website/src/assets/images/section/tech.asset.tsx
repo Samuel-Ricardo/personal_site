6e5d12809207873c8b321757e75744bfe37e23c3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-08 23:00:06 -0300

    [ :bento: ] | create: assets - ilustration > techs (app::assets)

--- personal_website/src/assets/images/section/tech.asset.tsx
+++ personal_website/src/assets/images/section/tech.asset.tsx
@@ -1,14 +1,21 @@
+import { MODULES } from '@/modules/app.factory';
 import Image from 'next/image';
 
-export const TechsIlustration = () => {
+export const TechsIlustration = async () => {
+  const src = await MODULES.ASSEMBLER.MAIN().findImage({
+    identifier: 'home_techs',
+  });
+
   return (
     <Image
       id="techs-ilustration-img"
-      src={'/techs-ilustration.jpg'}
+      src={src || '/techs-ilustration.jpg'}
       width={400}
       height={400}
       alt="techs section ilustration"
       className="min-w-fit min-h-fit object-contain"
+      placeholder="blur"
+      blurDataURL="/techs-ilustration.jpg"
     />
   );
 };
