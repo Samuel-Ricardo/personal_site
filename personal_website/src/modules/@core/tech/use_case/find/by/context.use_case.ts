bfa4f8a0445a8c2e34058535f61b7e7d6e659642
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 11:19:23 -0300

    [ :sparkles: ] | update: use-case - find by [context] > tech (module::core)

--- personal_website/src/modules/@core/tech/use_case/find/by/context.use_case.ts
+++ personal_website/src/modules/@core/tech/use_case/find/by/context.use_case.ts
@@ -1,9 +1,10 @@
 import { injectable } from 'inversify';
 import { TechGatewaySupport } from '../../../gateway/gateway.support';
+import { IFindTechByContextDTO } from '../../../DTO/use_case/find/by/context.dto';
 
 @injectable()
 export class FindTechByContextUseCase extends TechGatewaySupport {
-  async execute(context: string) {
+  async execute(context: IFindTechByContextDTO) {
     return await this.gateway.findByContext(context);
   }
 }
