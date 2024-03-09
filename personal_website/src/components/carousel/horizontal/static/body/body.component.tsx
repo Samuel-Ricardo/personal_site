0f504a8295eb0ce6ad58f1d7aba63e31a2624cc8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-08 23:09:15 -0300

    [ :zap: ] | improve: carrousel - horizontal > static [body] (app::component)

--- personal_website/src/components/carousel/horizontal/static/body/body.component.tsx
+++ personal_website/src/components/carousel/horizontal/static/body/body.component.tsx
@@ -1,19 +1,13 @@
 import './body.style.scss';
 
-export const SliderBody = ({ children }: { children: any[] }) => {
+export const SliderBody = async ({ children }: { children: any[] }) => {
   return (
     <ul className="static-carousel-container">
-      {children
-        .concat(children)
-        .concat(children)
-        .concat(children)
-        .concat(children)
-        .concat(children)
-        .map(child => (
-          <li key={child?.toString()} className="h-fit w-fit">
-            {child}
-          </li>
-        ))}
+      {children.map(child => (
+        <li key={child?.toString()} className="h-fit w-fit">
+          {child}
+        </li>
+      ))}
     </ul>
   );
 };
