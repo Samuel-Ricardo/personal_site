6f8e3bff9fd68c490c3bbdf0a4e51f5ffc19573c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-08 23:04:12 -0300

    [ :art: ] | update: card - social > footer (app::components)

--- personal_website/src/components/card/social/footer/footer.component.tsx
+++ personal_website/src/components/card/social/footer/footer.component.tsx
@@ -1,11 +1,18 @@
+'use client';
+
 import { ISocialCardFooterProps } from '@/@types/props/card/social';
 import './footer.style.scss';
+import { useSelectedTestimonial } from '@/hooks/testimonial/select.hook';
 
-export const SocialCardFooter = async ({
+export const SocialCardFooter = ({
   title,
   name,
   company,
 }: ISocialCardFooterProps) => {
+  const { selected } = useSelectedTestimonial();
+
+  if (selected?.data.person.name !== name) return null;
+
   return (
     <footer className="card-footer">
       <p className="name">{name}</p>
