76c04a427f7662d441ccd08d23803a246ff7fcdb
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-08 22:44:46 -0300

    [ :heart: ] | update: icon - share (app::component)

--- personal_website/src/assets/icons/share/icon.component.tsx
+++ personal_website/src/assets/icons/share/icon.component.tsx
@@ -1,6 +1,7 @@
 export const ShareIcon = () => {
   return (
     <svg
+      id="share-icon"
       className="w-6 h-6 text-gray-800 dark:text-white"
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
@@ -10,7 +11,7 @@ export const ShareIcon = () => {
       <path
         stroke="currentColor"
         strokeLinecap="round"
-        strokeWidth="2"
+        strokeWidth="1"
         d="m8 10.9 7-3.2m-7 5.4 7 3.2M8 12a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm12 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm0-11a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
       />
     </svg>
