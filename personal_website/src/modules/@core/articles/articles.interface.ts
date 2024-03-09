042b7e6780b5a31bbf39107601051587e4d1f8b5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 10:52:13 -0300

    [ :sparkles: ] | update: interface - article (module::core)

--- personal_website/src/modules/@core/articles/articles.interface.ts
+++ personal_website/src/modules/@core/articles/articles.interface.ts
@@ -2,5 +2,5 @@ import { Article } from './entity/article.entity';
 
 export interface IArticleModule {
   findAll(): Promise<Article[]>;
-  findMainArticles(): Promise<Article>;
+  findMainArticles(): Promise<Article[]>;
 }
