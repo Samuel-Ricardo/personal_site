617f7f95ac7f48fbd25f3725bd55bcda3bc39d67
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 12:06:38 -0300

    [ :door: ] | update: gateway - company (module::core)

--- personal_website/src/modules/@core/company/gateway/company.gateway.ts
+++ personal_website/src/modules/@core/company/gateway/company.gateway.ts
@@ -1,7 +1,7 @@
-import { IFindOnePersonDTO } from '../../person/DTO/gateway/find/one.dto';
+import { IFindOneComapanyDTO } from '../DTO/find/one.dto';
 import { Company } from '../entity/commpany.entity';
 
 export interface ICompanyGateway {
-  findAll(): Promise<Company>[];
-  findOne(DTO: IFindOnePersonDTO): Promise<Company>;
+  findAll(): Promise<Promise<Company>[]>;
+  findOne(DTO: IFindOneComapanyDTO): Promise<Company>;
 }
