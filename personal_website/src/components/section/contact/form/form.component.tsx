9b60be26fe0b8811770c9d482f00bdf95b9ecabc
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 09:22:07 -0300

    [ :scroll: ] | update: contact - form > container (app::component)

--- personal_website/src/components/section/contact/form/form.component.tsx
+++ personal_website/src/components/section/contact/form/form.component.tsx
@@ -1,17 +1,14 @@
 import { IContactFormProps } from '@/@types/props/section/contact/form';
 import './form.style.scss';
 
-import { MotionH3 } from '@/components/motion/h3.component';
-import { MODULES } from '@/modules/app.factory';
-import { MotionForm } from '@/components/motion/form.component';
+import { ContactFormFields } from './fields/fields.component';
 
 export const ContactForm = async ({ title }: IContactFormProps) => {
-  const OUT = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.OUT()();
   return (
     <div className="contact-form-container">
-      <MotionForm {...OUT}>
-        <MotionH3>{(await title) || 'Write me a letter 🖋️'}</MotionH3>
-      </MotionForm>
+      <ContactFormFields>
+        {(await title) || 'Write me a letter 🖋️'}
+      </ContactFormFields>
     </div>
   );
 };
