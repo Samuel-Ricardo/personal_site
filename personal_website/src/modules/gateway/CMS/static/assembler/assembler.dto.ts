420af6bac91241b4a0a5cdd495dbfdfca6aad5e7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 10:49:16 -0300

    [ :door: ] | update: gateway - assembler (module::static)

--- personal_website/src/modules/gateway/CMS/static/assembler/assembler.dto.ts
+++ personal_website/src/modules/gateway/CMS/static/assembler/assembler.dto.ts
@@ -2,8 +2,13 @@ import { TEXT } from '@/local/data/static/text.data';
 import { TITLES } from '@/local/data/static/title.data';
 import { IAssemblerFindDTO } from '@/modules/@core/assembler/DTO/gateway/find/index.dto';
 import { IAssemblerGateway } from '@/modules/@core/assembler/gateway/assembler.gateway';
+import { injectable } from 'inversify';
 
+@injectable()
 export class StaticAssemblerGateway implements IAssemblerGateway {
+  findImage(DTO: IAssemblerFindDTO): Promise<string | undefined> {
+    throw new Error('Method not implemented.');
+  }
   async findTitle(DTO: IAssemblerFindDTO) {
     return TITLES.find(async title => title.identifier === DTO.identifier)
       ?.content;
