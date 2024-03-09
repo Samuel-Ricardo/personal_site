a48331d73fdc1d9cac4408cea478c740deda9eef
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 11:38:09 -0300

    [ :door: ] | update: support - gateway > person (module::core)

--- personal_website/src/modules/@core/person/gateway/gateway.support.ts
+++ personal_website/src/modules/@core/person/gateway/gateway.support.ts
@@ -1,11 +1,10 @@
 import { inject, injectable } from 'inversify';
 import { type IPersonGateway } from './person.gateway';
 import { MODULE } from '@/modules/app.registry';
+import { injectGateway } from '@/modules/gateway/gateway.module';
 
 @injectable()
 export abstract class PersonGatewaySupport {
-  constructor(
-    @inject(MODULE.GATEWAY.CMS.SANITY.PERSON)
-    protected readonly gateway: IPersonGateway,
-  ) {}
+  @injectGateway(MODULE.GATEWAY.CMS.SANITY.PERSON)
+  protected readonly gateway: IPersonGateway;
 }
