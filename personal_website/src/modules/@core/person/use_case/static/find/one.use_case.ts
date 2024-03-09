4806e882929dadd6309fcf039f976938d9875a89
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 11:36:23 -0300

    [ :sparkles: ] | update: use-case - find [one] > person (module::core:static)

--- personal_website/src/modules/@core/person/use_case/static/find/one.use_case.ts
+++ personal_website/src/modules/@core/person/use_case/static/find/one.use_case.ts
@@ -1,14 +1,13 @@
-import { inject, injectable } from 'inversify';
+import { injectable } from 'inversify';
 import { type IPersonGateway } from '../../../gateway/person.gateway';
 import { MODULE } from '@/modules/app.registry';
 import { IFindOnePersonDTO } from '../../../DTO/gateway/find/one.dto';
+import { injectGateway } from '@/modules/gateway/gateway.module';
 
 @injectable()
 export class FindOneStaticPersonUseCase {
-  constructor(
-    @inject(MODULE.GATEWAY.CMS.STATIC.PERSON)
-    protected readonly gateway: IPersonGateway,
-  ) {}
+  @injectGateway(MODULE.GATEWAY.CMS.STATIC.PERSON)
+  protected readonly gateway: IPersonGateway;
 
   async execute(DTO: IFindOnePersonDTO) {
     return await this.gateway.findOne(DTO);
