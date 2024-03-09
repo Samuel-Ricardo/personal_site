247bc4fbbce0ae89fdf51c11ca9b9fd95c7609ab
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 11:16:32 -0300

    [ :clapper: ] | update: animation - emerge [up] (app::modules:framer_motion)

--- personal_website/src/modules/animations/framer_motion/impl/emerge/up.animation.tsx
+++ personal_website/src/modules/animations/framer_motion/impl/emerge/up.animation.tsx
@@ -11,10 +11,12 @@ export const EMERGE_UP_ANIMATION: Animator = props => ({
     inactive: {
       translateY: '50%',
       opacity: 0,
+      filter: 'blur(5px)',
     },
     active: {
       translateY: 0,
       opacity: 1,
+      filter: 'blur(0px)',
     },
   },
 });
