c3354d70494e9ecd6d505382e44f658a657aae8b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-08 22:38:25 -0300

    [ :zap: ] | improve: section - hero (app::component)

--- personal_website/src/app/sections/home/hero/hero.section.tsx
+++ personal_website/src/app/sections/home/hero/hero.section.tsx
@@ -2,7 +2,7 @@ import { Circuit } from '@/components/canvas/circuit.component';
 import './hero.style.scss';
 import { HeroCard } from '@/components/section/home/hero/card.component';
 
-export const Hero = () => {
+export const Hero = async () => {
   return (
     <section id="hero">
       <Circuit className="absolute top-0 left-0 right-0 h-screen w-full bg-primary-foreground z-0" />
