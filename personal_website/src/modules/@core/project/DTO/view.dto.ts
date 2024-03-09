6ec762141c40676fac3b65e079d572c0dd8ff7e4
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 11:28:54 -0300

    [ :label: ] | create: DTO - project > view (module::core)

--- personal_website/src/modules/@core/project/DTO/view.dto.ts
+++ personal_website/src/modules/@core/project/DTO/view.dto.ts
@@ -1,3 +1,11 @@
-import { IProjectItemProps } from '@/@types/props/section/project/item';
+import { ITechViewDTO } from '../../tech/DTO/view.dto';
 
-export interface IProjectViewDTO extends IProjectItemProps {}
+export interface IProjectViewDTO {
+  title: string;
+  description: string;
+  image: string;
+  repo?: string;
+  demo?: string;
+  link?: string;
+  main_techs: ITechViewDTO[];
+}
