265e88e1484fea43eeee9ce0afe2634d8fb49738
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 10:47:04 -0300

    [ :syringe: ] | update: module - gateway (app::modules)

--- personal_website/src/modules/gateway/gateway.module.ts
+++ personal_website/src/modules/gateway/gateway.module.ts
@@ -4,7 +4,6 @@ import { Container } from 'inversify';
 import lazy from 'inversify-inject-decorators';
 import { CMS_GATEWAY_MODULE } from './CMS/cms.module';
 import { ENGINE_MODULE } from '../engine/engine.module';
-import { CONFIG_MODULE } from '../@config/config.module';
 import { GATEWAY_EMAIL_MODULE } from './mailer/email.module';
 
 const MODULE = new Container({
@@ -15,7 +14,6 @@ const MODULE = new Container({
 export const GATEWAY_MODULE = Container.merge(
   MODULE,
   ENGINE_MODULE,
-  CONFIG_MODULE,
   CMS_GATEWAY_MODULE,
   GATEWAY_EMAIL_MODULE,
 );
