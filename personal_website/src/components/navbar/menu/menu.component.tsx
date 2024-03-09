627e9a7b22b9ba32b3eea23d6cb126c95fe7da7f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-08 23:29:54 -0300

    [ :art: ] | update: component - navmenu > sections (app::component)

--- personal_website/src/components/navbar/menu/menu.component.tsx
+++ personal_website/src/components/navbar/menu/menu.component.tsx
@@ -3,12 +3,16 @@ import { MenuContainer } from './container/container.component';
 import { MODULES } from '@/modules/app.factory';
 
 import './menu.style.scss';
+import { NavMenu } from '@/components/navigation/menu/menu.component';
+import { MenuTogglerWrapper } from '@/components/wrapper/toggler/menu/wrapper.component';
 
 export const Menu = () => {
   return (
     <MenuContainer>
-      {' '}
-      <MenuIcon Paths={MODULES.ANIMATION.FRAMER_MOTION.SVG.DRAW()()} />
+      <MenuTogglerWrapper>
+        <MenuIcon Paths={MODULES.ANIMATION.FRAMER_MOTION.SVG.DRAW()()} />
+      </MenuTogglerWrapper>
+      <NavMenu></NavMenu>
     </MenuContainer>
   );
 };
