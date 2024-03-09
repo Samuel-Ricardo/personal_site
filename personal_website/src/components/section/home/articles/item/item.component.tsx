1e3eebafaf2e8d0fe4017e17e2913e87eddded46
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 09:39:30 -0300

    [ :art: ] | update: component - articles > item (app::component)

--- personal_website/src/components/section/home/articles/item/item.component.tsx
+++ personal_website/src/components/section/home/articles/item/item.component.tsx
@@ -5,7 +5,8 @@ import Image from 'next/image';
 import { Platform } from '@/components/platform/item/item.component';
 import { MODULES } from '@/modules/app.factory';
 import { MotionLI } from '@/components/motion/li.component';
-//import { useEffect, useMemo, useRef } from 'react';
+import { MouseBall } from '@/components/canvas/mouse/ball.component';
+import React from 'react';
 
 export const ArticleItem = async ({
   index,
@@ -13,21 +14,6 @@ export const ArticleItem = async ({
   html,
   article,
 }: IArticleItemProps) => {
-  //  const card = useRef<HTMLLIElement>(null);
-  //  const a = useMemo(() => article, [article]);
-
-  /*
-  useEffect(() => {
-    if (card.current)
-      card.current.onmousemove = e => {
-        const x = e.clientX - (card.current?.offsetLeft || 0);
-        const y = e.clientY - (card.current?.offsetTop || 0);
-        card.current?.style.setProperty('--x', `${x}px`);
-        card.current?.style.setProperty('--y', `${y}px`);
-      };
-  }, [card]);
-  */
-
   const animation = MODULES.ANIMATION.FRAMER_MOTION.SLIDE.UP()({
     animate: 'inactive',
     transition: { delay: (index || 0) * 0.35 },
@@ -40,6 +26,7 @@ export const ArticleItem = async ({
       key={key}
       className="article-section-item"
     >
+      <MouseBall />
       <h1 className="title">{article.title}</h1>
 
       {article.image && (
