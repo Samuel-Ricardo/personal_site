1dd6fcb274f61d8b59d83a310c07333742c9b627
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 09:50:28 -0300

    [ :art: ] | update: card - hero > footer (app::component)

--- personal_website/src/components/section/home/hero/card/content/footer/footer.component.tsx
+++ personal_website/src/components/section/home/hero/card/content/footer/footer.component.tsx
@@ -1,20 +1,29 @@
 import './footer.style.scss';
 
-import { Button } from '@/components/button/button.component';
 import { NavigateButton } from '@/components/button/navigate/button.component';
 import { DownloadIcon } from '@/assets/icons/download/download.icon';
 import { MODULES } from '@/modules/app.factory';
+import { IHeroFooterProps } from '@/@types/props/section/hero/about';
 
-export const HeroCardContentFooter = () => {
+export const HeroCardContentFooter = async ({
+  contact,
+  resume,
+}: IHeroFooterProps) => {
   return (
     <div className="footer">
-      <NavigateButton link={{ href: './#contact-me' }}>
-        Contact me
+      <NavigateButton link={{ href: '#contact-section' }}>
+        {contact}
       </NavigateButton>
-      <Button>
+      <NavigateButton
+        link={{
+          className: 'btn-resume',
+          href: 'https://cdn.sanity.io/files/tk1o3drt/production/4e34d166901b269a7e848ca89cebbe35fb614e93.pdf',
+          target: '_blank',
+        }}
+      >
         <DownloadIcon Paths={MODULES.ANIMATION.FRAMER_MOTION.SVG.DRAW()()} />
-        Resume
-      </Button>
+        {resume}
+      </NavigateButton>
     </div>
   );
 };
