55060a124e77f8c59807d847d05c95792e01e924
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 11:39:29 -0300

    [ :label: ] | update: DTO - find [all] > person (module::core)

--- personal_website/src/modules/@core/person/DTO/gateway/find/all.dto.ts
+++ personal_website/src/modules/@core/person/DTO/gateway/find/all.dto.ts
@@ -1,5 +1,3 @@
 import { ICMSGatewayConfig } from '@/@types/module/gateway/cms/sanity';
 
 export interface IFindAllPersonDTO extends ICMSGatewayConfig {}
-
-export interface IFindAllPersonOutputDTO {}
