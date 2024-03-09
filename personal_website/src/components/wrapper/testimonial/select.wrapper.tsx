a1a12138020dc24636a04b44c707c2b599344b85
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-08 22:31:11 -0300

    [ :art: ] | update: wrapper - testimony (app::component)

--- personal_website/src/components/wrapper/testimonial/select.wrapper.tsx
+++ personal_website/src/components/wrapper/testimonial/select.wrapper.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ISelectedTestimonyWrapperProps } from '@/@types/props/wrapper/tesimony/selected';
+import { MotionDiv } from '@/components/motion/div.component';
 import { useSelectedTestimonial } from '@/hooks/testimonial/select.hook';
 import { useEffect, useRef } from 'react';
 
@@ -8,14 +9,22 @@ export const SelectTestimonialWrapper = ({
   data,
   children,
   className,
+  animation,
 }: ISelectedTestimonyWrapperProps) => {
   const ref = useRef<HTMLDivElement>(null);
 
   const { setSelected } = useSelectedTestimonial();
 
   useEffect(() => {
-    if (ref.current) ref.current.onclick = event => setSelected(data);
+    if (ref.current) ref.current.onclick = event => setSelected({ data, ref });
   }, [ref, data, setSelected]);
+
+  if (animation)
+    return (
+      <MotionDiv {...animation} ref={ref} className={className}>
+        {children}
+      </MotionDiv>
+    );
 
   return (
     <div ref={ref} className={className}>
