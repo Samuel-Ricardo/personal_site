d95a95e1a647ef4225fc7625fa27ec20013c0a82
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 11:37:13 -0300

    [ :sparkles: ] | update: service - person (module::core:static)

--- personal_website/src/modules/@core/person/service/person.service.ts
+++ personal_website/src/modules/@core/person/service/person.service.ts
@@ -4,9 +4,10 @@ import { FindOnePersonUseCase } from '../use_case/find/one.use_case';
 import { MODULE } from '@/modules/app.registry';
 import { IFindAllPersonDTO } from '../DTO/gateway/find/all.dto';
 import { IFindOnePersonDTO } from '../DTO/gateway/find/one.dto';
+import { IPersonService } from './service.interface';
 
 @injectable()
-export class PersonService {
+export class PersonService implements IPersonService {
   constructor(
     @inject(MODULE.PERSON.USE_CASE.FIND.ALL)
     protected readonly _findAll: FindAllPersonUseCase,
@@ -14,11 +15,11 @@ export class PersonService {
     protected readonly _findOne: FindOnePersonUseCase,
   ) {}
 
-  async findAll(DTO?: IFindAllPersonDTO) {
+  async findAllAsync(DTO?: IFindAllPersonDTO) {
     return await this._findAll.execute(DTO);
   }
 
-  async findOne(DTO: IFindOnePersonDTO) {
+  async findOneAsync(DTO: IFindOnePersonDTO) {
     return await this._findOne.execute(DTO);
   }
 }
