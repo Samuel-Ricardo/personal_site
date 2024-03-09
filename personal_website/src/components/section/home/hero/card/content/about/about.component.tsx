2da2d569fccaf40646997e85efdb76da458716a9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 09:48:16 -0300

    [ :zap: ] | improve: card - hero > content [about] (app::component)

--- personal_website/src/components/section/home/hero/card/content/about/about.component.tsx
+++ personal_website/src/components/section/home/hero/card/content/about/about.component.tsx
@@ -1,12 +1,12 @@
 import './about.style.scss';
 
-export const HeroCardContentAbout = () => {
+export const HeroCardContentAbout = async (props: React.PropsWithChildren) => {
   return (
     <div className="about">
       <p>
-        👨‍💻| I&apos;m a Programmer passionated about unlocking 🔓 the barriers
-        between the real 🧠 and the imaginary 🌌, convert dreams into reality
-        and of course... <br /> Coffee ☕ into solutions 💡
+        {props.children
+          ? props.children
+          : '👨‍💻| I&apos;m a Programmer passionated about unlocking 🔓 the barriers between the real 🧠 and the imaginary 🌌, convert dreams into reality and of course... <br /> Coffee ☕ into solutions 💡'}
       </p>
     </div>
   );
