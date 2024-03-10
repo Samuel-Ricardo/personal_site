f13455a12b7abd6eb04b41df7296350b4c941383
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-09 21:52:52 -0300

    [ :zap: ] | improve: canvas - circuit (app::component)

--- personal_website/src/components/canvas/circuit.component.tsx
+++ personal_website/src/components/canvas/circuit.component.tsx
@@ -33,7 +33,7 @@ export const Circuit = ({ className }: HTMLAttributes<HTMLDivElement>) => {
 
     const width = canvasRef.current?.width || 1;
 
-    const points = generate({ space, quantity: (width / 100) * 6 });
+    const points = generate({ space, quantity: (width / 100) * 5 });
 
     canvasRef.current?.addEventListener('mousemove', e => {
       mouse.x = e.clientX;
