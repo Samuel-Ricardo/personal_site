5d6ec16b255a54d3cf2a63d07e258c3cfffa5aaa
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-03 08:22:27 -0300

    [ :wheelchair: ] | improve: accessibility - button > glass (app::components)

--- personal_website/src/components/button/glass/glass.button.tsx
+++ personal_website/src/components/button/glass/glass.button.tsx
@@ -2,7 +2,11 @@ import { HTMLAttributes } from 'react';
 
 export const GlassButton = (props: HTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button className={`glass-button ${props.className}`} {...props}>
+    <button
+      className={`glass-button ${props.className}`}
+      {...props}
+      aria-label={props['aria-label'] || 'acrylic design button'}
+    >
       {props.children}
     </button>
   );
