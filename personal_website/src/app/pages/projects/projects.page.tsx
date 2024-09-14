a910d0ed0adf0a4b6c25b5526dc65f913da00148
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-09-14 16:34:11 -0300

    [ :mag: ] | create: engine - search > projects (app::component)

--- personal_website/src/app/pages/projects/projects.page.tsx
+++ personal_website/src/app/pages/projects/projects.page.tsx
@@ -1,23 +1,38 @@
+import { SearchProjectResult } from '@/components/search/result/project/result.component';
 import './projects.style.scss';
 
 import { MainProjectCard } from '@/components/card/project/main/card.component';
+import SearchBar from '@/components/search/search.component';
 import { Slider } from '@/components/slider/slider.component';
 import { MODULES } from '@/modules/app.factory';
 
-export const ProjectsPage = async () => {
-  const projects = await MODULES.PROJECT.CONTROLLER.MAIN().findMainProjects();
+export const ProjectsPage = async ({ search }: { search: string }) => {
+  const projects = await MODULES.PROJECT.CONTROLLER.MAIN().findAllSync();
+
+  const main = projects.filter(project => project.main);
+  const list =
+    !search || search.trim() === ''
+      ? projects
+      : projects.filter(
+          project =>
+            project.title.toLowerCase().includes(search.toLowerCase()) ||
+            project.description.toLowerCase().includes(search.toLowerCase()) ||
+            project.techs.filter(t =>
+              t.name.toLowerCase().includes(search.toLowerCase()),
+            ).length > 0,
+        );
 
   return (
     <div className="projects-page-container">
       <Slider>
-        {projects.map(async project => (
-          <MainProjectCard
-            key={(await project).title}
-            data={await (await project).toView()}
-          />
+        {main.map(async project => (
+          <MainProjectCard key={project.title} data={project} />
         ))}
       </Slider>
-      <div className="projects-page-search-container"></div>
+      <div className="projects-page-search-container">
+        <SearchBar />
+        <SearchProjectResult items={list} />
+      </div>
     </div>
   );
 };
