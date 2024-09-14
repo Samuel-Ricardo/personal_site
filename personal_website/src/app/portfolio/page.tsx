dd32df24239c628f8e1a5110a2c1d11c753c2160
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-09-14 16:18:30 -0300

    [ :page_with_curl: ] | update: page - portfolio [search params] (app::page)

--- personal_website/src/app/portfolio/page.tsx
+++ personal_website/src/app/portfolio/page.tsx
@@ -1,9 +1,13 @@
 import { ProjectsPage } from '../pages/projects/projects.page';
 
-export default async function Portfolio() {
+export default async function Portfolio(props) {
+  const {
+    searchParams: { searchTerm },
+  } = props;
+
   return (
-    <div className="page-container">
-      <ProjectsPage />
+    <div className="page-container overflow-y-scroll">
+      <ProjectsPage search={searchTerm} />
     </div>
   );
 }
