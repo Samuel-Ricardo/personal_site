0f1ca670f8f572fbb5904054e4b943f7f25e3933
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-09-15 02:46:22 -0300

    [ :movie_camera: ] | annimate: component - card > item [project] (app::component)

--- personal_website/src/components/search/result/project/result.component.tsx
+++ personal_website/src/components/search/result/project/result.component.tsx
@@ -1,18 +1,29 @@
 import { IProjectSyncDTO } from '@/modules/@core/project/DTO/sync.dto';
 import './result.style.scss';
 import { ProjectCard } from '@/components/card/project/item/card.component';
+import { MotionLI } from '@/components/motion/li.component';
+import { MODULES } from '@/modules/app.factory';
 
 export const SearchProjectResult = async ({
   items,
 }: {
   items: IProjectSyncDTO[];
 }) => {
+  const UP = MODULES.ANIMATION.FRAMER_MOTION.EMERGE.UP();
+
   return (
     <ul className="search-result-container">
-      {items.map(async item => (
-        <li key={item.title} className="search-result-item">
+      {items.map(async (item, index) => (
+        <MotionLI
+          key={item.title}
+          className="search-result-item"
+          {...UP({
+            transition: { delay: index * 0.25, duration: 1 },
+            animate: 'active',
+          })}
+        >
           <ProjectCard data={item} />
-        </li>
+        </MotionLI>
       ))}
     </ul>
   );
