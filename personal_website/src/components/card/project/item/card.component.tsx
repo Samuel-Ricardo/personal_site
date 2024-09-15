433abf4e264a97448a5560abc8c0c1bc4a461681
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-09-15 02:27:15 -0300

    [ :iphone: ] | responsive: component - card > item [project] (app::component)

--- personal_website/src/components/card/project/item/card.component.tsx
+++ personal_website/src/components/card/project/item/card.component.tsx
@@ -5,22 +5,26 @@ import './card.style.scss';
 
 import { IProjectSyncDTO } from '@/modules/@core/project/DTO/sync.dto';
 import { HorizontalLine } from '@/components/line/horizontal/horizontal.component';
+import Link from 'next/link';
 
 export const ProjectCard = ({ data }: { data: IProjectSyncDTO }) => {
   return (
     <div className="project-card">
-      <MainProjectCardImage url={data?.image} />
-      <HorizontalLine />
-      <div className="content-container">
-        <h1>{data?.title}</h1>
+      <Link className="link-container" href={`/project/${data?.title}`}>
+        <MainProjectCardImage url={data?.image} />
         <HorizontalLine />
-        <p>{data?.description}</p>
-      </div>
-      <div className="footer-container">
-        <ProjectItemTechs
-          main_techs={data?.main_techs.slice(0, 4).concat([AND_MORE])}
-        />
-      </div>
+        <div className="content-container">
+          <h1>{data?.title}</h1>
+          <HorizontalLine />
+          <p>{data?.description}</p>
+        </div>
+        <div className="footer-container">
+          <ProjectItemTechs
+            main_techs={data?.main_techs.slice(0, 4).concat([AND_MORE])}
+          />
+        </div>
+      </Link>
+      <div className="neon-line" />
     </div>
   );
 };
--- personal_website/src/components/search/result/project/result.component.tsx
+++ personal_website/src/components/search/result/project/result.component.tsx
@@ -1,5 +1,6 @@
 import { IProjectSyncDTO } from '@/modules/@core/project/DTO/sync.dto';
 import './result.style.scss';
+import { ProjectCard } from '@/components/card/project/item/card.component';
 
 export const SearchProjectResult = async ({
   items,
@@ -10,7 +11,7 @@ export const SearchProjectResult = async ({
     <ul className="search-result-container">
       {items.map(async item => (
         <li key={item.title} className="search-result-item">
-          {item.title}
+          <ProjectCard data={item} />
         </li>
       ))}
     </ul>
