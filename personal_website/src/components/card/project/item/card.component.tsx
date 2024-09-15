63e665861f558b2d7a4b2b71771dea26f6d81d4e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-09-15 02:03:46 -0300

    [ :art: ] | create: component - card > item [project] (app::component)

---
+++ personal_website/src/components/card/project/item/card.component.tsx
@@ -0,0 +1,26 @@
+import { ProjectItemTechs } from '@/components/section/home/projects/item/techs/techs.component';
+import { AND_MORE } from '@/local/data/static/techs.data';
+import { MainProjectCardImage } from '../main/image/image.component';
+import './card.style.scss';
+
+import { IProjectSyncDTO } from '@/modules/@core/project/DTO/sync.dto';
+import { HorizontalLine } from '@/components/line/horizontal/horizontal.component';
+
+export const ProjectCard = ({ data }: { data: IProjectSyncDTO }) => {
+  return (
+    <div className="project-card">
+      <MainProjectCardImage url={data?.image} />
+      <HorizontalLine />
+      <div className="content-container">
+        <h1>{data?.title}</h1>
+        <HorizontalLine />
+        <p>{data?.description}</p>
+      </div>
+      <div className="footer-container">
+        <ProjectItemTechs
+          main_techs={data?.main_techs.slice(0, 4).concat([AND_MORE])}
+        />
+      </div>
+    </div>
+  );
+};
