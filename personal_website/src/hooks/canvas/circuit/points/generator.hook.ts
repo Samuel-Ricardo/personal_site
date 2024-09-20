ef93f89a838e09379fe0f9aee405e1a302f87562
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-09-20 15:02:44 -0300

    [ :link: ] | update: hook's - circuit (app::hook)

--- personal_website/src/hooks/canvas/circuit/points/generator.hook.ts
+++ personal_website/src/hooks/canvas/circuit/points/generator.hook.ts
@@ -3,23 +3,30 @@ import { IPointsProps } from '@/@types/props/circuit/points';
 import { useCallback } from 'react';
 
 export const usePoints = () => {
-  const generate = useCallback(({ space, quantity = 40 }: IPointsProps) => {
-    const points: IPoint[] = [];
-    for (let i = 0; i < quantity; i++) {
-      points.push({
-        position: {
-          x: Math.random() * space.width,
-          y: Math.random() * space.height,
-        },
-        radius: Math.random() * 1 + 1,
-        velocity: {
-          x: Math.floor(Math.random() * 50) - 25,
-          y: Math.floor(Math.random() * 50) - 25,
-        },
-      });
-    }
-    return points;
-  }, []);
+  const generate = useCallback(
+    ({
+      space,
+      quantity = 40,
+      radius = Math.random() * 1 + 1,
+    }: IPointsProps) => {
+      const points: IPoint[] = [];
+      for (let i = 0; i < quantity; i++) {
+        points.push({
+          position: {
+            x: Math.random() * space.width,
+            y: Math.random() * space.height,
+          },
+          radius: radius,
+          velocity: {
+            x: Math.floor(Math.random() * 50) - 25,
+            y: Math.floor(Math.random() * 50) - 25,
+          },
+        });
+      }
+      return points;
+    },
+    [],
+  );
 
   return { generate };
 };
